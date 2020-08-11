467bc9598a155a748924c84ce47ce8aef1611cdd
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-08-11 09:02:55 -0400

    refactor: rename parameter to effectGeneratorFunction

--- source/Effect/doEffect.ts
+++ source/Effect/doEffect.ts
@@ -3,17 +3,17 @@ import { ask } from './ask'
 import { AddEnv, EffectGenerator, EffectOf } from './Effect'
 
 export const doEffect = <G extends () => EffectGenerator<any, any>>(
-  generatorFunction: G,
+  effectGeneratorFunction: G,
 ): EffectOf<G> =>
   (({
-    [Symbol.iterator]: generatorFunction,
+    [Symbol.iterator]: effectGeneratorFunction,
   } as unknown) as EffectOf<G>)
 
 export const doEffectWith = <G extends (e: any) => EffectGenerator<any, any>>(
-  generatorFunction: G,
+  effectGeneratorFunction: G,
 ): AddEnv<HeadArg<G>, EffectOf<G>> =>
   doEffect(function* () {
     const e1 = yield* ask<HeadArg<G>>()
 
-    return yield* generatorFunction(e1)
+    return yield* effectGeneratorFunction(e1)
   })
