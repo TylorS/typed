9977e24c0e0314ec9e6b034a47f7993e3a6caa47
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-08-16 20:19:10 -0400

    feat: provideKeyManager

--- src/hooks/KeyManagerChannel.ts
+++ src/hooks/KeyManagerChannel.ts
@@ -1,10 +1,20 @@
-import { asks } from '@typed/fp/Effect'
+import { asks, Effect } from '@typed/fp/Effect'
 
 import { createChannel } from './Channel'
 import { HookEnvironment } from './HookEnvironment'
+import { useChannel } from './useChannel'
 
 export interface KeyManagerState {
   readonly hookEnvironments: Map<any, HookEnvironment>
 }
 
 export const KeyManagerChannel = createChannel(asks((e: KeyManagerState) => e.hookEnvironments))
+
+const initial = Effect.fromIO(() => new Map<any, HookEnvironment>())
+
+/**
+ * Allow creating isolated KeyManager contexts
+ */
+export const provideKeyManager = <E>(
+  hookEnvironments: Effect<E, Map<any, HookEnvironment>> = initial,
+) => useChannel(KeyManagerChannel, hookEnvironments)
