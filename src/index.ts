47e38259d488ff14e990dfbd1a1194b61b67bc0b
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-08-16 20:07:39 -0400

    feat: only export instances

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-// New-Types
-export * from './Path'
-export * from './Uri'
-export * from './Uuid'
-
 // Instances
 export { effect, effectSeq } from './Effect'
 export { future, futureSeq } from './Future'
