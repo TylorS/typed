19367c655c1d3f51c6ecf27238cb1d18461395fa
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-08-18 22:10:21 -0400

    refactor: various Op improvments

--- src/Effect/Op.ts
+++ src/Effect/Op.ts
@@ -8,10 +8,10 @@ import { iso, Newtype } from 'newtype-ts'
 /**
  * Used to represent the resources required to perform a particular computation
  */
-export interface Op<Key = any, Args extends ReadonlyArray<any> = ReadonlyArray<any>, R = any>
+export interface Op<Key = any, Args extends ReadonlyArray<any> = any, R = any>
   extends Newtype<OpUri<Args, R>, Key> {}
 
-export interface OpUri<Args extends ReadonlyArray<any> = ReadonlyArray<any>, R = any> {
+export interface OpUri<Args extends ReadonlyArray<any> = any, R = any> {
   readonly Op: unique symbol
   readonly Args: Args
   readonly Return: R
@@ -32,28 +32,28 @@ export interface OpEnv<C extends Op>
     }
   > {}
 
-export type OpMap = Map<Op<any, any, any>, GetOp<any, Op<any, any, any>>>
+export type OpMap = Map<Op, GetOp<any, Op>>
 
 export type GetOp<E, C extends Op> = OpArgs<C> extends readonly []
   ? () => Effect<E, OpReturn<C>>
   : (...args: OpArgs<C>) => Effect<E, OpReturn<C>>
 
-const opIso = iso<Op<any, any, any>>()
+const opIso = iso<Op>()
 const opEnvIso = iso<OpEnv<any>>()
 const emptyOpEnv = (): OpEnv<any> => opEnvIso.wrap({ [OP]: new Map() })
 
 export function createOp<A extends Op>(key: OpKey<A>): A {
-  return opIso.wrap(key) as A
+  return  opIso.wrap(key) as A
 }
 
-export function provideOp<O extends Op, E>(Op: O, computation: GetOp<E, O>) {
+export function provideOp<O extends Op, E>(Op: O, implementation: GetOp<E, O>) {
   return <F, A>(eff: Effect<F & OpEnv<O>, A>): Effect<E & F, A> => {
     const effect = doEffect(function* () {
       // Since OpEnv is opaque and uses the Op as a key to it's map, it could already be provided
       const env = yield* ask<Partial<OpEnv<any>>>()
       const opEnv = isOpEnv(env) ? env : emptyOpEnv()
 
-      opEnvIso.unwrap(opEnv)[OP].set(Op, computation)
+      opEnvIso.unwrap(opEnv)[OP].set(Op, implementation)
 
       const value = yield* pipe(eff, provide(opEnv))
 
@@ -68,8 +68,8 @@ export function useOp<O extends Op>(Op: O) {
   return (...args: OpArgs<O>): Effect<OpEnv<O>, OpReturn<O>> => {
     return doEffect(function* () {
       const { [OP]: map } = yield* asks(opEnvIso.unwrap)
-      const computation = map.get(Op)! as GetOp<unknown, O>
-      const value = yield* computation(...args)
+      const op = map.get(Op)! as GetOp<unknown, O>
+      const value = yield* op(...args)
 
       return value
     })
