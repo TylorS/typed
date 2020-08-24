377bbdc57a6fa6e9c685f6964649d837da2fc921
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-08-24 18:40:42 -0400

    fix: add missing type

--- src/hooks/runWithHooks.ts
+++ src/hooks/runWithHooks.ts
@@ -7,6 +7,7 @@ import { HookOpEnvs } from './HookOps'
 export const RUN_WITH_HOOKS = Symbol('@typed/fp/hooks/RunWithHooks')
 export type RUN_WITH_HOOKS = typeof RUN_WITH_HOOKS
 
+// Opaque type used to represent the current implementations requirements to run a nested hook-enabled effect
 export interface HookRequirements
   extends Newtype<{ readonly HookRequirements: unique symbol }, unknown> {}
 
@@ -21,6 +22,9 @@ export const runWithHooks = callOp(RunWithHooksOp)
 
 declare module '@typed/fp/Op' {
   export interface Ops<Env> {
-    [RUN_WITH_HOOKS]: <E, A>(eff: Effect<E & HookOpEnvs, A>, req: HookRequirements) => Effect<E, A>
+    [RUN_WITH_HOOKS]: <E, A>(
+      eff: Effect<E & HookOpEnvs, A>,
+      req: HookRequirements,
+    ) => Effect<Env & E, A>
   }
 }
