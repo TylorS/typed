f88bad4a1fa57c95c48dba69e4c5e9f7df371ef7
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-08-27 16:36:10 -0400

    test: add test for info change

--- src/VirtualTimer/createVirtualScheduler.ts
+++ src/VirtualTimer/createVirtualScheduler.ts
@@ -1,11 +1,10 @@
 import { newScheduler, newTimeline } from '@most/scheduler'
 
-import { createVirtualClock, VirtualClock } from './VirtualClock'
-import { createVirtualTimer } from './VirtualTimer'
+import { createVirtualTimer, VirtualTimer } from './VirtualTimer'
 
 export const createVirtualScheduler = () => {
-  const clock: VirtualClock = createVirtualClock()
-  const scheduler = newScheduler(createVirtualTimer(clock), newTimeline())
+  const timer: VirtualTimer = createVirtualTimer()
+  const scheduler = newScheduler(timer, newTimeline())
 
-  return [clock, scheduler] as const
+  return [timer, scheduler] as const
 }
--- src/fibers/fibers.test.ts
+++ src/fibers/fibers.test.ts
@@ -3,7 +3,13 @@ import { doEffect } from '@typed/fp/Effect/doEffect'
 import { fork, getParentFiber } from '@typed/fp/fibers/FiberEnv'
 import { runAsFiber } from '@typed/fp/fibers/runAsFiber'
 import { describe, it } from '@typed/test'
+import { flow } from 'fp-ts/es6/function'
 import { none, some } from 'fp-ts/es6/Option'
+
+import { disposeNone } from '../Disposable'
+import { createVirtualScheduler } from '../VirtualTimer'
+import { foldFiberInfo } from './Fiber'
+import { delay } from './SchedulerEnv'
 
 export const test = describe(`fibers`, [
   describe(`getParentFiber`, [
@@ -65,6 +71,38 @@ export const test = describe(`fibers`, [
       })
 
       const fiber = runAsFiber(test, newDefaultScheduler())
+    }),
+
+    it(`parent emits success notification when waiting on fiber to complete`, ({ equal }, done) => {
+      const delayMs = 10
+      const [timer, scheduler] = createVirtualScheduler()
+      const expected = 10
+
+      const child = doEffect(function* () {
+        yield* delay(delayMs)
+
+        return 1
+      })
+
+      const parent = doEffect(function* () {
+        yield* fork(child)
+
+        return expected
+      })
+
+      const fiber = runAsFiber(parent, scheduler)
+
+      fiber.onInfoChange(
+        foldFiberInfo(
+          flow(() => timer.progressTimeBy(0), disposeNone), // Fork child
+          disposeNone,
+          flow(done, disposeNone),
+          flow(equal(expected), () => timer.progressTimeBy(delayMs), disposeNone), // Return child
+          flow(equal(expected), () => done(), disposeNone),
+        ),
+      )
+
+      timer.progressTimeBy(0) // Start parent fiber
     }),
   ]),
 ])
