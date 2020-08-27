a9d49455f6dc358007196dc145b1118107a77e4b
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-08-27 18:50:25 -0400

    feat: add types to Console

--- src/Console/index.ts
+++ src/Console/index.ts
@@ -1,13 +1,23 @@
-import { fromEnv, sync } from '@typed/fp/Effect'
+import { Effect, fromEnv, sync } from '@typed/fp/Effect'
+import { LoggerEffect } from '@typed/fp/logging'
 
 export interface ConsoleEnv {
   readonly console: Console
 }
 
-export const withConsole = <A>(f: (c: Console) => A) =>
+export const withConsole = <A>(f: (c: Console) => A): Effect<ConsoleEnv, A> =>
   fromEnv((e: ConsoleEnv) => sync(f(e.console)))
+
 export const clear = withConsole((c) => c.clear())
-export const error = (message: string) => withConsole((c) => c.error(message))
-export const info = (message: string) => withConsole((c) => c.info(message))
-export const log = (message: string) => withConsole((c) => c.log(message))
-export const warn = (message: string) => withConsole((c) => c.warn(message))
+
+export const error: LoggerEffect<ConsoleEnv, string> = (message: string) =>
+  withConsole((c) => c.error(message))
+
+export const info: LoggerEffect<ConsoleEnv, string> = (message: string) =>
+  withConsole((c) => c.info(message))
+
+export const log: LoggerEffect<ConsoleEnv, string> = (message: string) =>
+  withConsole((c) => c.log(message))
+
+export const warn: LoggerEffect<ConsoleEnv, string> = (message: string) =>
+  withConsole((c) => c.warn(message))
