b92f22f6370e4a4bbbda29c2f9d851db02f56621
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-08-30 13:20:25 -0400

    feat: update to latest from smallspace

--- src/io/smallspace.ts
+++ src/io/smallspace.ts
@@ -2,13 +2,12 @@ import { left, right } from 'fp-ts/es6/Either'
 import { fromNullable } from 'fp-ts/es6/Option'
 import { range } from 'fp-ts/es6/ReadonlyArray'
 import { Int } from 'io-ts'
-import { memoize, Schemable2C, WithUnion2C } from 'io-ts/es6/Schemable'
+import { memoize, Schemable1, WithUnion1 } from 'io-ts/es6/Schemable'
 import {
   always,
   array,
   boolean,
   char,
-  Nat,
   number,
   oneof,
   record,
@@ -26,25 +25,25 @@ import {
   Success,
 } from '../RemoteData'
 import { uuid4 } from '../Uuid/uuid4'
-import { TypedSchemable2C } from './TypedSchemable'
+import { TypedSchemable1 } from './TypedSchemable'
 
 export const URI = 'Smallspace/Source'
 export type URI = typeof URI
 
 declare module 'fp-ts/lib/HKT' {
-  export interface URItoKind2<E, A> {
-    [URI]: Source<E, A>
+  export interface URItoKind<A> {
+    [URI]: Source<A>
   }
 }
 
 declare module 'fp-ts/es6/HKT' {
-  export interface URItoKind2<E, A> {
-    [URI]: Source<E, A>
+  export interface URItoKind<A> {
+    [URI]: Source<A>
   }
 }
 
 export type SmallspaceSchemableParams = {
-  readonly string?: Source<Nat, string> // the characters in which to use to derive strings
+  readonly string?: Source<string> // the characters in which to use to derive strings
   readonly stringSeperator?: string
   readonly bigIntMultiplier?: number
 }
@@ -53,27 +52,27 @@ const DEFAULT_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 export function createSmallspaceSchemable(
   params: SmallspaceSchemableParams = {},
-): TypedSchemable2C<URI, Nat> {
+): TypedSchemable1<URI> {
   const { bigIntMultiplier = 1_333_333_333 } = params
 
-  const schemable: TypedSchemable2C<URI, Nat> = {
+  const schemable: TypedSchemable1<URI> = {
     URI,
     literal: (...values) => oneof(...values.map(always)),
     string: string(params.string ?? char(DEFAULT_CHARACTERS), params.stringSeperator),
     number,
     boolean,
     nullable: (s) => oneof(s, always(null)),
-    type: record as Schemable2C<URI, Nat>['type'],
-    array: array as Schemable2C<URI, Nat>['array'],
-    tuple: tuple as Schemable2C<URI, Nat>['tuple'],
+    type: record as Schemable1<URI>['type'],
+    array: array as Schemable1<URI>['array'],
+    tuple: tuple as Schemable1<URI>['tuple'],
     partial,
     record: (codomain) => (n) =>
       schemable.string(n).flatMap((key) => codomain(n).map((value) => ({ [key]: value }))),
     intersect: (right) => (left) => (n) =>
       left(n).flatMap((l) => right(n).map((r) => ({ ...l, ...r }))),
-    sum: () => (members) => oneof(...(Object.values(members) as Array<Source<Nat, any>>)),
+    sum: () => (members) => oneof(...(Object.values(members) as Array<Source<any>>)),
     lazy,
-    union: oneof as WithUnion2C<URI, Nat>['union'],
+    union: oneof as WithUnion1<URI>['union'],
     set: (v) => (n) => v(n).map((v) => new Set([v])),
     map: (k, v) => (n) => k(n).flatMap((key) => v(n).map((value) => new Map([[key, value]]))),
     option: (s) => (n) =>
@@ -105,23 +104,23 @@ export function createSmallspaceSchemable(
   return schemable
 }
 
-function lazy<A>(_id: string, f: () => Source<Nat, A>): Source<Nat, A> {
-  const get = memoize<void, Source<Nat, A>>(f)
+function lazy<A>(_id: string, f: () => Source<A>): Source<A> {
+  const get = memoize<void, Source<A>>(f)
 
   return (n) => get()(n)
 }
 
-function partial<A>(props: { [K in keyof A]: Source<Nat, A[K]> }): Source<Nat, Partial<A>> {
-  const entries = Object.entries(props) as Array<[keyof A, Source<Nat, unknown>]>
+function partial<A>(props: { [K in keyof A]: Source<A[K]> }): Source<Partial<A>> {
+  const entries = Object.entries(props) as Array<[keyof A, Source<unknown>]>
   const mapped = entries.map(([key, value]) => convertToPartial(key, value))
   const partialProps = Object.fromEntries(mapped)
 
-  return record(partialProps) as Source<Nat, Partial<A>>
+  return record(partialProps) as Source<Partial<A>>
 }
 
 function convertToPartial<K extends PropertyKey, V>(
   key: K,
-  value: Source<Nat, V>,
-): [K, Source<Nat, V | undefined>] {
+  value: Source<V>,
+): [K, Source<V | undefined>] {
   return [key, oneof(value, always(undefined))]
 }
