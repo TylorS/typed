2d178f8856eb2ed4bc45a833cf56166cf1aba970
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-09-20 16:27:04 -0400

    feat: make it easier to use Key

--- src/Key/exports.ts
+++ src/Key/exports.ts
@@ -3,14 +3,14 @@ import { Const } from 'fp-ts/Const'
 import { Iso } from 'monocle-ts'
 import { iso, Newtype } from 'newtype-ts'
 
-export interface Key<A> extends Newtype<{ readonly Key: unique symbol }, Const<string, A>> {}
+export interface Key<A> extends Newtype<Const<{ readonly Key: unique symbol }, A>, string> {}
 
-export interface KeyIso<A> extends Iso<Key<A>, Const<string, A>> {}
+export interface KeyIso<A> extends Iso<Key<A>, string> {}
 
 export const getKeyIso = <A>(): KeyIso<A> => iso<Key<A>>()
 
-export interface UuidKey<A> extends Newtype<{ readonly key: unique symbol }, Const<Uuid, A>> {}
+export interface UuidKey<A> extends Newtype<Const<{ readonly key: unique symbol }, A>, Uuid> {}
 
-export interface UuidKeyIso<A> extends Iso<UuidKey<A>, Const<Uuid, A>> {}
+export interface UuidKeyIso<A> extends Iso<UuidKey<A>, Uuid> {}
 
 export const getUuidKeyIso = <A>(): UuidKeyIso<A> => iso<UuidKey<A>>()
