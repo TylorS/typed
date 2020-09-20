33d5de1b08086e5eee41ec32c33422e20f6c1666
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-09-20 14:23:27 -0400

    feat: add Iso helpers

--- src/Key/exports.ts
+++ src/Key/exports.ts
@@ -5,8 +5,12 @@ import { iso, Newtype } from 'newtype-ts'
 
 export interface Key<A> extends Newtype<{ readonly Key: unique symbol }, Const<string, A>> {}
 
-export const getKeyIso = <A>(): Iso<Key<A>, Const<string, A>> => iso<Key<A>>()
+export interface KeyIso<A> extends Iso<Key<A>, Const<string, A>> {}
+
+export const getKeyIso = <A>(): KeyIso<A> => iso<Key<A>>()
 
 export interface UuidKey<A> extends Newtype<{ readonly key: unique symbol }, Const<Uuid, A>> {}
 
-export const getUuidKeyIso = <A>(): Iso<UuidKey<A>, Const<Uuid, A>> => iso<UuidKey<A>>()
+export interface UuidKeyIso<A> extends Iso<UuidKey<A>, Const<Uuid, A>> {}
+
+export const getUuidKeyIso = <A>(): UuidKeyIso<A> => iso<UuidKey<A>>()
