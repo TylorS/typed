72933ac72b17b96dcb1d213779b21c2e90bb4dfd
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-09-27 18:28:37 -0400

    feat: left + right helpers

--- src/Future/exports.ts
+++ src/Future/exports.ts
@@ -8,11 +8,12 @@ import {
   FailEnv,
   fromReader,
   fromTask,
+  Pure,
   toReaderTask,
   URI as EffectURI,
 } from '@typed/fp/Effect/exports'
 import { Alt3 } from 'fp-ts/Alt'
-import { Either } from 'fp-ts/Either'
+import * as E from 'fp-ts/Either'
 import { EitherM2, getEitherM } from 'fp-ts/EitherT'
 import { Monad3 } from 'fp-ts/Monad'
 import { pipe, pipeable } from 'fp-ts/pipeable'
@@ -22,7 +23,7 @@ import { isLeft } from 'fp-ts/These'
 export const URI = '@typed/fp/Future/exports'
 export type URI = typeof URI
 
-export type Future<E, A, B> = Effect<E, Either<A, B>>
+export type Future<E, A, B> = Effect<E, E.Either<A, B>>
 
 declare module 'fp-ts/HKT' {
   export interface URItoKind3<R, E, A> {
@@ -35,6 +36,9 @@ export const futureSeq: Monad3<URI> & Alt3<URI> & EitherM2<EffectURI> = {
   ...getEitherM(effectSeq),
   URI,
 }
+
+export const left = <A, B>(value: A): Future<{}, A, B> => Pure.of(E.left<A, B>(value))
+export const right = <A, B>(value: B): Future<{}, A, B> => Pure.of(E.right<A, B>(value))
 
 export const {
   alt,
