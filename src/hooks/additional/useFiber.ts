cce8f44b7913d6cc48210b1fcc9c25df2a7efbe9
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-10-10 18:21:03 -0400

    feat: track fiber disposable

--- src/hooks/additional/useFiber.ts
+++ src/hooks/additional/useFiber.ts
@@ -1,25 +1,38 @@
+import { disposeNone } from '@most/disposable'
+import { Disposable } from '@most/types'
 import { deepEqualsEq } from '@typed/fp/common/exports'
-import { doEffect, Effect, EnvOf } from '@typed/fp/Effect/exports'
+import { doEffect, Effect, EnvOf, Pure } from '@typed/fp/Effect/exports'
 import { Fiber, FiberEnv, fork } from '@typed/fp/fibers/exports'
 import { Fn } from '@typed/fp/lambda/exports'
 import { createRef, Ref } from '@typed/fp/SharedRef/exports'
 import { Eq } from 'fp-ts/Eq'
 import { getEq } from 'fp-ts/ReadonlyArray'
 
-import { useDepChange, useMemo } from '../core/exports'
+import { addDisposable, getHookEnv, useDepChange, useMemo, useRef } from '../core/exports'
 
 export const useFiber = <A extends ReadonlyArray<any>, B, C>(
   f: Fn<A, Effect<B, C>>,
   args: A | Readonly<A>,
   eq: Eq<A> | Eq<Readonly<A>> = getEq(deepEqualsEq),
-): Effect<B & FiberEnv & EnvOf<typeof useDepChange> & EnvOf<typeof useMemo>, Fiber<C>> => {
+): Effect<
+  B & FiberEnv & EnvOf<typeof useDepChange> & EnvOf<typeof useMemo> & EnvOf<typeof addDisposable>,
+  Fiber<C>
+> => {
   const eff = doEffect(function* () {
+    const env = yield* getHookEnv
     const depsChanged = yield* useDepChange(args, eq, true)
     const ref: Ref<Fiber<C> | undefined> = yield* useMemo(() => createRef<Fiber<C>>(), [])
+    const disposable: Ref<Disposable> = yield* useRef(Pure.fromIO(disposeNone))
 
     if (depsChanged) {
+      disposable.current.dispose()
       ref.current?.dispose()
-      ref.current = yield* fork(f(...args))
+
+      const fiber = yield* fork(f(...args))
+
+      ref.current = fiber
+
+      disposable.current = yield* addDisposable(env.id, fiber)
     }
 
     return ref.current!
