412b706553f0771464cb6004431904da6cda9ccb
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-10-12 12:45:31 -0400

    feat: make useMatchManager widen environments and return types

--- src/hooks/patching/useMatchManager.ts
+++ src/hooks/patching/useMatchManager.ts
@@ -1,37 +1,40 @@
-import { Arity1 } from '@typed/fp/common/exports'
+import { And, Arity1 } from '@typed/fp/common/exports'
 import { doEffect, Effect } from '@typed/fp/Effect/exports'
+import { Fn } from '@typed/fp/lambda/exports'
 import { Match } from '@typed/fp/logic/exports'
-import { Ref } from '@typed/fp/SharedRef/exports'
+import { Ref, SharedRefEnv } from '@typed/fp/SharedRef/exports'
 import { identity } from 'fp-ts/function'
-import { isNone, none, some } from 'fp-ts/Option'
+import { isNone, none, Option, some } from 'fp-ts/Option'
 
 import { useEffectBy, useMatches } from '../additional/exports'
-import { useMemo } from '../core/exports'
+import { HookEnv, HookPositions, HookSymbols, useMemo } from '../core/exports'
 import { useKeyManager } from './useKeyManager'
 
+/**
+ * Allows sequencing a series of possible matches to a some value.
+ */
 export function useMatchManager<
   A,
   B,
-  C,
-  Matches extends ReadonlyArray<Match<A, Arity1<Ref<B | null | undefined>, Effect<any, C>>>>
->(value: A, ...matches: Matches) {
+  Matches extends ReadonlyArray<Match<A, Arity1<Ref<B | null | undefined>, Effect<any, any>>>>
+>(
+  value: A,
+  ...matches: Matches
+): Effect<
+  HookEnv & SharedRefEnv<HookPositions> & SharedRefEnv<HookSymbols> & EnvOfMatchesReturn<Matches>,
+  Option<ReturnOfMatchesReturn<Matches>>
+> {
   const eff = doEffect(function* () {
     const modifiedMatches = yield* useMemo(
       (ms) => ms.map((m) => Match.map((c) => [m, c] as const, m)),
       [matches],
     )
-    const option = yield* useMatches(value, ...modifiedMatches)
-    const [c] = yield* useEffectBy([option], identity, (opt) =>
+
+    const match = yield* useMatches(value, ...modifiedMatches)
+
+    const [c] = yield* useEffectBy([match], identity, (option) =>
       doEffect(function* () {
-        if (isNone(opt)) {
-          return none
-        }
-
-        const [match, computation] = opt.value
-
-        const x = yield* useKeyManager(match, computation)
-
-        return some(x)
+        return isNone(option) ? none : some(yield* useKeyManager(...option.value))
       }),
     )
 
@@ -40,3 +43,17 @@ export function useMatchManager<
 
   return eff
 }
+
+export type EnvOfMatchesReturn<
+  Matches extends ReadonlyArray<Match<any, Fn<any, Effect<any, any>>>>
+> = And<
+  {
+    [K in keyof Matches]: Matches[K] extends Match<any, Fn<any, Effect<infer R, any>>> ? R : never
+  }
+>
+
+export type ReturnOfMatchesReturn<
+  Matches extends ReadonlyArray<Match<any, Fn<any, Effect<any, any>>>>
+> = {
+  [K in keyof Matches]: Matches[K] extends Match<any, Fn<any, Effect<any, infer R>>> ? R : never
+}[number]
