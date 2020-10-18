44f5b7421dc4afc29cc4b88756a8e94f89d00042
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-10-18 13:26:12 -0400

    feat: create package.json files that point to all of the files in the project

--- tools/build-packages.ts
+++ tools/build-packages.ts
@@ -1,25 +1,107 @@
-import { existsSync, mkdirSync, writeFileSync } from 'fs'
-import { join } from 'path'
+import { existsSync, mkdirSync, readdirSync, statSync, writeFileSync } from 'fs'
+import { basename, dirname, join, relative } from 'path'
 import * as rimraf from 'rimraf'
 
-import { MODULES, readRelativeFile, ROOT_DIR } from './common'
+import { MODULES, readRelativeFile, ROOT_DIR, SOURCE_DIR } from './common'
 
-const NAME_REGEX = new RegExp('%name%', 'g')
+const MAIN_REGEX = new RegExp('%main%', 'g')
+const TYPES_REGEX = new RegExp('%types%', 'g')
+const MODULE_REGEX = new RegExp('%module%', 'g')
+
 const PACKAGE_JSON_TEMPLATE = readRelativeFile(__dirname, 'package-template.json')
+const TS_REGEX = /.tsx?$/g
+const TEST_REGEX = /.test.tsx?$/g
+const BROWSER_TEST_REGEX = /.browser-test.tsx?$/g
 
-function createTemplate(name: string) {
+MODULES.forEach(createExportTemplate)
+
+MODULES.forEach((name) =>
+  readAllFiles(join(SOURCE_DIR, name)).forEach((file) => createPackageFor(name, file)),
+)
+
+function createExportTemplate(name: string) {
   const directory = join(ROOT_DIR, name)
 
-  if (existsSync(directory)) {
-    rimraf.sync(directory)
-  }
-
-  mkdirSync(directory)
+  createDir(directory)
 
   const packageJsonPath = join(directory, 'package.json')
-  const packageJsonContents = PACKAGE_JSON_TEMPLATE.replace(NAME_REGEX, name)
+  const main = `../cjs/${name}/exports.js`
+  const types = `../cjs/${name}/exports.d.ts`
+  const module = `../esm/${name}/exports.d.ts`
+  const packageJsonContents = createPkgJson(main, types, module)
 
   writeFileSync(packageJsonPath, packageJsonContents)
 }
 
-MODULES.forEach(createTemplate)
+function readAllFiles(directory: string): ReadonlyArray<string> {
+  const contents = readdirSync(directory).map((n) => join(directory, n))
+  const fileNames = contents
+    .filter((p) => statSync(p).isFile())
+    .filter((p) => shouldCreatePackageFor(basename(p)))
+  const directoryNames = contents.filter((p) => statSync(p).isDirectory())
+
+  return [...fileNames, ...directoryNames.flatMap(readAllFiles)]
+}
+
+function createPackageFor(name: string, filePath: string) {
+  const moduleDir = join(ROOT_DIR, name)
+  const srcDir = join(SOURCE_DIR, name)
+  const cjsDir = join(ROOT_DIR, 'cjs', name)
+  const esmDir = join(ROOT_DIR, 'esm', name)
+  const relativeSrcPath = relative(srcDir, filePath)
+  const relativePath = relativeSrcPath.replace(TS_REGEX, '')
+  const modulePath = join(moduleDir, relativePath)
+  const jsonPath = join(modulePath, 'package.json')
+  const cjsPath = join(cjsDir, relativePath)
+  const typesPath = join(cjsDir, relativePath)
+  const esmPath = join(esmDir, relativePath)
+
+  const main = relative(modulePath, cjsPath) + '.js'
+  const types = relative(modulePath, typesPath) + '.d.ts'
+  const module = relative(modulePath, esmPath) + '.js'
+
+  const json = createPkgJson(main, types, module)
+
+  createDir(modulePath)
+
+  writeFileSync(jsonPath, json)
+}
+
+function createDir(directory: string) {
+  if (existsSync(directory)) {
+    rimraf.sync(directory)
+  }
+
+  let parent = dirname(directory)
+  const dirsToCreate: string[] = []
+
+  if (!existsSync(parent)) {
+    dirsToCreate.unshift(parent)
+
+    parent = dirname(parent)
+  }
+
+  for (const dir of dirsToCreate) {
+    mkdirSync(dir)
+  }
+
+  mkdirSync(directory)
+}
+
+function shouldCreatePackageFor(fileName: string) {
+  if (
+    TEST_REGEX.test(fileName) ||
+    BROWSER_TEST_REGEX.test(fileName) ||
+    (fileName.startsWith('tsconfig.') && fileName.endsWith('.json'))
+  ) {
+    return false
+  }
+
+  return true
+}
+
+function createPkgJson(main: string, types: string, module: string): string {
+  return PACKAGE_JSON_TEMPLATE.replace(MAIN_REGEX, main)
+    .replace(TYPES_REGEX, types)
+    .replace(MODULE_REGEX, module)
+}
