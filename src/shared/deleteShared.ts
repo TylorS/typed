c4f0d7b0bee44ddafc725f6a94a0a8263037a5ba
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-10-31 16:46:22 -0400

    feat: add global helpers

--- src/shared/deleteShared.ts
+++ src/shared/deleteShared.ts
@@ -4,7 +4,8 @@ import { Shared } from './Shared'
 import { getCurrentNamespace, getNamespacesMap, sendSharedEvent, SharedEnv } from './SharedEnv'
 
 /**
- * Delete a shared value.
+ * Delete a shared value. Will allow re-initializing a default value the next time you
+ * ask for it.
  */
 export const deleteShared = <S extends Shared>(shared: S): Effect<SharedEnv, void> =>
   doEffect(function* () {
--- src/shared/global.ts
+++ src/shared/global.ts
@@ -1,4 +1,14 @@
+import { Effect } from '@typed/fp/Effect/Effect'
+import { curry } from '@typed/fp/lambda/exports'
+import { flow, pipe } from 'fp-ts/lib/function'
+
+import { deleteShared } from './deleteShared'
+import { getShared } from './getShared'
+import { EnvOf, Shared, ValueOf } from './Shared'
+import { SharedEnv } from './SharedEnv'
+import { updatedShared } from './updateShared'
 import { usingNamespace } from './usingNamespace'
+import { withShared } from './withShared'
 
 /**
  * A namespace to be used as if it is global state.
@@ -10,3 +20,22 @@ export const GLOBAL_NAMESPACE = Symbol.for('@typed/fp/Global')
  * used to model singletons.
  */
 export const usingGlobal = usingNamespace(GLOBAL_NAMESPACE)
+
+export const getGlobal = flow(getShared, usingGlobal) as typeof getShared
+
+export const updateGlobal = curry(
+  <S extends Shared>(
+    shared: S,
+    update: (current: ValueOf<S>) => ValueOf<S>,
+  ): Effect<SharedEnv & EnvOf<Shared>, ValueOf<S>> =>
+    pipe(updatedShared(shared, update), usingGlobal),
+) as typeof updatedShared
+
+export const withGlobal = curry(
+  <S extends Shared, E, A>(
+    shared: S,
+    f: (value: ValueOf<S>) => Effect<E, A>,
+  ): Effect<E & SharedEnv & EnvOf<S>, A> => pipe(withShared(shared, f), usingGlobal),
+) as typeof withShared
+
+export const deleteGlobal = flow(deleteShared, usingGlobal) as typeof deleteShared
