28660ba6460675f49ea8f60ed693c96f94c8a9b1
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-11-08 13:30:44 -0500

    feat: don't memoize based on Effect

--- src/Effect/provideWith.ts
+++ src/Effect/provideWith.ts
@@ -5,29 +5,15 @@ import { Effect } from './Effect'
 import { provideSome, useSome } from './provide'
 
 export function provideWith<E1, E2>(provider: Effect<E1, E2>) {
-  const providedEnvs = new WeakMap<Effect<any, any>, any>()
-
   return <E3, A>(effect: Effect<E2 & E3, A>): Effect<E1 & E3, A> =>
     doEffect(function* () {
-      const provided = providedEnvs.has(effect)
-        ? providedEnvs.get(effect)!
-        : providedEnvs.set(effect, yield* provider).get(effect)!
-      const value = yield* pipe(effect, provideSome(provided)) as Effect<E3, A>
-
-      return value
+      return yield* pipe(effect, provideSome(yield* provider))
     })
 }
 
 export function useWith<E1, E2>(provider: Effect<E1, E2>) {
-  const providedEnvs = new WeakMap<Effect<any, any>, any>()
-
   return <E3, A>(effect: Effect<E2 & E3, A>): Effect<E1 & E3, A> =>
     doEffect(function* () {
-      const provided = providedEnvs.has(effect)
-        ? providedEnvs.get(effect)!
-        : providedEnvs.set(effect, yield* provider).get(effect)!
-      const value = yield* pipe(effect, useSome(provided)) as Effect<E3, A>
-
-      return value
+      return yield* pipe(effect, useSome(yield* provider))
     })
 }
