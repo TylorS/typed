30c65b28feaa8a47444224022a333f650bb21ce3
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-11-08 15:00:25 -0500

    feat: separate RafEnv

--- src/dom/provideRafEnv.ts
+++ src/dom/provideRafEnv.ts
@@ -2,7 +2,9 @@ import { Provider, provideSome } from '@typed/fp/Effect/exports'
 
 import { RafEnv } from './raf'
 
-export const provideRafEnv: Provider<RafEnv> = provideSome({
+export const rafEnv: RafEnv = {
   requestAnimationFrame: (cb: FrameRequestCallback) => window.requestAnimationFrame(cb),
   cancelAnimationFrame: (n: number) => window.cancelAnimationFrame(n),
-})
+}
+
+export const provideRafEnv: Provider<RafEnv> = provideSome(rafEnv)
