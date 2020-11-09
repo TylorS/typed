0ab2ccd588b1e12b9ff5c1151d8e51c4e2f63706
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-11-09 13:19:38 -0500

    feat: encode/decode options

--- src/routing/createPath.ts
+++ src/routing/createPath.ts
@@ -2,8 +2,8 @@ import * as P2R from 'path-to-regexp'
 
 import { Route, RouteParts, ValuesOf } from './Route'
 
-export function createPath<A extends Route<RouteParts>>(route: A) {
-  const compile = P2R.compile(route.path)
+export function createPath<A extends Route<RouteParts>>(route: A, encode = encodeURIComponent) {
+  const compile = P2R.compile(route.path, { encode })
 
   return (values: ValuesOf<A>) => compile(values as object)
 }
--- src/routing/matchRoute.ts
+++ src/routing/matchRoute.ts
@@ -4,8 +4,8 @@ import * as P2R from 'path-to-regexp'
 import { createRoute, Route, RouteParts, ValuesOf } from './Route'
 import { RouteParam } from './RouteParam'
 
-export const matchRoute = <A extends Route<RouteParts>>(route: A) => {
-  const match = P2R.match(route.path)
+export const matchRoute = <A extends Route<RouteParts>>(route: A, decode = decodeURIComponent) => {
+  const match = P2R.match(route.path, { decode, end: route.path === '/' })
 
   return (path: string) => {
     const values = match(path)
