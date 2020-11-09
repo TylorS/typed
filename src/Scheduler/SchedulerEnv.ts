cb13dd8d1405b3271e1468c837e3cdcec0548b56
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-11-09 17:57:12 -0500

    feat: move VirtualTimer into Scheduler

---
+++ src/Scheduler/SchedulerEnv.ts
@@ -0,0 +1,42 @@
+import { currentTime, delay as delayScheduler, newDefaultScheduler } from '@most/scheduler'
+import { Scheduler, Time } from '@most/types'
+import { Effect, fromEnv, Provider, provideWith, Pure } from '@typed/fp/Effect/exports'
+import { async } from '@typed/fp/Resume/exports'
+import { flow } from 'fp-ts/function'
+import { IO } from 'fp-ts/IO'
+
+import { createCallbackTask } from './createCallbackTask'
+
+export interface SchedulerEnv {
+  readonly scheduler: Scheduler
+}
+
+export const provideSchedulerEnv: Provider<SchedulerEnv> = provideWith(
+  Pure.fromIO(
+    (): SchedulerEnv => ({
+      scheduler: newDefaultScheduler(),
+    }),
+  ),
+)
+
+/**
+ * Add a delay at the specified about of time
+ */
+export const delay = (delay: Time): Effect<SchedulerEnv, Time> =>
+  fromEnv(({ scheduler }: SchedulerEnv) =>
+    async((cb) =>
+      delayScheduler(
+        delay,
+        createCallbackTask(() => cb(currentTime(scheduler))),
+        scheduler,
+      ),
+    ),
+  )
+
+/**
+ * Run an IO asynchronously
+ */
+export const asyncIO = <A>(io: IO<A>): Effect<SchedulerEnv, A> =>
+  fromEnv(({ scheduler }: SchedulerEnv) =>
+    async((cb) => delayScheduler(0, createCallbackTask(flow(io, cb)), scheduler)),
+  )
---
+++ src/Scheduler/createCallbackTask.ts
@@ -0,0 +1,28 @@
+import { Task } from '@most/types'
+import { Disposable, lazy } from '@typed/fp/Disposable/exports'
+import { IO } from 'fp-ts/IO'
+
+/**
+ * Convert an IO<Disposable> into a Most.js Task
+ */
+export function createCallbackTask(cb: IO<Disposable>, onError?: (error: Error) => void): Task {
+  const disposable = lazy()
+
+  return {
+    run() {
+      if (!disposable.disposed) {
+        disposable.addDisposable(cb())
+      }
+    },
+    error(_, e) {
+      disposable.dispose()
+
+      if (onError) {
+        return onError(e)
+      }
+
+      throw e
+    },
+    dispose: disposable.dispose,
+  }
+}
