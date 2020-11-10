da0b608e3b66123f0d992672206e19305376ea67
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-11-09 20:48:17 -0500

    feat: provide a new namespace

--- src/Patch/patchOnRaf.ts
+++ src/Patch/patchOnRaf.ts
@@ -1,8 +1,9 @@
 import { raf, RafEnv } from '@typed/fp/dom/raf'
-import { doEffect, Effect } from '@typed/fp/Effect/exports'
+import { doEffect, Effect, forever } from '@typed/fp/Effect/exports'
 import { SchedulerEnv } from '@typed/fp/scheduler/exports'
-import { getCurrentNamespace, getShared, SharedEnv } from '@typed/fp/Shared/core/exports'
+import { getShared, Namespace, runWithNamespace, SharedEnv } from '@typed/fp/Shared/core/exports'
 import { HasBeenUpdated } from '@typed/fp/Shared/memoNamespace/exports'
+import { pipe } from 'fp-ts/lib/function'
 
 import { Patch, patch } from './Patch'
 
@@ -11,23 +12,26 @@ export type PatchOnRafEnv<A, B> = SchedulerEnv & SharedEnv & RafEnv & Patch<A, B
 /**
  * Keep the root of an Application properly Patched
  */
-export function patchOnRaf<A, E, B>(
+export function patchOnRaf<A, E extends SharedEnv, B>(
   initial: A,
   main: Effect<E, B>,
 ): Effect<E & PatchOnRafEnv<A, B>, never> {
   const eff = doEffect(function* () {
-    const namespace = yield* getCurrentNamespace
+    const namespace = Namespace.wrap(Symbol('PatchOnRaf :: Main'))
     const hasBeenUpdated = yield* getShared(HasBeenUpdated(namespace))
+    const runMain = pipe(main, runWithNamespace(namespace))
 
-    let previous = yield* patch(initial, yield* main)
+    let previous = yield* patch(initial, yield* runMain)
 
-    while (true) {
-      yield* raf
+    return yield* forever(
+      doEffect(function* () {
+        yield* raf
 
-      if (hasBeenUpdated.current) {
-        previous = yield* patch(previous, yield* main)
-      }
-    }
+        if (hasBeenUpdated.current) {
+          previous = yield* patch(previous, yield* runMain)
+        }
+      }),
+    )
   })
 
   return eff
