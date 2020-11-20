455920ab0dc1d7eb53f2ba7960314b07dbb04eb6
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-11-20 16:35:40 -0500

    docs: add code comment for provideSharedEnv

--- src/Shared/createSharedEnvProvider/provideSharedEnv.ts
+++ src/Shared/createSharedEnvProvider/provideSharedEnv.ts
@@ -6,5 +6,15 @@ import { SharedEnv } from '../core/exports'
 import { createSharedEnvProvider } from './createSharedEnvProvider'
 import { defaultHandlers } from './defaultHandlers'
 
+/**
+ * This is for running an application defined using an Effect requiring a SharedEnv, using
+ * @most/schedulers default scheduler andour "defaultHandlers" which powers the core functionality,
+ * a react-hooks-like functionality, and areact-context-like functionality through the use of listening
+ * to and responding to SharedEvent occurences. The default namespace for this root-level effect will be
+ * that of our GlobalNamespace.
+ *
+ * You can use createSharedEnvProvider and provide your own handlers to replace or add any and all functionality
+ * as you see fit. It can be convenient to always use GlobalNamespace to have an easy way to reach up into the root.
+ */
 export const provideSharedEnv: Provider<SharedEnv> = (effect) =>
   pipe(effect, createSharedEnvProvider({ handlers: defaultHandlers }), provideSchedulerEnv)
