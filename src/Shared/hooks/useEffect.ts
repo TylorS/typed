80e1e3d88cfee54f7ab38bc56e69707255ab64cb
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-11-21 20:48:36 -0500

    feat: useOneOf
    useOneOf is capable of matching inputs and keeping that value in a Shared value.

--- src/Shared/hooks/useEffect.ts
+++ src/Shared/hooks/useEffect.ts
@@ -1,16 +1,12 @@
-import { disposeAll, disposeNone } from '@most/disposable'
 import { Disposable } from '@most/types'
-import { Effect, Pure } from '@typed/fp/Effect/Effect'
-import { ask, chain, doEffect, execPure, provideAll } from '@typed/fp/Effect/exports'
+import { Effect } from '@typed/fp/Effect/Effect'
+import { askFor, chain, doEffect, execPure } from '@typed/fp/Effect/exports'
 import { delay, SchedulerEnv } from '@typed/fp/Scheduler/exports'
-import { defaultEqs, EqsOf, tupleEqOf } from '@typed/fp/Shared/common/EqsOf'
+import { defaultEqs, EqsOf } from '@typed/fp/Shared/common/EqsOf'
 import { SharedEnv } from '@typed/fp/Shared/core/exports'
 import { pipe } from 'fp-ts/function'
 
-import { addDisposable } from '../core/disposables/exports'
-import { Ref } from '../Ref/exports'
-import { useDepChange } from './useDepChange'
-import { useRef } from './useRef'
+import { useDisposable } from './useDisposable'
 
 /**
  * Schedule to perform an Effect everytime the dependencies change,
@@ -22,24 +18,14 @@ export const useEffect = <E, A, Deps extends ReadonlyArray<unknown> = ReadonlyAr
   eqs: EqsOf<Deps> = defaultEqs(deps),
 ): Effect<SharedEnv & E & SchedulerEnv, Disposable> => {
   const eff = doEffect(function* () {
-    const depsChanged = yield* useDepChange(deps, tupleEqOf(eqs), true)
-    const ref: Ref<Disposable> = yield* useRef(Pure.fromIO(disposeNone))
-
-    if (depsChanged) {
-      ref.current.dispose()
-
-      const env = yield* ask<E & SchedulerEnv>()
-      const disposable = pipe(
+    const pure = yield* askFor(
+      pipe(
         delay(0),
         chain(() => effect),
-        provideAll(env),
-        execPure,
-      )
+      ),
+    )
 
-      ref.current = disposeAll([disposable, yield* addDisposable(disposable)])
-    }
-
-    return ref.current
+    return yield* useDisposable(() => execPure(pure), deps, eqs)
   })
 
   return eff
---
+++ src/Shared/hooks/useOneOf.ts
@@ -0,0 +1,27 @@
+import { doEffect, Effect } from '@typed/fp/Effect/exports'
+import { Match, oneOf } from '@typed/fp/logic/exports'
+import { isSome } from 'fp-ts/Option'
+
+import { Shared, SharedEnv, SharedKey } from '../core/exports'
+import { getSharedState } from '../State/exports'
+import { useMemo } from './useMemo'
+
+export const useOneOf = <E, O, I, A extends ReadonlyArray<Match<I, O>>>(
+  shared: Shared<SharedKey, E, O>,
+  input: I,
+  ...matches: A
+): Effect<SharedEnv & E, O> => {
+  const eff = doEffect(function* () {
+    const [getOutput, setOutput] = yield* getSharedState(shared)
+    const match = yield* useMemo(() => oneOf(matches), [matches])
+    const matched = yield* useMemo(() => match(input), [input, match] as const)
+
+    if (isSome(matched) && !shared.eq.equals(matched.value, getOutput())) {
+      setOutput(matched.value)
+    }
+
+    return getOutput()
+  })
+
+  return eff
+}
