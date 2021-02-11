9e8674336e28dbfa0526513c9a16c55aadae3310
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-02-11 18:12:56 -0500

    test: add a basic test for IOFx

---
+++ src/IO/IOFx.test.ts
@@ -0,0 +1,18 @@
+import { describe, it } from '@typed/test'
+import * as IO from 'fp-ts/dist/IO'
+
+import * as IOFx from './'
+import { doIO, toIO } from './IoFx'
+
+export const test = describe(`IOFx`, [
+  it(`provides do-notation for IO`, ({ equal }) => {
+    const fx = doIO(function* (liftIO) {
+      const [x, y] = yield* IOFx.zip([liftIO(IO.of(1)), IOFx.of(2)])
+
+      return x + y
+    })
+    const io = toIO(fx)
+
+    equal(3, io())
+  }),
+])
