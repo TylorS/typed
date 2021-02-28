6602464a87d279561d9be80da26ea6ef184cde5d
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-02-28 16:14:47 -0500

    fix: incorrect signature

--- src/Ask/asks.ts
+++ src/Ask/asks.ts
@@ -1,5 +1,5 @@
 import { pipe } from 'fp-ts/dist/function'
-import { Functor, Functor1, Functor2C, Functor3 } from 'fp-ts/dist/Functor'
+import { Functor, Functor1, Functor2C, Functor3, Functor3C } from 'fp-ts/dist/Functor'
 import { HKT, Kind, Kind2, Kind3, URIS, URIS2, URIS3 } from 'fp-ts/dist/HKT'
 
 import { Ask, Ask1, Ask2, Ask3, Ask3C } from './Ask'
@@ -16,9 +16,9 @@ export function asks<F extends URIS3>(
   M: Ask3<F> & Functor3<F>,
 ): <R, A, B>(f: (asked: A) => B) => Kind3<F, R, A, B>
 
-export function asks<F extends URIS3, A>(
-  M: Ask3C<F, A> & Functor3<F>,
-): <E, B>(f: (asked: A) => B) => Kind3<F, A, E, B>
+export function asks<F extends URIS3, E>(
+  M: Ask3C<F, E> & Functor3C<F, E>,
+): <A, B>(f: (asked: A) => B) => Kind3<F, A, E, B>
 
 export function asks<F>(M: Ask<F> & Functor<F>): <A, B>(f: (asked: A) => B) => HKT<F, B>
 
