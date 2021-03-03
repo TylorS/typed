17289661b72d62099b13a32f11bd52fb39c4b400
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-03-03 17:39:09 -0500

    refactor: split up environments

--- src/runtime/RuntimeEnv.ts
+++ src/runtime/RuntimeEnv.ts
@@ -4,11 +4,23 @@ import { CurrentNamespace, Namespace } from '@typed/fp/Namespace'
 import { GlobalNamespace } from './global'
 import { EffectOf, SharedEvent } from './SharedEvent'
 
-export interface RuntimeEnv<F> extends CurrentNamespace {
+export interface RuntimeEnv<F>
+  extends CurrentNamespace,
+    SharedEventsEnv<F>,
+    SharedKeyStoreEnv,
+    SharedEffectsEnv<F> {}
+
+export interface SharedEventsEnv<F> {
   // Send an receive runtime lifecycle events
   readonly sharedEvents: Adapter<SharedEvent<F>, SharedEvent<F>>
+}
+
+export interface SharedKeyStoreEnv {
   // A *mutable* map of Namespaces to key-value pairs
   readonly sharedKeyStore: Map<Namespace, Map<unknown, unknown>>
+}
+
+export interface SharedEffectsEnv<F> {
   // A *mutable* map of Namespace to the effect associated with running within it.
   readonly sharedEffects: Map<Namespace, EffectOf<F>>
 }
