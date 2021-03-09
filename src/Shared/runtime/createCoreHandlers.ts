ebe1d86757f65d0ea967063eb6fe429a96e72ecb
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-03-08 19:00:15 -0500

    feat: track effects in core handlers

--- src/Shared/runtime/createCoreHandlers.ts
+++ src/Shared/runtime/createCoreHandlers.ts
@@ -1,40 +1,70 @@
 import { MonadAsk, MonadAsk2, MonadAsk3, MonadAsk4 } from '@typed/fp/MonadAsk'
 import { pipe } from 'fp-ts/dist/function'
 import { URIS2, URIS3, URIS4 } from 'fp-ts/dist/HKT'
+import { none, some } from 'fp-ts/dist/Option'
+import { filterMapWithIndex } from 'fp-ts/dist/ReadonlyArray'
 
 import { RuntimeHandler } from './listenToEvents'
 import { RuntimeEnv } from './RuntimeEnv'
-import { EffectOf, NamespaceDeleted, SharedEvent } from './SharedEvent'
+import { EffectOf, NamespaceDeleted, NamespaceStarted, SharedEvent } from './SharedEvent'
 
-export function createCoreHandlers<F extends URIS2>(
-  M: MonadAsk2<F>,
-): readonly [RuntimeHandler<F, NamespaceDeleted>]
+export type CoreHandlers<F> = readonly [
+  RuntimeHandler<F, NamespaceDeleted>,
+  RuntimeHandler<F, NamespaceStarted<F>>,
+]
 
-export function createCoreHandlers<F extends URIS3>(
-  M: MonadAsk3<F>,
-): readonly [RuntimeHandler<F, NamespaceDeleted>]
+const isNamespaceDeleted = <F>(event: SharedEvent<F>): event is NamespaceDeleted =>
+  event.type === 'namespace/deleted'
 
-export function createCoreHandlers<F extends URIS4>(
-  M: MonadAsk4<F>,
-): readonly [RuntimeHandler<F, NamespaceDeleted>]
+const isNamespaceStarted = <F>(event: SharedEvent<F>): event is NamespaceStarted<F> =>
+  event.type === 'namespace/started'
 
-export function createCoreHandlers<F>(
-  M: MonadAsk<F>,
-): readonly [RuntimeHandler<F, NamespaceDeleted>]
+export function createCoreHandlers<F extends URIS2>(M: MonadAsk2<F>): CoreHandlers<F>
 
-export function createCoreHandlers<F>(M: MonadAsk<F>) {
+export function createCoreHandlers<F extends URIS3>(M: MonadAsk3<F>): CoreHandlers<F>
+
+export function createCoreHandlers<F extends URIS4>(M: MonadAsk4<F>): CoreHandlers<F>
+
+export function createCoreHandlers<F>(M: MonadAsk<F>): CoreHandlers<F>
+
+export function createCoreHandlers<F>(M: MonadAsk<F>): CoreHandlers<F> {
   // Ensure resources are cleaned up after deleting a namespace
   const onDeleteNamespace: RuntimeHandler<F, NamespaceDeleted> = {
-    guard: (event: SharedEvent<F>): event is NamespaceDeleted => event.type === 'namespace/deleted',
+    guard: isNamespaceDeleted,
     handler: (event) =>
       pipe(
         M.ask<RuntimeEnv<F>>(),
         M.map((env) => {
           env.sharedEffects.delete(event.namespace)
           env.sharedKeyStore.delete(event.namespace)
+
+          // Delete queued effects
+          const indexesToRemove = pipe(
+            env.queuedEffects,
+            filterMapWithIndex((i, [namespace]) =>
+              namespace === event.namespace ? some(i) : none,
+            ),
+          )
+
+          let offset = 0
+          for (const index of indexesToRemove) {
+            env.queuedEffects.splice(index + offset, 1)
+            offset++
+          }
         }),
       ) as EffectOf<F, RuntimeEnv<F>>,
   }
 
-  return [onDeleteNamespace] as const
+  const onNamespaceStarted: RuntimeHandler<F, NamespaceStarted<F>> = {
+    guard: isNamespaceStarted,
+    handler: (event) =>
+      pipe(
+        M.ask<RuntimeEnv<F>>(),
+        M.map((env) => {
+          env.sharedEffects.set(event.namespace, event.effect)
+        }),
+      ) as EffectOf<F, RuntimeEnv<F>>,
+  }
+
+  return [onDeleteNamespace, onNamespaceStarted] as const
 }
--- src/hooks/createHooksHandlers.ts
+++ src/hooks/createHooksHandlers.ts
@@ -8,7 +8,6 @@ import {
   NamespaceStarted,
   RuntimeEnv,
   RuntimeHandler,
-  SharedEvent,
 } from '@typed/fp/Shared'
 import { FromIO, FromIO2, FromIO3, FromIO4 } from 'fp-ts/dist/FromIO'
 import { pipe } from 'fp-ts/dist/function'
@@ -16,12 +15,6 @@ import { HKT, URIS2, URIS3, URIS4 } from 'fp-ts/dist/HKT'
 
 import { createResetIndex } from './createGetNextIndex'
 import { NAMESPACE_DISPOSABLE } from './NamespaceDisposable'
-
-const isDeleteNamespace = <F>(event: SharedEvent<F>): event is NamespaceDeleted =>
-  event.type === 'namespace/deleted'
-
-const isNamespaceStarted = <F>(event: SharedEvent<F>): event is NamespaceStarted<F> =>
-  event.type === 'namespace/started'
 
 export type HooksHandlers<F> = readonly [
   RuntimeHandler<F, NamespaceDeleted>,
@@ -43,13 +36,13 @@ export function createHooksHandlers<F extends URIS4>(
 export function createHooksHandlers<F>(M: MonadAsk<F> & FromIO<F> & UseSome<F>): HooksHandlers<F>
 
 export function createHooksHandlers<F>(M: MonadAsk<F> & FromIO<F> & UseSome<F>): HooksHandlers<F> {
-  const [coreDeleteNamespace] = createCoreHandlers<F>(M)
+  const [coreDeleteNamespace, coreNamespaceStarted] = createCoreHandlers<F>(M)
   const resetIndex = createResetIndex<F>(M)()
   const using = usingNamespace(M)
 
   // Ensure resources are cleaned up after deleting a namespace
   const onDeleteNamespace: RuntimeHandler<F, NamespaceDeleted> = {
-    guard: isDeleteNamespace,
+    ...coreDeleteNamespace,
     handler: (event) =>
       pipe(
         M.ask<RuntimeEnv<F>>(),
@@ -63,8 +56,12 @@ export function createHooksHandlers<F>(M: MonadAsk<F> & FromIO<F> & UseSome<F>):
   }
 
   const onNamespaceStart: RuntimeHandler<F, NamespaceStarted<F>> = {
-    guard: isNamespaceStarted,
-    handler: (event) => pipe(resetIndex, using(event.namespace)) as EffectOf<F, RuntimeEnv<F>>,
+    ...coreNamespaceStarted,
+    handler: (event) =>
+      pipe(
+        coreNamespaceStarted.handler(event) as HKT<F, void>,
+        M.chain(() => pipe(resetIndex, using(event.namespace))),
+      ) as EffectOf<F, RuntimeEnv<F>>,
   }
 
   return [onDeleteNamespace, onNamespaceStart] as const
