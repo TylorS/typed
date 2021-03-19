17c2a1e7eadad4f6e55c863bae8fe29d3eee297d
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-03-18 23:21:33 -0400

    chore: remove build files

--- MonadReader/ask.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ask = void 0;
-const Reader_1 = require("fp-ts/dist/Reader");
-function ask(M) {
-    return () => M.fromReader(Reader_1.ask());
-}
-exports.ask = ask;
-//# sourceMappingURL=ask.js.map
--- MonadReader/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./ask";
-export * from "./MonadReader";
-//# sourceMappingURL=index.d.ts.map
