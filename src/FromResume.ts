e250b90d8d4e62aec7282f3f1fc0de097facd03b
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-03-29 20:10:17 -0400

    feat: FromResume Kliesli arrows

--- src/FromResume.ts
+++ src/FromResume.ts
@@ -1,6 +1,8 @@
 import { Hkt } from './Hkt'
 import { Resume } from './Resume'
-import { URIS, URIS2, URIS3, URIS4 } from 'fp-ts/HKT'
+import { HKT, URIS, URIS2, URIS3, URIS4 } from 'fp-ts/HKT'
+import { Chain, Chain1, Chain2, Chain3, Chain4, chainFirst } from 'fp-ts/Chain'
+import { flow } from 'fp-ts/function'
 
 export type FromResume<F> = {
   readonly fromResume: <A>(resume: Resume<A>) => Hkt<F, [A]>
@@ -30,4 +32,103 @@ export type FromResume4<F extends URIS4> = {
   readonly fromResume: <A, S = unknown, R = unknown, E = never>(
     resume: Resume<A>,
   ) => Hkt<F, [S, R, E, A]>
+}
+
+export function fromResumeK<F extends URIS>(
+  F: FromResume1<F>,
+): <A extends readonly any[], B>(f: (...args: A) => Resume<B>) => (...args: A) => Hkt<F, [B]>
+
+export function fromResumeK<F extends URIS2>(
+  F: FromResume2<F>,
+): <A extends readonly any[], B>(f: (...args: A) => Resume<B>) => <E>(...args: A) => Hkt<F, [E, B]>
+
+export function fromResumeK<F extends URIS3>(
+  F: FromResume3<F>,
+): <A extends readonly any[], B>(
+  f: (...args: A) => Resume<B>,
+) => <R, E>(...args: A) => Hkt<F, [R, E, B]>
+
+export function fromResumeK<F extends URIS4>(
+  F: FromResume4<F>,
+): <A extends readonly any[], B>(
+  f: (...args: A) => Resume<B>,
+) => <S, R, E>(...args: A) => Hkt<F, [S, R, E, B]>
+
+export function fromResumeK<F>(
+  F: FromResume<F>,
+): <A extends readonly any[], B>(f: (...args: A) => Resume<B>) => (...args: A) => Hkt<F, [B]>
+
+export function fromResumeK<F>(F: FromResume<F>) {
+  return <A extends readonly any[], B>(f: (...args: A) => Resume<B>) => (...args: A): Hkt<F, [B]> =>
+    F.fromResume(f(...args))
+}
+
+export function chainResumeK<F extends URIS>(
+  F: FromResume1<F>,
+  C: Chain1<F>,
+): <A, B>(f: (value: A) => Resume<B>) => (hkt: Hkt<F, [A]>) => Hkt<F, [B]>
+
+export function chainResumeK<F extends URIS2>(
+  F: FromResume2<F>,
+  C: Chain2<F>,
+): <A, B>(f: (value: A) => Resume<B>) => <E>(hkt: Hkt<F, [E, A]>) => Hkt<F, [E, B]>
+
+export function chainResumeK<F extends URIS3>(
+  F: FromResume3<F>,
+  C: Chain3<F>,
+): <A, B>(f: (value: A) => Resume<B>) => <R, E>(hkt: Hkt<F, [R, E, A]>) => Hkt<F, [R, E, B]>
+
+export function chainResumeK<F extends URIS4>(
+  F: FromResume4<F>,
+  C: Chain4<F>,
+): <A, B>(
+  f: (value: A) => Resume<B>,
+) => <S, R, E>(hkt: Hkt<F, [S, R, E, A]>) => Hkt<F, [S, R, E, B]>
+
+export function chainResumeK<F>(
+  F: FromResume<F>,
+  C: Chain<F>,
+): <A, B>(f: (value: A) => Resume<B>) => (hkt: Hkt<F, [A]>) => Hkt<F, [B]>
+
+export function chainResumeK<F>(
+  F: FromResume<F>,
+  C: Chain<F>,
+): <A, B>(f: (value: A) => Resume<B>) => (hkt: HKT<F, A>) => HKT<F, B> {
+  return (f) => C.chain(flow(f, F.fromResume) as any)
+}
+
+export function chainFirstResumeK<F extends URIS>(
+  F: FromResume1<F>,
+  C: Chain1<F>,
+): <A, B>(f: (value: A) => Resume<B>) => (hkt: Hkt<F, [A]>) => Hkt<F, [A]>
+
+export function chainFirstResumeK<F extends URIS2>(
+  F: FromResume2<F>,
+  C: Chain2<F>,
+): <A, B>(f: (value: A) => Resume<B>) => <E>(hkt: Hkt<F, [E, A]>) => Hkt<F, [E, A]>
+
+export function chainFirstResumeK<F extends URIS3>(
+  F: FromResume3<F>,
+  C: Chain3<F>,
+): <A, B>(f: (value: A) => Resume<B>) => <R, E>(hkt: Hkt<F, [R, E, A]>) => Hkt<F, [R, E, A]>
+
+export function chainFirstResumeK<F extends URIS4>(
+  F: FromResume4<F>,
+  C: Chain4<F>,
+): <A, B>(
+  f: (value: A) => Resume<B>,
+) => <S, R, E>(hkt: Hkt<F, [S, R, E, A]>) => Hkt<F, [S, R, E, A]>
+
+export function chainFirstResumeK<F>(
+  F: FromResume<F>,
+  C: Chain<F>,
+): <A, B>(f: (value: A) => Resume<B>) => (hkt: Hkt<F, [A]>) => Hkt<F, [A]>
+
+export function chainFirstResumeK<F>(
+  F: FromResume<F>,
+  C: Chain<F>,
+): <A, B>(f: (value: A) => Resume<B>) => (hkt: HKT<F, A>) => HKT<F, A> {
+  const chainF = chainFirst(C)
+
+  return (f) => chainF(flow(f, F.fromResume) as any)
 }
