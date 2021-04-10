0c64e1ee49b0b92207e6d53e134318679d510774
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-04-10 15:43:47 -0400

    fix: recursively abort all children

--- src/Fiber/runtime/internal/abort.ts
+++ src/Fiber/runtime/internal/abort.ts
@@ -1,10 +1,12 @@
 import { doEnv, toEnv } from '@fp/Fx/Env'
+import { zip } from '@fp/Resume'
 import { disposeBoth } from '@most/disposable'
 import { Disposable } from '@most/types'
 import { pipe } from 'fp-ts/function'
 
 import { Fiber } from '../../Fiber'
 import { Status } from '../../Status'
+import { getFiberChildren } from '../FiberChildren'
 import { FiberDisposable } from '../FiberDisposable'
 import { setFiberStatus } from '../FiberStatus'
 import { sendStatus } from './FiberSendEvent'
@@ -15,6 +17,12 @@ export function abort<A>(fiber: Fiber<A>, disposable: Disposable) {
     // Check for any registered finalizers which should run before changing status to aborted
     // This should run first to allow for a finalizer to never finish to make this fiber "uncancellable"
     yield* _(finalize(fiber, true))
+
+    const children = yield* _(getFiberChildren)
+    const fibers = Array.from(children.values())
+
+    // Abort all the child fibers
+    yield* _(() => zip(fibers.map((f) => f.abort)))
 
     // Cancel all the synchronously cancellable resources
     disposeBoth(yield* _(fiber.refs.getRef(FiberDisposable)), disposable).dispose()
