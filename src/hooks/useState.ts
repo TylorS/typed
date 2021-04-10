75eb770a6c0d50c7c5980f203784dc14717573c7
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-04-10 02:45:10 -0400

    feat: just set the value

--- src/hooks/useState.ts
+++ src/hooks/useState.ts
@@ -1,6 +1,6 @@
-import { ask, chain, Env, fromIO, useAll } from '@fp/Env'
-import { neverEqualsEq } from '@fp/Eq'
-import { Arity1, constant, pipe } from '@fp/function'
+import { ask, Env, fromIO, useAll } from '@fp/Env'
+import { deepEqualsEq } from '@fp/Eq'
+import { constant, pipe } from '@fp/function'
 import { Do } from '@fp/Fx/Env'
 import { createRef, getRef, Ref, Refs, setRef } from '@fp/Ref'
 import { Eq } from 'fp-ts/Eq'
@@ -8,25 +8,19 @@ import { Eq } from 'fp-ts/Eq'
 import { getNextSymbol } from './internal/HookSymbols'
 import { useMemo } from './useMemo'
 
-export const useState = <E, A>(initial: Env<E, A>, eq: Eq<A> = neverEqualsEq) =>
+export const useState = <E, A>(initial: Env<E, A>, eq: Eq<A> = deepEqualsEq) =>
   Do(function* (_) {
     const symbol = yield* _(getNextSymbol)
     const ref = createRef(initial, symbol, eq)
     const value = yield* _(getRef(ref))
     const env = yield* _(ask<E & Refs>())
-    const updateValue = yield* pipe(ref, modifyRef(env), constant, fromIO, useMemo, _)
+    const setValue = yield* pipe(ref, setRefWith(env), constant, fromIO, useMemo, _)
 
-    return [value, updateValue] as const
+    return [value, setValue] as const
   })
 
 // Takes in the environemnt just in case this is called in another environment
 // where the reference has been passed along
-function modifyRef<E>(env: E & Refs) {
-  return <A>(ref: Ref<E, A>) => (f: Arity1<A, A>) =>
-    pipe(
-      ref,
-      getRef,
-      chain((a) => pipe(ref, pipe(a, f, setRef))),
-      useAll(env),
-    )
+function setRefWith<E>(env: E & Refs) {
+  return <A>(ref: Ref<E, A>) => (value: A): Env<unknown, A> => pipe(ref, setRef(value), useAll(env))
 }
