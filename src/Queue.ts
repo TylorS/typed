b4b34a4d6f9a9d97829bc43586296fb18b9dcf05
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-04-12 00:55:35 -0400

    feat: make Queue using Resume

--- src/Queue.ts
+++ src/Queue.ts
@@ -1,48 +1,50 @@
-import { IO } from 'fp-ts/IO'
+import * as R from '@fp/Resume'
 import { none, Option, some } from 'fp-ts/Option'
 
 /**
  * A synchronous, likely in-memory, representation of a Queue.
  */
 export interface Queue<A> {
-  readonly enqueue: (...values: ReadonlyArray<A>) => void
-  readonly dequeue: IO<Option<A>>
-  readonly dequeueAll: IO<ReadonlyArray<A>>
-  readonly peek: IO<Option<A>>
-
-  readonly some: (f: (value: A) => boolean) => boolean
-  readonly modify: (f: (queue: ReadonlyArray<A>) => ReadonlyArray<A>) => ReadonlyArray<A>
+  readonly enqueue: (...values: ReadonlyArray<A>) => R.Resume<void>
+  readonly dequeue: R.Resume<Option<A>>
+  readonly dequeueAll: R.Resume<ReadonlyArray<A>>
+  readonly peek: R.Resume<Option<A>>
+  readonly some: (f: (value: A) => boolean) => R.Resume<boolean>
+  readonly modify: (f: (queue: ReadonlyArray<A>) => ReadonlyArray<A>) => R.Resume<ReadonlyArray<A>>
 }
 
+/**
+ * Creates a synchronous in-memory queue
+ */
 export function createQueue<A>(initial: ReadonlyArray<A> = []): Queue<A> {
   let queue = [...initial]
 
   function enqueue(...values: ReadonlyArray<A>) {
-    queue.push(...values)
+    return R.sync(() => queue.push(...values))
   }
 
-  function dequeue() {
-    return queue.length > 0 ? some(queue.shift()!) : none
-  }
+  const dequeue = R.sync<Option<A>>(() => (queue.length > 0 ? some(queue.shift()!) : none))
 
-  function dequeueAll() {
+  const dequeueAll = R.sync(() => {
     const q = queue.slice()
 
     queue = []
 
     return q
-  }
+  })
 
-  function peek() {
+  const peek = R.sync(() => {
     return queue.length > 0 ? some(queue[0]) : none
-  }
+  })
 
   function modify(f: (array: ReadonlyArray<A>) => ReadonlyArray<A>) {
-    const updated = f(queue)
+    return R.sync(() => {
+      const updated = f(queue)
 
-    queue = updated.slice()
+      queue = updated.slice()
 
-    return updated
+      return updated
+    })
   }
 
   return {
@@ -51,39 +53,42 @@ export function createQueue<A>(initial: ReadonlyArray<A> = []): Queue<A> {
     dequeueAll,
     peek,
     modify,
-    some: (f) => queue.some(f),
+    some: (f) => R.sync(() => queue.some(f)),
   }
 }
 
+/**
+ * Creates a synchronous in-memory stack
+ */
 export function createStack<A>(initial: ReadonlyArray<A> = []): Queue<A> {
   let queue = [...initial]
 
   function enqueue(...values: ReadonlyArray<A>) {
-    queue.unshift(...values)
+    return R.sync(() => queue.unshift(...values))
   }
 
-  function dequeue() {
-    return queue.length > 0 ? some(queue.shift()!) : none
-  }
+  const dequeue = R.sync<Option<A>>(() => (queue.length > 0 ? some(queue.shift()!) : none))
 
-  function dequeueAll() {
+  const dequeueAll = R.sync(() => {
     const q = queue.slice()
 
     queue = []
 
     return q
-  }
+  })
 
-  function peek() {
+  const peek = R.sync(() => {
     return queue.length > 0 ? some(queue[0]) : none
-  }
+  })
 
   function modify(f: (array: ReadonlyArray<A>) => ReadonlyArray<A>) {
-    const updated = f(queue)
+    return R.sync(() => {
+      const updated = f(queue)
 
-    queue = updated.slice()
+      queue = updated.slice()
 
-    return updated
+      return updated
+    })
   }
 
   return {
@@ -92,6 +97,6 @@ export function createStack<A>(initial: ReadonlyArray<A> = []): Queue<A> {
     dequeueAll,
     peek,
     modify,
-    some: (f) => queue.some(f),
+    some: (f) => R.sync(() => queue.some(f)),
   }
 }
