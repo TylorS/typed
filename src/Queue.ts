1ebb1946a89fe1c2c9d5e89350e45097eec02e73
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-04-12 00:49:08 -0400

    feat: create LIFO queue (aka Stack)

--- src/Queue.ts
+++ src/Queue.ts
@@ -14,11 +14,52 @@ export interface Queue<A> {
   readonly modify: (f: (queue: ReadonlyArray<A>) => ReadonlyArray<A>) => ReadonlyArray<A>
 }
 
-export function createFifoQueue<A>(initial: ReadonlyArray<A> = []): Queue<A> {
+export function createQueue<A>(initial: ReadonlyArray<A> = []): Queue<A> {
   let queue = [...initial]
 
   function enqueue(...values: ReadonlyArray<A>) {
     queue.push(...values)
+  }
+
+  function dequeue() {
+    return queue.length > 0 ? some(queue.shift()!) : none
+  }
+
+  function dequeueAll() {
+    const q = queue.slice()
+
+    queue = []
+
+    return q
+  }
+
+  function peek() {
+    return queue.length > 0 ? some(queue[0]) : none
+  }
+
+  function modify(f: (array: ReadonlyArray<A>) => ReadonlyArray<A>) {
+    const updated = f(queue)
+
+    queue = updated.slice()
+
+    return updated
+  }
+
+  return {
+    enqueue,
+    dequeue,
+    dequeueAll,
+    peek,
+    modify,
+    some: (f) => queue.some(f),
+  }
+}
+
+export function createStack<A>(initial: ReadonlyArray<A> = []): Queue<A> {
+  let queue = [...initial]
+
+  function enqueue(...values: ReadonlyArray<A>) {
+    queue.unshift(...values)
   }
 
   function dequeue() {
