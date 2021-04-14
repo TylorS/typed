5d79e7d65c5368647d6f13497521bc90acfc5c9e
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-04-14 07:41:23 -0400

    chore: fix cleanup script add tsup for later

--- tools/cleanup.ts
+++ tools/cleanup.ts
@@ -1,8 +1,8 @@
-import { readdirSync, statSync } from 'fs'
+import { existsSync, readdirSync, statSync } from 'fs'
 import { basename, dirname, join } from 'path'
 import rimraf from 'rimraf'
 
-import { ROOT_DIR } from './common'
+import { MODULES, ROOT_DIR } from './common'
 
 rimraf.sync(join(ROOT_DIR, 'build'))
 rimraf.sync(join(ROOT_DIR, 'cjs'))
@@ -16,3 +16,7 @@ readdirSync(ROOT_DIR)
   .filter((files) => files.length === 1 && basename(files[0]) === 'package.json')
   .flat()
   .forEach((file) => rimraf.sync(dirname(file)))
+
+MODULES.map((m) => join(ROOT_DIR, m))
+  .filter((p) => existsSync(p) && statSync(p).isDirectory())
+  .forEach((dir) => rimraf.sync(dir))
