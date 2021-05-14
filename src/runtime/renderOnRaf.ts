6f12c6f9c4e7ba053d651f448ebbb3156d1dee79
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-05-14 15:34:25 -0400

    refactor: simplify rendering

--- src/runtime/renderOnRaf.ts
+++ src/runtime/renderOnRaf.ts
@@ -1,62 +1,51 @@
 import * as E from '@fp/Env'
 import * as F from '@fp/Fiber'
+import { Fiber } from '@fp/Fiber'
 import { pipe } from '@fp/function'
 import { Do } from '@fp/Fx/Env'
-import { resetIndex, useDisposable } from '@fp/hooks'
+import { resetIndex } from '@fp/hooks'
+import { useStream } from '@fp/hooks/useStream'
 import { patch } from '@fp/Patch'
 import { createReferences } from '@fp/Ref'
-import { SchedulerEnv } from '@fp/Scheduler'
-import { createSink } from '@fp/Stream'
 
 import { raf } from './raf'
 
 /**
- * Runs the provided effect anytime there are Ref updates and the browser has idle time to spare. It
- * will use the provided Patch instance to provide updates.
+ * Runs the provided effect anytime there are Ref updates, and will use the provided Patch instance
+ * to provide updates.
  */
-export const renderOnRaf = <E, A, B>(env: E.Env<E, A>, initial: B) =>
+export const renderWhenIdle = <E, A, B>(env: E.Env<E, A>, initial: B) =>
   Do(function* (_) {
-    const current = yield* _(F.getCurrentFiber)
     const refs = createReferences()
     const main = pipe(
-      // Always reset hooks index before running
       resetIndex,
       F.usingFiberRefs,
       E.chain(() => env),
     )
 
-    let fiber = yield* _(F.fork(main, { refs }))
-    let rendered = yield* _(F.join(fiber))
-    let patched = yield* _(patch(initial, rendered))
-    let currentStatus = yield* _(() => current.status)
-    let hasBeenUpdated = false
-
-    const { scheduler } = yield* _(E.ask<SchedulerEnv>())
-
     yield* _(
-      useDisposable(() =>
-        refs.events.run(
-          createSink({
-            event: (_, x) => {
-              if (x.type === 'updated' || x.type === 'deleted') {
-                hasBeenUpdated = true
-              }
-            },
-          }),
-          scheduler,
-        ),
-      ),
+      useStream(refs.events, {
+        event: (_, x) => {
+          if (x.type === 'updated' || x.type === 'deleted') {
+            hasBeenUpdated = true
+          }
+        },
+      }),
     )
 
-    while (!F.isTerminal(currentStatus)) {
-      yield* _(raf)
+    let fiber: Fiber<A>
+    let rendered: unknown
+    let patched = initial
+    let hasBeenUpdated = true
 
+    while (true) {
       if (hasBeenUpdated) {
         hasBeenUpdated = false
         fiber = yield* _(F.fork(main, { refs }))
         rendered = yield* _(F.join(fiber))
         patched = yield* _(patch(patched, rendered))
-        currentStatus = yield* _(() => current.status)
       }
+
+      yield* _(raf)
     }
   })
