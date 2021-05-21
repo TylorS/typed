7bb6d5fbd71d459b365cc79fb209860c5b01bcc9
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-05-21 19:15:36 -0400

    refactor: cleanup useContext

--- src/hooks/useContext.ts
+++ src/hooks/useContext.ts
@@ -1,7 +1,7 @@
 import * as E from '@fp/Env'
-import { CurrentFiber, Fiber, usingFiberRefs } from '@fp/Fiber'
+import * as F from '@fp/Fiber'
 import { Arity1, flow, pipe } from '@fp/function'
-import { deleteRef, getRef, hasRef, makeRef, modifyRef, Ref, setRef, WrappedRef } from '@fp/Ref'
+import * as R from '@fp/Ref'
 import { SchedulerEnv } from '@fp/Scheduler'
 
 import { findProvider } from './findProvider'
@@ -11,81 +11,70 @@ import { withProvider } from './withProvider'
 /**
  * Gets and Subscribes to future updates regarding a Reference
  */
-export const useContext = <E, A>(ref: Ref<E, A>) =>
+export const useContext = <E, A>(ref: R.Ref<E, A>) =>
   withProvider(ref, (currentFiber) =>
     pipe(
       useReplicateRefEvents(currentFiber, ref),
-      E.chain(() =>
-        pipe(
-          ref,
-          getRef,
-          usingFiberRefs,
-          E.useSome<CurrentFiber>({ currentFiber }),
-        ),
-      ),
+      E.chain(() => getRefUsingFiber(ref, currentFiber)),
     ),
   )
 
-export const getContext = <E, A>(ref: Ref<E, A>) =>
+export const getContext = <E, A>(ref: R.Ref<E, A>) =>
+  withProvider(ref, (currentFiber) => getRefUsingFiber(ref, currentFiber))
+
+const getRefUsingFiber = <E, A>(ref: R.Ref<E, A>, currentFiber: F.Fiber<unknown>) =>
+  pipe(ref, R.getRef, F.withFiberRefs(currentFiber))
+
+export const hasContext = <E, A>(ref: R.Ref<E, A>) =>
   withProvider(ref, (currentFiber) =>
     pipe(
       ref,
-      getRef,
-      usingFiberRefs,
-      E.useSome<CurrentFiber>({ currentFiber }),
+      R.hasRef,
+      F.usingFiberRefs,
+      E.useSome<F.CurrentFiber>({ currentFiber }),
     ),
   )
 
-export const hasContext = <E, A>(ref: Ref<E, A>) =>
-  withProvider(ref, (currentFiber) =>
-    pipe(
-      ref,
-      hasRef,
-      usingFiberRefs,
-      E.useSome<CurrentFiber>({ currentFiber }),
-    ),
-  )
-
-export const setContext = <E, A>(ref: Ref<E, A>) => (value: A) =>
+export const setContext = <E, A>(ref: R.Ref<E, A>) => (value: A) =>
   withProvider(ref, (currentFiber) =>
     pipe(
       value,
-      setRef(ref),
-      usingFiberRefs,
-      E.useSome<CurrentFiber>({ currentFiber }),
+      R.setRef(ref),
+      F.usingFiberRefs,
+      E.useSome<F.CurrentFiber>({ currentFiber }),
     ),
   )
 
-export const setContext_ = <A>(value: A) => <E>(ref: Ref<E, A>) => setContext(ref)(value)
+export const setContext_ = <A>(value: A) => <E>(ref: R.Ref<E, A>) => setContext(ref)(value)
 
-export const modifyContext = <E, A>(ref: Ref<E, A>) => (f: Arity1<A, A>) =>
+export const modifyContext = <E, A>(ref: R.Ref<E, A>) => (f: Arity1<A, A>) =>
   withProvider(ref, (currentFiber) =>
     pipe(
       f,
-      modifyRef(ref),
-      usingFiberRefs,
-      E.useSome<CurrentFiber>({ currentFiber }),
+      R.modifyRef(ref),
+      F.usingFiberRefs,
+      E.useSome<F.CurrentFiber>({ currentFiber }),
     ),
   )
 
-export const modifyContext_ = <A>(f: Arity1<A, A>) => <E>(ref: Ref<E, A>) => modifyContext(ref)(f)
+export const modifyContext_ = <A>(f: Arity1<A, A>) => <E>(ref: R.Ref<E, A>) => modifyContext(ref)(f)
 
-export const deleteContext = <E, A>(ref: Ref<E, A>) =>
+export const deleteContext = <E, A>(ref: R.Ref<E, A>) =>
   withProvider(ref, (currentFiber) =>
     pipe(
       ref,
-      deleteRef,
-      usingFiberRefs,
-      E.useSome<CurrentFiber>({ currentFiber }),
+      R.deleteRef,
+      F.usingFiberRefs,
+      E.useSome<F.CurrentFiber>({ currentFiber }),
     ),
   )
 
-export interface ContextRef<E, A> extends WrappedRef<CurrentFiber, E, A> {
-  readonly use: E.Env<E & CurrentFiber & SchedulerEnv, A>
-  readonly provider: E.Env<CurrentFiber, Fiber<unknown>>
+export interface ContextRef<E, A> extends R.WrappedRef<F.CurrentFiber, E, A> {
+  readonly use: E.Env<E & F.CurrentFiber & SchedulerEnv, A>
+  readonly provider: E.Env<F.CurrentFiber, F.Fiber<unknown>>
 }
 
-export function wrapContext<E, A>(ref: Ref<E, A>): ContextRef<E, A> {
+export function wrapContext<E, A>(ref: R.Ref<E, A>): ContextRef<E, A> {
   return {
     id: ref.id,
     initial: ref.initial,
@@ -100,4 +89,4 @@ export function wrapContext<E, A>(ref: Ref<E, A>): ContextRef<E, A> {
   }
 }
 
-export const createContext = flow(makeRef, wrapContext)
+export const createContext = flow(R.makeRef, wrapContext)
