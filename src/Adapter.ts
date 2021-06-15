c95aeff2527f5171099d0097274f5366cc9e7ed7
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-06-15 18:31:40 -0400

    feat: add profunctor instance to Adapter

--- src/Adapter.ts
+++ src/Adapter.ts
@@ -1,9 +1,10 @@
 import * as MA from '@most/adapter'
 import * as M from '@most/core'
 import { Stream } from '@most/types'
-import { flow } from 'fp-ts/function'
+import { flow, pipe } from 'fp-ts/function'
 import { Functor2 } from 'fp-ts/Functor'
 import { Predicate } from 'fp-ts/Predicate'
+import { Profunctor2 } from 'fp-ts/Profunctor'
 import { Refinement } from 'fp-ts/Refinement'
 
 export const URI = '@most/adapter'
@@ -34,13 +35,18 @@ export function create<A, B>(f?: (stream: Stream<A>) => Stream<B>) {
   return [send, f ? f(stream) : stream] as const
 }
 
-export function contramap<A, B>(f: (value: A) => B) {
+export function local<A, B>(f: (value: A) => B) {
   return <C>([send, stream]: Adapter<B, C>): Adapter<A, C> => [flow(f, send), stream]
 }
 
 export function map<A, B>(f: (value: A) => B): <C>(adapter: Adapter<C, A>) => Adapter<C, B> {
   return adapt(M.map(f))
 }
+
+export const promap =
+  <B, A, C, D>(f: (value: B) => A, g: (value: C) => D) =>
+  (adapter: Adapter<A, C>): Adapter<B, D> =>
+    pipe(adapter, local(f), map(g))
 
 export function filter<A, B extends A>(
   f: Refinement<A, B>,
@@ -53,4 +59,9 @@ export function filter<A>(f: Predicate<A>): <C>(adapter: Adapter<C, A>) => Adapt
 
 export const Functor: Functor2<URI> = {
   map,
+}
+
+export const Contravariant: Profunctor2<URI> = {
+  ...Functor,
+  promap,
 }
