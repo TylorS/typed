710c1b53563dd85b6720f3a554afacc31d25faca
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-07-02 15:59:33 -0400

    feat: add type signatures to Counters example

--- examples/counters/src/index.ts
+++ examples/counters/src/index.ts
@@ -8,25 +8,26 @@ import * as U from '@fp/use'
 import { newDefaultScheduler } from '@most/scheduler'
 import * as F from 'fp-ts/function'
 import { range } from 'fp-ts/ReadonlyNonEmptyArray'
-import { html, render } from 'uhtml'
+import { html, render, Renderable } from 'uhtml'
 
 /**
  * This is an example of using hooks to create a homogenous list of
  * isolated Ref.Refs environments
  */
 
-const rootElement = document.getElementById('app')
+const rootElement: HTMLElement | null = document.getElementById('app')
 
 if (!rootElement) {
   throw new Error('Unable to find element by #app')
 }
 
 // Creates a Reference to keep our Count
-const Count = Ref.create(E.of(0))
+// It requires no resources and tracks a number
+const Count: Ref.Wrapped<unknown, number> = Ref.create(E.of(0))
 
 // Actions to update our Count Reference - easily tested
-const increment = Count.update(F.flow(F.increment, E.of))
-const decrement = Count.update(
+const increment: E.Env<Ref.Refs, number> = Count.update(F.flow(F.increment, E.of))
+const decrement: E.Env<Ref.Refs, number> = Count.update(
   F.flow(
     F.decrement,
     E.of,
@@ -35,7 +36,7 @@ const decrement = Count.update(
 )
 
 // Creates a component which represents our counter
-const Counter = (label: string) =>
+const Counter = (label: string): E.Env<Ref.Refs, Renderable> =>
   F.pipe(
     E.Do,
     E.bindW('inc', () => U.useOp(() => increment)),
@@ -51,11 +52,15 @@ const Counter = (label: string) =>
   )
 
 // Creates a Counter to keep track of the total number of Counters
-const Header = F.pipe(`Number Of Counters`, Counter, H.withHooks)
+const Header: RS.ReaderStream<Ref.Refs, Renderable> = F.pipe(
+  `Number Of Counters`,
+  Counter,
+  H.withHooks,
+)
 
 // Create a list of Counters with their own isolated Ref.Refs for state management
 // based on the current count
-const Counters = F.pipe(
+const Counters: RS.ReaderStream<Ref.Refs, ReadonlyArray<Renderable>> = F.pipe(
   Count.get,
   E.map((count) => (count === 0 ? [] : range(1, count))),
   H.withHooks,
@@ -63,14 +68,14 @@ const Counters = F.pipe(
 )
 
 // Combines Counters with a Header that is also just a Counter and renders on each update
-const Main = F.pipe(
+const Main: RS.ReaderStream<Ref.Refs, HTMLElement> = F.pipe(
   RS.combineAll(Header, Counters),
   RS.map(([header, counters]) => html`<div>${header} ${counters}</div>`),
   RS.scan(render, rootElement),
 )
 
 // Provide Main with its required resources
-const stream = Main(Ref.refs())
+const stream: S.Stream<HTMLElement> = Main(Ref.refs())
 
 // Execute our Stream with a default scheduler
 S.runEffects(stream, newDefaultScheduler()).catch((error) => console.error(error))
