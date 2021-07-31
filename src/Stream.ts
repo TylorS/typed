d8aa390bb61b580d992aa69f8d016a651f1ab065
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-07-31 01:32:44 -0400

    docs: add link to most/core docs

--- src/Stream.ts
+++ src/Stream.ts
@@ -5,12 +5,29 @@
  *
  * A large goal of @typed/fp is to expand the `fp-ts` ecosystem to include
  * [`@most/core`](https://github.com/mostjs/core) for a Reactive programming style, including
- * derivatives such as `ReaderStream`, `ReaderStreamEither`, `StateReaderStreamEither` and a few
+ * derivatives such as [ReaderStream](./ReaderStream.ts.md), [ReaderStreamEither](./ReaderStreamEither.ts.md), [StateReaderStreamEither](./StateReaderStreamEither.ts.md) and a few
  * others. It's the fastest push-based reactive library in JS period. The performance characteristics
  * are due to it's architecture of getting out of the way of the computations you need to perform. It's
  * also the first experience I had with FP. For instance, Most utilizes `Functor` laws to remove
  * unneeded machinery through function composition improving runtime performance amongst other
  * optimizations.
+ *
+ * It's simple architecture, and it's
+ * [always-async guarantee](https://mostcore.readthedocs.io/en/latest/concepts.html#always-async),
+ * which is fantastic for modularity, allows for it's
+ * [`Scheduler`](https://mostcore.readthedocs.io/en/latest/api.html#most-scheduler) to be the only
+ * place in the codebase to require a `try/catch`. This ensures that a much greater portion of the
+ * stream graph can be inlined by the optimizing compiler your JS is running within. This `Scheduler`
+ * can be reused to inject _time_ into your applications like any other dependency, with packages like
+ * [most-virtual-scheduler](https://github.com/mostjs-community/virtual-scheduler) allowing you to
+ * control time imperatively for your time-precise tests with millisecond accuracy. This could also
+ * allow you to create a React-like framework where you avoid starting non-blocking async workflows
+ * (think useEffect) by utilizing a virtual scheduler on the server but then utilizing most's default
+ * scheduler to utilize `performance.now()` for millisecond accuracy with monotonic, referentially
+ * transparent, time in the browser.
+ *
+ * See the [@most/core Documentation](https://mostcore.readthedocs.io/en/latest/) for the remaining API
+ * exposed by this module. Both @most/core + @most/types are re-exported from this module
  *
  * ```ts
  * import * as M from '@most/core'
@@ -19,20 +36,6 @@
  * // transformed to effectively at time of construction
  * const output = pipe(stream, M.map(flow(f, g)))
  * ```
- *
- * It's simple architecture, and it's
- * [always-async guarantee](https://mostcore.readthedocs.io/en/latest/concepts.html#always-async),
- * which is fantastic for modularity, but it also allows for it's
- * [`Scheduler`](https://mostcore.readthedocs.io/en/latest/api.html#most-scheduler) to be the only
- * place in the codebase to require a `try/catch`. This ensures that a much greater portion of the
- * stream graph can be inlined by the optimizing compiler your JS is running within. This `Scheduler`
- * can be reused to inject time into your applications like any other dependency, with packages like
- * [most-virtual-scheduler](https://github.com/mostjs-community/virtual-scheduler) allowing you to
- * control time imperatively for your time-precise tests with millisecond accuracy. This could also
- * allow you to create a React-like framework where you avoid starting non-blocking async workflows
- * (think useEffect) by utilizing a virtual scheduler on the server but then utilizing most's default
- * scheduler to utilize `performance.now()` for millisecond accuracy with monotonic, referentially
- * transparent, time in the browser.
  *
  * @since 0.9.2
  */
