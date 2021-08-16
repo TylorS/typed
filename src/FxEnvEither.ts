94f1a3a76b63a026a0bf53a404a97b8bdacf0b57
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-08-15 20:59:22 -0400

    feat: expand FxEnvEither

--- src/FxEnvEither.ts
+++ src/FxEnvEither.ts
@@ -7,13 +7,16 @@ import { Applicative3 } from 'fp-ts/Applicative'
 import { Apply3 } from 'fp-ts/Apply'
 import { Chain3 } from 'fp-ts/Chain'
 import { ChainRec3 } from 'fp-ts/ChainRec'
+import * as EI from 'fp-ts/Either'
 import { FromReader3 } from 'fp-ts/FromReader'
 import { Functor3 } from 'fp-ts/Functor'
+import * as IO from 'fp-ts/IO'
 import { Monad3 } from 'fp-ts/Monad'
 import { Pointed3 } from 'fp-ts/Pointed'
 
-import * as E from './EnvEither'
-import { flow } from './function'
+import * as E from './Env'
+import * as EE from './EnvEither'
+import { flow, Lazy } from './function'
 import { Fx } from './Fx'
 import * as FxT from './FxT'
 import * as P from './Provide'
@@ -23,72 +26,91 @@ import * as R from './Reader'
  * @category Constructor
  * @since 0.13.0
  */
-export const of = FxT.of(E.Pointed)
+export const of = FxT.of(EE.Pointed)
 /**
  * @category Combinator
  * @since 0.13.0
  */
-export const ap = FxT.ap({ ...E.MonadRec, ...E.Apply })
+export const ap = FxT.ap({ ...EE.MonadRec, ...EE.Apply })
 /**
  * @category Combinator
  * @since 0.13.0
  */
-export const chain = FxT.chain<E.URI>()
+export const chain = FxT.chain<EE.URI>()
 /**
  * @category Combinator
  * @since 0.13.0
  */
-export const chainRec = FxT.chainRec(E.MonadRec)
+export const chainRec = FxT.chainRec(EE.MonadRec)
 /**
  * @category Do
  * @since 0.13.0
  */
-export const doEnvEither = FxT.getDo<E.URI>()
+export const doEnvEither = FxT.getDo<EE.URI>()
 /**
  * @category Constructor
  * @since 0.13.0
  */
-export const liftEnvEither = FxT.liftFx<E.URI>()
+export const liftEnvEither = FxT.liftFx<EE.URI>()
 /**
  * @category Combinator
  * @since 0.13.0
  */
-export const map = FxT.map<E.URI>()
+export const map = FxT.map<EE.URI>()
 /**
  * @category Interpreter
  * @since 0.13.0
  */
-export const toEnvEither = FxT.toMonad<E.URI>(E.MonadRec)
+export const toEnvEither = FxT.toMonad<EE.URI>(EE.MonadRec)
 /**
  * @category Constructor
  * @since 0.13.0
  */
-export const ask = FxT.ask(E.FromReader)
+export const ask = FxT.ask(EE.FromReader)
 /**
  * @category Constructor
  * @since 0.13.0
  */
-export const asks = FxT.fromNaturalTransformation<R.URI, E.URI>(E.fromReader)
+export const asks = FxT.fromNaturalTransformation<R.URI, EE.URI>(EE.fromReader)
+
+/**
+ * @category Constructor
+ * @since 0.13.1
+ */
+export const fromEither = FxT.fromNaturalTransformation<EI.URI, EE.URI>(EE.fromEither)
+
+/**
+ * @category Constructor
+ * @since 0.13.1
+ */
+export const fromEnv = FxT.fromNaturalTransformation<E.URI, EE.URI>(EE.fromEnv)
+
+/**
+ * @category Constructor
+ * @since 0.13.1
+ */
+export const fromIO = FxT.fromNaturalTransformation<IO.URI, EE.URI>(EE.fromIO)
+
 /**
  * @category Combinator
  * @since 0.13.0
  */
-export const useSome = FxT.useSome({ ...E.UseSome, ...E.MonadRec })
+export const useSome = FxT.useSome({ ...EE.UseSome, ...EE.MonadRec })
 /**
  * @category Combinator
  * @since 0.13.0
  */
-export const useAll = FxT.useAll({ ...E.UseAll, ...E.MonadRec })
+export const useAll = FxT.useAll({ ...EE.UseAll, ...EE.MonadRec })
 /**
  * @category Combinator
  * @since 0.13.0
  */
-export const provideSome = FxT.provideSome({ ...E.ProvideSome, ...E.MonadRec })
+export const provideSome = FxT.provideSome({ ...EE.ProvideSome, ...EE.MonadRec })
 /**
  * @category Combinator
  * @since 0.13.0
  */
-export const provideAll = FxT.provideAll({ ...E.ProvideAll, ...E.MonadRec })
+export const provideAll = FxT.provideAll({ ...EE.ProvideAll, ...EE.MonadRec })
 /**
  * @category Interpreter
  * @since 0.13.0
@@ -110,7 +132,7 @@ export type URI = typeof URI
  * @category Model
  * @since 0.13.0
  */
-export interface FxEnvEither<R, E, A> extends Fx<E.EnvEither<R, E, unknown>, A> {}
+export interface FxEnvEither<R, E, A> extends Fx<EE.EnvEither<R, E, unknown>, A> {}
 
 declare module 'fp-ts/HKT' {
   export interface URItoKind3<R, E, A> {
@@ -222,10 +244,16 @@ export const Provide: P.Provide3<URI> = {
 }
 
 /**
+ * @since 0.13.1
+ * @category Constructor
+ */
+export const fromOption = <E>(lazy: Lazy<E>) => flow(EE.fromOption(lazy), liftEnvEither)
+
+/**
  * @since 0.13.0
  * @category Constructor
  */
-export const fromReader = flow(E.fromReader, liftEnvEither)
+export const fromReader = flow(EE.fromReader, liftEnvEither)
 
 /**
  * @since 0.13.0
