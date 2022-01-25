f423e0c9aaa9d1ee3a288289d99356da711351b4
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-01-24 21:29:43 -0500

    feat: add operator configuration to withinContext

--- src/Stream/withinContext.ts
+++ src/Stream/withinContext.ts
@@ -11,15 +11,14 @@ import { addOperator, make, Stream } from './Stream'
  * Runs a part of the stream graph within a particular Context
  */
 export const withinContext =
-  <E>(context: Context<E>) =>
+  <E>(context: Context<E>, operator = 'withinContext') =>
   <R, A>(stream: Stream<R, E, A>): Stream<R, E, A> =>
     make<R, E, A>((resources, sink, _, scope, tracer) =>
-      stream.run(resources, sink, context, scope, pipe(tracer, addOperator('withinContext'))),
+      stream.run(resources, sink, context, scope, pipe(tracer, addOperator(operator))),
     )
 
 export const withinRootContext = <R, E, A>(stream: Stream<R, E, A>): Stream<R, E, A> =>
   pipe(
-    getRootContext,
-    fromFx,
-    chain((context) => pipe(stream, withinContext(context))),
+    fromFx(getRootContext, 'withinRootContext'),
+    chain((context) => pipe(stream, withinContext(context, 'withinRootContext'))),
   )
