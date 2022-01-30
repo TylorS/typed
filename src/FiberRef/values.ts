c8790f943ce3ed09934a694f8bc1ff02127089ef
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-01-29 19:18:46 -0500

    fix: doesn't need any resources

--- src/FiberRef/values.ts
+++ src/FiberRef/values.ts
@@ -4,5 +4,5 @@ import { make, Stream } from '@/Stream'
 
 import { FiberRef } from './FiberRef'
 
-export const values = <R, E, A>(fiberRef: FiberRef<R, E, A>): Stream<R, never, Option<A>> =>
+export const values = <R, E, A>(fiberRef: FiberRef<R, E, A>): Stream<unknown, never, Option<A>> =>
   make((sink, context) => context.fiberContext.locals.values(fiberRef).run(sink, context))
