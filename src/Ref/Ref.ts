43b06bc92d65440c9cd38e8581dcc6b36a5d699f
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-02-03 22:50:18 -0500

    feat: Allow ref to have update capabilities

--- src/Ref/Ref.ts
+++ src/Ref/Ref.ts
@@ -3,13 +3,13 @@ import { Option } from 'fp-ts/Option'
 
 import * as Fiber from '@/Fiber'
 import * as FiberRef from '@/FiberRef'
-import { Fx } from '@/Fx'
+import { Fx, of } from '@/Fx'
 import * as Stream from '@/Stream'
 
 export interface Ref<R, E, I, O = I> {
   readonly get: Fx<R, E, O>
   readonly has: Fx<R, E, boolean>
-  readonly set: (input: I) => Fx<R, E, O>
+  readonly update: <R2, E2>(f: (output: O) => Fx<R2, E2, I>) => Fx<R & R2, E | E2, O>
   readonly delete: Fx<R, E, Option<O>>
   readonly values: Stream.Stream<R, E, Option<O>>
 }
@@ -18,7 +18,7 @@ export function fromFiberRef<R, E, A>(fiberRef: FiberRef.FiberRef<R, E, A>): Ref
   return {
     get: FiberRef.get(fiberRef),
     has: FiberRef.has(fiberRef),
-    set: (value: A) => pipe(fiberRef, FiberRef.set(value)),
+    update: (f) => pipe(fiberRef, FiberRef.update(f)),
     delete: FiberRef.delete(fiberRef),
     values: FiberRef.values(fiberRef) as unknown as Ref<R, E, A>['values'],
   }
@@ -32,7 +32,7 @@ export function toGlobal<R, E, I, O>(ref: Ref<R, E, I, O>): Global<R, E, I, O> {
   return {
     get: Fiber.withinRootContext(ref.get),
     has: Fiber.withinRootContext(ref.has),
-    set: flow(ref.set, Fiber.withinRootContext),
+    update: flow(ref.update, Fiber.withinRootContext),
     delete: Fiber.withinRootContext(ref.delete),
     values: Stream.withinRootContext(ref.values),
   }
@@ -40,12 +40,12 @@ export function toGlobal<R, E, I, O>(ref: Ref<R, E, I, O>): Global<R, E, I, O> {
 
 export const global = flow(make, toGlobal)
 
-export function update<A, R2, E2>(f: (a: A) => Fx<R2, E2, A>) {
-  return <R, E>(ref: Ref<R, E, A>): Fx<R & R2, E | E2, A> =>
-    Fx(function* () {
-      const a1 = yield* ref.get
-      const a2 = yield* f(a1)
+export const set =
+  <I>(input: I) =>
+  <R, E, O>(ref: Ref<R, E, I, O>) =>
+    ref.update(() => of(input))
 
-      return yield* ref.set(a2)
-    })
-}
+export const compute =
+  <O, I>(f: (output: O) => I) =>
+  <R, E>(ref: Ref<R, E, I, O>): Fx<R, E, O> =>
+    ref.update(flow(f, of))
