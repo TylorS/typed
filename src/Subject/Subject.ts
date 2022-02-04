da2166110b786ffd421ded5c5ae5bcbfd8ded785
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-02-04 00:48:54 -0500

    feat: allow for Subjects to be given a trace

--- src/Subject/Subject.ts
+++ src/Subject/Subject.ts
@@ -1,17 +1,18 @@
-import { none } from 'fp-ts/Option'
+import { fromNullable } from 'fp-ts/Option'
 
 import { Cause } from '@/Cause'
 import { sync } from '@/Disposable'
 import { Sink, tryEnd, tryEvent } from '@/Sink'
 import * as Stream from '@/Stream'
 import { StreamContext } from '@/Stream'
+import { Trace } from '@/Trace'
 
 export class Subject<R, E, A> implements SubjectSink<E, A>, Stream.Stream<R, E, A> {
   protected observers: Set<Stream.MulticastObserver<R, E, A>> = new Set()
 
   constructor(readonly name: string = 'Subject') {}
 
-  readonly event = (a: A): void => {
+  readonly event = (a: A, trace?: Trace): void => {
     Array.from(this.observers).forEach((o) =>
       tryEvent(o.sink, {
         fiberId: o.context.fiberContext.fiberId,
@@ -19,32 +20,32 @@ export class Subject<R, E, A> implements SubjectSink<E, A>, Stream.Stream<R, E, 
         operator: this.name,
         time: o.context.fiberContext.scheduler.getCurrentTime(),
         value: a,
-        trace: none,
+        trace: fromNullable(trace),
       }),
     )
   }
 
-  readonly error = (cause: Cause<E>): void => {
+  readonly error = (cause: Cause<E>, trace?: Trace): void => {
     Array.from(this.observers).forEach((o) =>
       o.sink.error({
         type: 'Error',
         fiberId: o.context.fiberContext.fiberId,
         operator: this.name,
         time: o.context.fiberContext.scheduler.getCurrentTime(),
-        trace: none,
+        trace: fromNullable(trace),
         cause,
       }),
     )
   }
 
-  readonly end = (): void => {
+  readonly end = (trace?: Trace): void => {
     Array.from(this.observers).forEach((o) =>
       tryEnd(o.sink, {
         type: 'End',
         fiberId: o.context.fiberContext.fiberId,
         operator: this.name,
         time: o.context.fiberContext.scheduler.getCurrentTime(),
-        trace: none,
+        trace: fromNullable(trace),
       }),
     )
   }
@@ -65,7 +66,7 @@ export class Subject<R, E, A> implements SubjectSink<E, A>, Stream.Stream<R, E, 
 }
 
 export interface SubjectSink<E, A> {
-  readonly event: (value: A) => void
-  readonly error: (cause: Cause<E>) => void
-  readonly end: () => void
+  readonly event: (value: A, trace?: Trace) => void
+  readonly error: (cause: Cause<E>, trace?: Trace) => void
+  readonly end: (trace?: Trace) => void
 }
