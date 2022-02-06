d3c359e80d20e09ce6c9bbc427d3770ffe12d348
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-02-06 01:35:38 -0500

    test: cleanup excess testing

--- src/Stream/Stream.test.ts
+++ src/Stream/Stream.test.ts
@@ -3,26 +3,14 @@ import { describe } from 'mocha'
 
 import { pipe } from '@/function'
 import * as Fx from '@/Fx'
-import { formatSinkTraceElement } from '@/Sink'
 
-import { chain } from './chain'
-import { chainFxK } from './chainFxK'
-import { collectEventElements, collectEvents } from './collectEvents'
-import { ask, of } from './fromFx'
+import { collectEvents } from './collectEvents'
+import { ask } from './fromFx'
 
 describe(__filename, () => {
   it('traces the stream graph in events', async () => {
     const test = Fx.Fx(function* () {
       const events = yield* pipe(ask<{ a: number }>('foo'), collectEvents)
-
-      const elements = yield* pipe(
-        ask<{ a: number }>('foo'),
-        chain(({ a }) => of(a)),
-        chainFxK((a) => Fx.of(a + 1)),
-        collectEventElements,
-      )
-
-      console.log(elements.map(formatSinkTraceElement).join('\n'))
 
       deepStrictEqual(events, [{ a: 1 }])
 
