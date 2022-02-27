9548bcaa84f9c4a2e3b04ea7bb7eb7c233eaea1d
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-02-26 20:39:51 -0500

    feat: CommutativeEither

---
+++ tools/overloads/definitions/CommutativeEither.ts
@@ -0,0 +1,37 @@
+import {
+  FunctionSignature,
+  HKTParam,
+  HKTPlaceholder,
+  Interface,
+  InterfaceProperty,
+  Kind,
+  KindReturn,
+  StaticNode,
+  StaticTypeParam,
+} from '../AST'
+
+export const hkt = new HKTParam('T')
+export const placeholder = new HKTPlaceholder(hkt)
+export const aTypeParam = new StaticTypeParam('A')
+export const bTypeParam = new StaticTypeParam('B')
+
+export const node = new Interface(
+  'CommutativeEither',
+  [hkt],
+  [
+    new InterfaceProperty(
+      'either',
+      new FunctionSignature(
+        '',
+        [placeholder, bTypeParam],
+        [new Kind('second', hkt, [placeholder, bTypeParam])],
+        new FunctionSignature(
+          '',
+          [aTypeParam],
+          [new Kind('first', hkt, [placeholder, aTypeParam])],
+          new KindReturn(hkt, [placeholder, new StaticNode('Either<A, B>')]),
+        ),
+      ),
+    ),
+  ],
+)
