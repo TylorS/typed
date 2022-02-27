bbd874255b2fa0e287a1c9887a33f13508d43b74
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-02-26 22:21:33 -0500

    feat: CovariantWithIndex

---
+++ tools/overloads/definitions/CovariantWithIndex.ts
@@ -0,0 +1,43 @@
+import {
+  DynamicFunctionParam,
+  FunctionSignature,
+  HKTParam,
+  HKTPlaceholder,
+  Interface,
+  InterfaceProperty,
+  Kind,
+  KindReturn,
+  StaticTypeParam,
+} from '../AST'
+
+export const hkt = new HKTParam('T')
+export const aTypeParam = new StaticTypeParam('A')
+export const bTypeParam = new StaticTypeParam('B')
+export const placeholder = new HKTPlaceholder(hkt)
+
+export const node = new Interface(
+  'CovariantWithIndex',
+  [hkt],
+  [
+    new InterfaceProperty(
+      'map',
+      new FunctionSignature(
+        '',
+        [aTypeParam, bTypeParam],
+        [
+          new DynamicFunctionParam(
+            'f',
+            [aTypeParam, bTypeParam] as const,
+            ([a, b]) => `(a: ${a}, index: number) => ${b}`,
+          ),
+        ],
+        new FunctionSignature(
+          '',
+          [placeholder],
+          [new Kind('kind', hkt, [placeholder, aTypeParam])],
+          new KindReturn(hkt, [placeholder, bTypeParam]),
+        ),
+      ),
+    ),
+  ],
+)
