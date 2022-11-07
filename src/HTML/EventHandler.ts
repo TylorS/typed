aef505ef96ce1cadd3b7f2c2be345feff50352c2
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-11-07 16:37:05 -0500

    feat: preventDefault + stopPropagation event handlers

--- src/HTML/EventHandler.ts
+++ src/HTML/EventHandler.ts
@@ -14,6 +14,22 @@ export function EventHandler<T extends Event, R = never>(
   return new EventHandlerImplementation<T, R>(handler, options)
 }
 
+export namespace EventHandler {
+  export function preventDefault<T extends Event, R = never>(
+    handler: (event: T) => Effect<R, never, void>,
+    options?: boolean | AddEventListenerOptions,
+  ): EventHandler<T, R> {
+    return EventHandler((event) => (event.preventDefault(), handler(event)), options)
+  }
+
+  export function stopPropagation<T extends Event, R = never>(
+    handler: (event: T) => Effect<R, never, void>,
+    options?: boolean | AddEventListenerOptions,
+  ): EventHandler<T, R> {
+    return EventHandler((event) => (event.stopPropagation(), handler(event)), options)
+  }
+}
+
 export class EventHandlerImplementation<T extends Event, R = never> implements EventHandler<T, R> {
   readonly __Placeholder__!: {
     readonly _R: (_: never) => R
