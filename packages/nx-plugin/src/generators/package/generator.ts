658fc45cb79453caf2ff93b4e64cb2fd54e12fc0
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-11-21 22:15:27 -0500

    chore: fix linting

--- packages/nx-plugin/src/generators/package/generator.ts
+++ packages/nx-plugin/src/generators/package/generator.ts
@@ -1,17 +1,17 @@
-import * as devkit from '@nrwl/devkit'
 import * as path from 'path'
 
+import * as devkit from '@nrwl/devkit'
+
 interface NormalizedSchema {
-  name: string;
+  name: string
   projectName: string
   projectRoot: string
 }
 
-function normalizeOptions(tree: devkit.Tree, options: {name: string}): NormalizedSchema {
+function normalizeOptions(tree: devkit.Tree, options: { name: string }): NormalizedSchema {
   const name = devkit.names(options.name).fileName
   const projectName = name.replace(new RegExp('/', 'g'), '-')
   const projectRoot = `${devkit.getWorkspaceLayout(tree).libsDir}/${name}`
-
 
   return {
     ...options,
@@ -21,15 +21,15 @@ function normalizeOptions(tree: devkit.Tree, options: {name: string}): Normalize
 }
 
 function addFiles(tree: devkit.Tree, options: NormalizedSchema) {
-    const templateOptions = {
-      ...options,
-      ...devkit.names(options.name),
-      offsetFromRoot: devkit.offsetFromRoot(options.projectRoot),
-      template: ''
-    }
-    devkit.generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions)
+  const templateOptions = {
+    ...options,
+    ...devkit.names(options.name),
+    offsetFromRoot: devkit.offsetFromRoot(options.projectRoot),
+    template: '',
+  }
+  devkit.generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions)
 }
-export default async function (tree: devkit.Tree, options: {name: string})  {
+export default async function (tree: devkit.Tree, options: { name: string }) {
   const normalizedOptions = normalizeOptions(tree, options)
   addFiles(tree, normalizedOptions)
   await devkit.formatFiles(tree)
