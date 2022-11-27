7c7c99b6c248cffeb1c450310f49f982302ef549
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-11-26 21:51:00 -0500

    refactor: simplify Clock

--- packages/clock/src/Clock.ts
+++ packages/clock/src/Clock.ts
@@ -4,33 +4,44 @@ import { Time, UnixTime } from '@typed/time'
 
 export interface Clock {
   readonly startTime: UnixTime
-  readonly time: GetAndDelay<Time>
-  readonly unixTime: GetAndDelay<UnixTime>
+  readonly get: () => UnixTime
+  readonly delay: (duration: Duration) => UnixTime
   readonly fork: () => Clock
 }
 
-export interface GetAndDelay<T> {
-  readonly get: () => T
-  readonly delay: (duration: Duration) => T
+const getUnixTime = (): UnixTime => UnixTime(Date.now())
+
+const GetUnixTime = {
+  get: getUnixTime,
+  delay: (duration: Duration) => UnixTime(getUnixTime() + duration.millis),
+  fork: () => Clock(),
 }
 
-const now = (): UnixTime => UnixTime(Date.now())
-
-export function Clock(startTime: UnixTime = now()): Clock {
-  const clock: Clock = {
-    startTime: UnixTime(startTime),
-    time: {
-      get: () => Time(now() - startTime),
-      delay: (duration) => Time(now() - startTime + duration.millis),
-    },
-    unixTime: {
-      get: now,
-      delay: (duration) => UnixTime(now() + duration.millis),
-    },
-    fork: () => Clock(),
+export function Clock(startTime: UnixTime = getUnixTime()): Clock {
+  return {
+    ...GetUnixTime,
+    startTime,
   }
-
-  return clock
 }
 
-Clock.Tag = C.Tag<Clock>()
+export const Tag = C.Tag<Clock>()
+
+export function startTime(clock: Clock): UnixTime {
+  return clock.startTime
+}
+
+export function get(clock: Clock): UnixTime {
+  return clock.get()
+}
+
+export function delay(duration: Duration) {
+  return (clock: Clock): UnixTime => clock.delay(duration)
+}
+
+export function getTime(clock: Clock): Time {
+  return Time(clock.get() - clock.startTime)
+}
+
+export function timeDelay(duration: Duration) {
+  return (clock: Clock): Time => Time(clock.delay(duration) - clock.startTime)
+}
