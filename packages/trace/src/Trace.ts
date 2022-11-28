0630011b0ddcac98ce5a6479d2ba73e005ea403c
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-11-28 16:27:27 -0500

    feat: Trace extends Equal

--- packages/trace/src/Trace.ts
+++ packages/trace/src/Trace.ts
@@ -1,20 +1,27 @@
+import { Equal, hash, hashRandom } from '@fp-ts/data/Equal'
+import { hashAll, memoHash } from '@typed/internal'
+
 import { parseCustomTrace, StackFrame } from './StackFrame.js'
 import { getStackFrames } from './getStackFrames.js'
 
 export type Trace = EmptyTrace | StackFrameTrace
 
-export interface EmptyTrace {
+export interface EmptyTrace extends Equal {
   readonly _tag: 'Empty'
 }
-export const EmptyTrace: EmptyTrace = { _tag: 'Empty' }
+export const EmptyTrace: EmptyTrace = { _tag: 'Empty', ...memoHash(() => hashRandom(EmptyTrace)) }
 
-export interface StackFrameTrace {
+export interface StackFrameTrace extends Equal {
   readonly _tag: 'StackFrame'
   readonly stackFrames: readonly StackFrame[]
 }
 
 export function StackFrameTrace(stackFrames: readonly StackFrame[]): StackFrameTrace {
-  return { _tag: 'StackFrame', stackFrames }
+  return {
+    _tag: 'StackFrame',
+    stackFrames,
+    ...memoHash(() => hashAll('StackFrameTrace', ...stackFrames.map(hash))),
+  }
 }
 
 export const custom = (trace: string): StackFrameTrace => StackFrameTrace([parseCustomTrace(trace)])
