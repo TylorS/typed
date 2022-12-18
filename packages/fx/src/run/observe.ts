66e92ef04147ae88f70a606b2a74efb3eb590f73
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-18 14:39:37 -0500

    feat: generalize observe into run

--- packages/fx/src/run/observe.ts
+++ packages/fx/src/run/observe.ts
@@ -1,18 +1,10 @@
-import * as Deferred from '@effect/io/Deferred'
 import * as Effect from '@effect/io/Effect'
-import * as Exit from '@effect/io/Exit'
-import * as Fiber from '@effect/io/Fiber'
-import * as Schedule from '@effect/io/Schedule'
 import { Scope } from '@effect/io/Scope'
-import { millis } from '@fp-ts/data/Duration'
-import { flow, pipe } from '@fp-ts/data/Function'
+import { flow } from '@fp-ts/data/Function'
 
-import { Fx, Sink } from '../Fx.js'
+import { Fx } from '../Fx.js'
 
-const asyncStart = pipe(
-  Schedule.once(),
-  Schedule.delayed(() => millis(0)),
-)
+import { run_ } from './run.js'
 
 export function observe<A, R2, E2, B>(
   f: (a: A) => Effect.Effect<R2, E2, B>,
@@ -20,22 +12,8 @@ export function observe<A, R2, E2, B>(
   return flow(observe_(f), Effect.scoped)
 }
 
-export function observe_<A, R2, E2, B>(f: (a: A) => Effect.Effect<R2, E2, B>) {
-  return <R, E>(fx: Fx<R, E, A>): Effect.Effect<R | R2 | Scope, E | E2, void> =>
-    Effect.gen(function* ($) {
-      const deferred = yield* $(Deferred.make<E | E2, void>())
-      const sink: Sink<R2, E | E2, A> = Sink(
-        flow(
-          f,
-          Effect.foldCauseEffect((cause) => sink.error(cause), Effect.unit),
-        ),
-        (cause) => pipe(deferred, Deferred.done<E | E2, void>(Exit.failCause(cause))),
-        pipe(deferred, Deferred.done<E | E2, void>(Exit.unit())),
-      )
-
-      const fiber = yield* $(pipe(fx.run(sink), Effect.scheduleForked(asyncStart)))
-
-      yield* $(Deferred.await(deferred))
-      yield* $(Fiber.interrupt(fiber))
-    })
+export function observe_<A, R2, E2, B>(
+  f: (a: A) => Effect.Effect<R2, E2, B>,
+): <R, E>(fx: Fx<R, E, A>) => Effect.Effect<R | R2 | Scope, E | E2, void> {
+  return run_(f, Effect.failCause, Effect.unit())
 }
---
+++ packages/fx/src/run/run.ts
@@ -0,0 +1,49 @@
+import { Cause } from '@effect/io/Cause'
+import * as Deferred from '@effect/io/Deferred'
+import * as Effect from '@effect/io/Effect'
+import * as Fiber from '@effect/io/Fiber'
+import { Scope } from '@effect/io/Scope'
+import { flow, pipe } from '@fp-ts/data/Function'
+
+import { Fx, Sink } from '../Fx.js'
+
+export function run<A, R2, E2, E, R3, E3, B, R4, E4>(
+  event: (a: A) => Effect.Effect<R2, E2, any>,
+  error: (cause: Cause<E>) => Effect.Effect<R3, E3, B>,
+  end: Effect.Effect<R4, E4, B>,
+): <R>(fx: Fx<R, E, A>) => Effect.Effect<R | R2 | R3 | R4, E2 | E3 | E4, B> {
+  return flow(run_(event, error, end), Effect.scoped)
+}
+
+export function run_<A, R2, E2, E, R3, E3, B, R4, E4>(
+  event: (a: A) => Effect.Effect<R2, E2, any>,
+  error: (cause: Cause<E>) => Effect.Effect<R3, E3, B>,
+  end: Effect.Effect<R4, E4, B>,
+) {
+  return <R>(fx: Fx<R, E, A>): Effect.Effect<R | R2 | R3 | R4 | Scope, E2 | E3 | E4, B> =>
+    Effect.gen(function* ($) {
+      const deferred = yield* $(Deferred.make<E2 | E3 | E4, B>())
+      const sink = Sink<R2 | R3 | R4, E, A>(
+        flow(
+          event,
+          Effect.foldCauseEffect(
+            (cause) =>
+              Effect.sync(() =>
+                pipe(deferred, Deferred.unsafeDone<E2 | E3 | E4, B>(Effect.failCause(cause))),
+              ),
+            Effect.unit,
+          ),
+        ),
+        flow(error, Effect.intoDeferred(deferred)),
+        pipe(end, Effect.intoDeferred(deferred)),
+      )
+
+      const fiber = yield* $(Effect.forkScoped(fx.run(sink)))
+
+      const c = yield* $(Deferred.await(deferred))
+
+      yield* $(Fiber.interrupt(fiber))
+
+      return c
+    })
+}
