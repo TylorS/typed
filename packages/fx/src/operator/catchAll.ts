6aab95e70acd816e7254bdb79f455201eeb3f0b0
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-20 11:15:04 -0500

    feat: catchAll + catchAllDefect

---
+++ packages/fx/src/operator/catchAll.ts
@@ -0,0 +1,16 @@
+import * as Cause from '@effect/io/Cause'
+import { match } from '@fp-ts/data/Either'
+import { pipe } from '@fp-ts/data/Function'
+
+import { Fx } from '../Fx.js'
+import { failCause } from '../constructor/failCause.js'
+
+import { catchAllCause } from './catchAllCause.js'
+
+export function catchAll<E, R2, E2, B>(f: (error: E) => Fx<R2, E2, B>) {
+  return <R, A>(fx: Fx<R, E, A>): Fx<R | R2, E2, A | B> =>
+    pipe(
+      fx,
+      catchAllCause((cause) => pipe(cause, Cause.failureOrCause, match(f, failCause))),
+    )
+}
---
+++ packages/fx/src/operator/catchAllDefect.ts
@@ -0,0 +1,22 @@
+import * as Cause from '@effect/io/Cause'
+import { pipe } from '@fp-ts/data/Function'
+import { match } from '@fp-ts/data/Option'
+
+import { Fx } from '../Fx.js'
+import { failCause } from '../constructor/failCause.js'
+
+import { catchAllCause } from './catchAllCause.js'
+
+export function catchAllDefect<R2, E2, B>(f: (defect: unknown) => Fx<R2, E2, B>) {
+  return <R, E, A>(fx: Fx<R, E, A>): Fx<R | R2, E | E2, A | B> =>
+    pipe(
+      fx,
+      catchAllCause(
+        (cause): Fx<R2, E | E2, B> =>
+          pipe(
+            Cause.defects(cause).get(0),
+            match(() => failCause(cause), f),
+          ),
+      ),
+    )
+}
