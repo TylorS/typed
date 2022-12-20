04ed3df85c090cd14c352e594f2ddf30b2e95254
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-20 15:27:44 -0500

    feat: switchMapEffect

---
+++ packages/fx/src/operator/switchMapEffect.ts
@@ -0,0 +1,13 @@
+import { Effect } from '@effect/io/Effect'
+import { flow } from '@fp-ts/data/Function'
+
+import { Fx } from '../Fx.js'
+import { fromEffect } from '../constructor/fromEffect.js'
+
+import { switchMap } from './switchMap.js'
+
+export function switchMapEffect<A, R2, E2, B>(
+  f: (a: A) => Effect<R2, E2, B>,
+): <R, E>(stream: Fx<R, E, A>) => Fx<R | R2, E | E2, B> {
+  return switchMap(flow(f, fromEffect))
+}
