cf55e9cab7fc1ff4de3d1e51b679d9781dd2f2e3
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-20 11:09:23 -0500

    feat: catchAllCause

--- packages/fx/src/operator/catchAllCause.test.ts
+++ packages/fx/src/operator/catchAllCause.test.ts
@@ -4,22 +4,23 @@ import * as Effect from '@effect/io/Effect'
 import { pipe } from '@fp-ts/data/Function'
 import { describe, it } from 'vitest'
 
+import { fail } from '../constructor/fail.js'
 import { fromArray } from '../constructor/fromArray.js'
 import { collectAll } from '../run/collectAll.js'
 
-import { continueWith } from './continueWith.js'
+import { catchAllCause } from './catchAllCause.js'
 
 describe(import.meta.url, () => {
-  describe(continueWith.name, () => {
-    it('concatenating a stream after another', async () => {
+  describe(catchAllCause.name, () => {
+    it('recoving from failures with another Fx', async () => {
       const test = pipe(
-        fromArray([1, 2, 3]),
-        continueWith(() => fromArray([4, 5, 6])),
+        fail('test'),
+        catchAllCause(() => fromArray([4, 5, 6])),
         collectAll,
       )
       const events = await Effect.unsafeRunPromise(test)
 
-      deepStrictEqual(events, [1, 2, 3, 4, 5, 6])
+      deepStrictEqual(events, [4, 5, 6])
     })
   })
 })
