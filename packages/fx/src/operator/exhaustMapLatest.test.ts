3c81fa151c46d3be20619f8caf6b45be4be478e0
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-20 17:31:00 -0500

    feat: exhaustMapLatest

---
+++ packages/fx/src/operator/exhaustMapLatest.test.ts
@@ -0,0 +1,38 @@
+import { deepStrictEqual } from 'assert'
+
+import * as Effect from '@effect/io/Effect'
+import * as Duration from '@fp-ts/data/Duration'
+import { pipe } from '@fp-ts/data/Function'
+import { describe, it } from 'vitest'
+
+import { fromIterable } from '../constructor/fromIterable.js'
+import { collectAll } from '../run/collectAll.js'
+
+import { delay } from './delay.js'
+import { exhaustMapLatest } from './exhaustMapLatest.js'
+
+describe(import.meta.url, () => {
+  describe(exhaustMapLatest.name, () => {
+    it('allows chaining multiple synchronous streams, favoring the first, replaying the latest', async () => {
+      const result = await pipe(
+        fromIterable([1, 2, 3]),
+        exhaustMapLatest((n) => fromIterable([n, n, n])),
+        collectAll,
+        Effect.unsafeRunPromise,
+      )
+
+      deepStrictEqual(result, [1, 1, 1, 3, 3, 3])
+    })
+
+    it('allows chaining multiple asynchronous streams, favoring the first, replaying the latest', async () => {
+      const result = await pipe(
+        fromIterable([1, 2, 3]),
+        exhaustMapLatest((n) => delay(Duration.millis(10))(fromIterable([n, n, n]))),
+        collectAll,
+        Effect.unsafeRunPromise,
+      )
+
+      deepStrictEqual(result, [1, 1, 1, 3, 3, 3])
+    })
+  })
+})
---
+++ packages/fx/src/operator/exhaustMapLatest.ts
@@ -0,0 +1,86 @@
+import * as Effect from '@effect/io/Effect'
+import * as Fiber from '@effect/io/Fiber'
+import * as Ref from '@effect/io/Ref/Synchronized'
+import { Scope } from '@effect/io/Scope'
+import { pipe } from '@fp-ts/data/Function'
+
+import { Fx } from '../Fx.js'
+import { withRefCounter } from '../_internal/RefCounter.js'
+
+export function exhaustMapLatest<A, R2, E2, B>(f: (a: A) => Fx<R2, E2, B>) {
+  return <R, E>(fx: Fx<R, E, A>): Fx<R | R2, E | E2, B> => new ExhaustMapFx(fx, f)
+}
+
+export class ExhaustMapFx<R, E, A, R2, E2, B>
+  extends Fx.Variance<R | R2, E | E2, B>
+  implements Fx<R | R2, E | E2, B>
+{
+  constructor(readonly fx: Fx<R, E, A>, readonly f: (a: A) => Fx<R2, E2, B>) {
+    super()
+  }
+
+  run<R3>(sink: Fx.Sink<R3, E | E2, B>) {
+    return pipe(
+      Ref.make<Fiber.RuntimeFiber<never, unknown> | null>(null),
+      Effect.zip(Ref.make<Fx<R2, E2, B> | null>(null)),
+      Effect.flatMap(([fiberRef, nextFxRef]) => {
+        return withRefCounter(
+          1,
+          (counter) => {
+            const resetRef = pipe(fiberRef, Ref.set<Fiber.Fiber<never, unknown> | null>(null))
+
+            const runNextFx: Effect.Effect<R3 | R2 | Scope, never, void> = pipe(
+              nextFxRef,
+              Ref.getAndSet<Fx<R2, E2, B> | null>(null),
+              Effect.flatMap((fx) =>
+                fx
+                  ? pipe(
+                      fiberRef,
+                      Ref.updateEffect(() => runFx(fx)),
+                    )
+                  : counter.decrement,
+              ),
+            )
+
+            const runFx = (
+              fx: Fx<R2, E2, B>,
+            ): Effect.Effect<R2 | R3 | Scope, never, Fiber.RuntimeFiber<never, unknown> | null> =>
+              Effect.forkScoped(
+                fx.run(
+                  Fx.Sink(
+                    sink.event,
+                    (e) => pipe(resetRef, Effect.zipRight(sink.error(e))),
+                    pipe(resetRef, Effect.zipRight(runNextFx)),
+                  ),
+                ),
+              )
+
+            return this.fx.run(
+              Fx.Sink(
+                (a) =>
+                  pipe(
+                    fiberRef,
+                    Ref.updateEffect((fiber) =>
+                      fiber
+                        ? pipe(
+                            nextFxRef,
+                            Ref.set<Fx<R2, E2, B> | null>(this.f(a)),
+                            Effect.as(fiber),
+                          )
+                        : pipe(
+                            counter.increment,
+                            Effect.flatMap(() => runFx(this.f(a))),
+                          ),
+                    ),
+                  ),
+                sink.error,
+                counter.decrement,
+              ),
+            )
+          },
+          sink.end,
+        )
+      }),
+    )
+  }
+}
