a094fffe0311410f0dff472ef0a7bfd302ea9612
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-20 11:48:40 -0500

    test: takeUntil + skipUntil descriptions

--- packages/fx/src/operator/takeUntil.test.ts
+++ packages/fx/src/operator/takeUntil.test.ts
@@ -7,19 +7,19 @@ import { describe, it } from 'vitest'
 import { fromArray } from '../constructor/fromArray.js'
 import { collectAll } from '../run/collectAll.js'
 
-import { skipUntil } from './skipUntil.js'
+import { takeUntil } from './takeUntil.js'
 
 describe(import.meta.url, () => {
-  describe(skipUntil.name, () => {
-    it('skips values until a predicate has been matched', async () => {
+  describe(takeUntil.name, () => {
+    it('ends an Fx once a predicate has been matched', async () => {
       const test = pipe(
         fromArray([1, 2, 3, 4, 5, 6, 7, 8]),
-        skipUntil((x) => x === 4),
+        takeUntil((x) => x === 4),
         collectAll,
       )
       const events = await Effect.unsafeRunPromise(test)
 
-      deepStrictEqual(events, [4, 5, 6, 7, 8])
+      deepStrictEqual(events, [1, 2, 3])
     })
   })
 })
