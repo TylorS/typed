b66380e54ab55de29b763b92cffffebb728da94c
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-20 16:10:53 -0500

    feat: Fx.skipAfter

---
+++ packages/fx/src/operator/skipAfter.test.ts
@@ -0,0 +1,29 @@
+import { deepStrictEqual } from 'assert'
+
+import * as Effect from '@effect/io/Effect'
+import { pipe } from '@fp-ts/data/Function'
+import { describe, it } from 'vitest'
+
+import { fromArray } from '../constructor/fromArray.js'
+import { collectAll } from '../run/collectAll.js'
+
+import { skipAfter } from './skipAfter.js'
+import { tap } from './tap.js'
+
+describe(import.meta.url, () => {
+  describe(skipAfter.name, () => {
+    it('skips values after a predicate has been matched', async () => {
+      let count = 0
+      const test = pipe(
+        fromArray([1, 2, 3, 4, 5, 6, 7, 8]),
+        tap(() => Effect.sync(() => count++)),
+        skipAfter((x) => x === 4),
+        collectAll,
+      )
+      const events = await Effect.unsafeRunPromise(test)
+
+      deepStrictEqual(events, [1, 2, 3])
+      deepStrictEqual(count, 8)
+    })
+  })
+})
---
+++ packages/fx/src/operator/skipAfter.ts
@@ -0,0 +1,39 @@
+import { unit } from '@effect/io/Effect'
+import { Predicate } from '@fp-ts/data/Predicate'
+
+import { Fx } from '../Fx.js'
+
+export function skipAfter<A>(predicate: Predicate<A>) {
+  return <R, E>(fx: Fx<R, E, A>): Fx<R, E, A> => new SkipAfterFx(fx, predicate)
+}
+
+export class SkipAfterFx<R, E, A> extends Fx.Variance<R, E, A> implements Fx<R, E, A> {
+  constructor(readonly fx: Fx<R, E, A>, readonly predicate: Predicate<A>) {
+    super()
+  }
+
+  run<R2>(sink: Fx.Sink<R2, E, A>) {
+    return this.fx.run(new SkipAfterSink(sink, this.predicate))
+  }
+}
+
+export class SkipAfterSink<R, E, A> implements Fx.Sink<R, E, A> {
+  protected running = true
+
+  constructor(readonly sink: Fx.Sink<R, E, A>, readonly predicate: Predicate<A>) {}
+
+  event = (a: A) => {
+    const passed = this.predicate(a)
+
+    if (this.running && !passed) {
+      return this.sink.event(a)
+    } else if (passed) {
+      this.running = false
+    }
+
+    return unit()
+  }
+
+  error = this.sink.error
+  end = this.sink.end
+}
