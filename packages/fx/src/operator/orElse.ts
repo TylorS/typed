a5717c29b1872d9dc0c55e7ea4c948c68fffada7
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-20 15:40:31 -0500

    feat: orElse

---
+++ packages/fx/src/operator/orElse.ts
@@ -0,0 +1,9 @@
+import { Fx } from '../Fx.js'
+
+import { catchAll } from './catchAll.js'
+
+export function orElse<R2, E2, B>(
+  second: Fx<R2, E2, B>,
+): <R, E, A>(fx: Fx<R, E, A>) => Fx<R | R2, E | E2, A | B> {
+  return catchAll(() => second)
+}
