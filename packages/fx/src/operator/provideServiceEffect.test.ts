cadf8c86f60c85e89e0412723e5682b9b7e523bf
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-19 19:51:32 -0500

    test: provideServiceEffect

---
+++ packages/fx/src/operator/provideServiceEffect.test.ts
@@ -0,0 +1,41 @@
+import { deepStrictEqual } from 'assert'
+
+import * as Effect from '@effect/io/Effect'
+import { pipe } from '@fp-ts/data/Function'
+import { Tag } from 'node_modules/@fp-ts/data/Context.js'
+import { describe, it } from 'vitest'
+
+import { serviceWith } from '../constructor/serviceWith.js'
+import { collectAll } from '../run/collectAll.js'
+
+import { provideServiceEffect } from './provideServiceEffect.js'
+
+describe(import.meta.url, () => {
+  describe(provideServiceEffect.name, () => {
+    it('allows merging multiple streams together', async () => {
+      interface Foo {
+        readonly foo: number
+      }
+      const Foo = Object.assign(Tag<Foo>(), {
+        with<A>(f: (foo: Foo) => A) {
+          return serviceWith(Foo)(f)
+        },
+        provide(foo: number) {
+          return provideServiceEffect(
+            Foo,
+            Effect.sync(() => ({ foo })),
+          )
+        },
+      })
+
+      const test = pipe(
+        Foo.with((f) => f.foo),
+        Foo.provide(42),
+        collectAll,
+      )
+      const events = await Effect.unsafeRunPromise(test)
+
+      deepStrictEqual(events, [42])
+    })
+  })
+})
