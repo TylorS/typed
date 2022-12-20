828fa20bbf0804533224026af2335d538df24183
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-20 17:22:26 -0500

    feat: Fx.exhaustMap

---
+++ packages/fx/src/operator/exhaustMap.test.ts
@@ -0,0 +1,38 @@
+import { deepStrictEqual } from 'assert'
+
+import * as Effect from '@effect/io/Effect'
+import * as Duration from '@fp-ts/data/Duration'
+import { pipe } from '@fp-ts/data/Function'
+import { describe, it } from 'vitest'
+
+import { fromIterable } from '../constructor/fromIterable.js'
+import { collectAll } from '../run/collectAll.js'
+
+import { delay } from './delay.js'
+import { exhaustMap } from './exhaustMap.js'
+
+describe(import.meta.url, () => {
+  describe(exhaustMap.name, () => {
+    it('allows chaining multiple synchronous streams, favoring the first', async () => {
+      const result = await pipe(
+        fromIterable([1, 2, 3]),
+        exhaustMap((n) => fromIterable([n, n, n])),
+        collectAll,
+        Effect.unsafeRunPromise,
+      )
+
+      deepStrictEqual(result, [1, 1, 1])
+    })
+
+    it('allows chaining multiple asynchronous streams, favoring the first', async () => {
+      const result = await pipe(
+        fromIterable([1, 2, 3]),
+        exhaustMap((n) => delay(Duration.millis(10))(fromIterable([n, n, n]))),
+        collectAll,
+        Effect.unsafeRunPromise,
+      )
+
+      deepStrictEqual(result, [1, 1, 1])
+    })
+  })
+})
---
+++ packages/fx/src/operator/exhaustMap.ts
@@ -0,0 +1,63 @@
+import * as Effect from '@effect/io/Effect'
+import * as Fiber from '@effect/io/Fiber'
+import * as Ref from '@effect/io/Ref/Synchronized'
+import { pipe } from '@fp-ts/data/Function'
+
+import { Fx } from '../Fx.js'
+import { withRefCounter } from '../_internal/RefCounter.js'
+
+export function exhaustMap<A, R2, E2, B>(f: (a: A) => Fx<R2, E2, B>) {
+  return <R, E>(fx: Fx<R, E, A>): Fx<R | R2, E | E2, B> => new ExhaustMapFx(fx, f)
+}
+
+export class ExhaustMapFx<R, E, A, R2, E2, B>
+  extends Fx.Variance<R | R2, E | E2, B>
+  implements Fx<R | R2, E | E2, B>
+{
+  constructor(readonly fx: Fx<R, E, A>, readonly f: (a: A) => Fx<R2, E2, B>) {
+    super()
+  }
+
+  run<R3>(sink: Fx.Sink<R3, E | E2, B>) {
+    return pipe(
+      Ref.make<Fiber.Fiber<never, unknown> | null>(null),
+      Effect.flatMap((fiberRef) => {
+        const resetRef = pipe(fiberRef, Ref.set<Fiber.Fiber<never, unknown> | null>(null))
+
+        return withRefCounter(
+          1,
+          (counter) =>
+            this.fx.run(
+              Fx.Sink(
+                (a) =>
+                  pipe(
+                    fiberRef,
+                    Ref.updateEffect((fiber) =>
+                      fiber
+                        ? Effect.succeed(fiber)
+                        : pipe(
+                            counter.increment,
+                            Effect.flatMap(() =>
+                              Effect.forkScoped(
+                                this.f(a).run(
+                                  Fx.Sink(
+                                    sink.event,
+                                    (cause) => pipe(resetRef, Effect.zipRight(sink.error(cause))),
+                                    pipe(resetRef, Effect.zipRight(counter.decrement)),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                    ),
+                  ),
+                sink.error,
+                counter.decrement,
+              ),
+            ),
+          sink.end,
+        )
+      }),
+    )
+  }
+}
