53c5f5ddcffd0599db51292ffc60fec72b986c03
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-26 19:40:10 -0500

    chore: lazy-load react too

--- examples/pages/react/counter.tsx
+++ examples/pages/react/counter.tsx
@@ -1,8 +1,6 @@
 import * as Effect from '@effect/io/Effect'
 import { pipe } from '@fp-ts/data/Function'
 import { Main } from '@typed/framework/Module.js'
-import { Counter } from 'components/react-counter.js'
-import { createRoot } from 'react-dom/client'
 
 import { createElement } from '@typed/dom/Document.js'
 import * as Fx from '@typed/fx/index.js'
@@ -12,6 +10,15 @@ export const route = Route('/react/:counter')
 
 export const main = Main.make(route)((params) =>
   Fx.gen(function* ($) {
+    // Lazy-load React and the Counter component
+    const [{ createRoot }, Counter] = yield* $(
+      Effect.promise(() =>
+        Promise.all([
+          import('react-dom/client'),
+          import('../../components/react-counter.jsx').then((m) => m.Counter),
+        ] as const),
+      ),
+    )
     // TODO: Is there a way to make this work without creating a wrapper element?
     const container = yield* $(createElement('div'))
     const root = createRoot(container)
