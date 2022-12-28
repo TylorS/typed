cc2542c10e2fc9aeb198f2a68d45ea2edf4ea960
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-28 16:28:47 -0500

    fix: correct signature of earlyExit

--- packages/fx/src/_internal/earlyExit.ts
+++ packages/fx/src/_internal/earlyExit.ts
@@ -4,7 +4,7 @@ import { pipe } from '@fp-ts/data/Function'
 export function withEarlyExit<R, E, A, R2, E2, B>(
   f: (_: Effect.Effect<never, never, never>) => Effect.Effect<R, E, A>,
   onEnd: Effect.Effect<R2, E2, B>,
-): Effect.Effect<R | R2, E | E2, void> {
+): Effect.Effect<R | R2, E | E2, A | B> {
   return Effect.asyncEffect((cb) =>
     pipe(
       Effect.sync(() => cb(onEnd)),
