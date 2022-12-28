1f0b7b73465b6f48d2c122d3d8aa13a640621d9f
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-12-28 15:59:18 -0500

    refactor: remove all the old code

--- packages/fx/src/_internal/earlyExit.ts
+++ packages/fx/src/_internal/earlyExit.ts
@@ -1,36 +1,10 @@
-// import * as Cause from '@effect/io/Cause'
 import * as Effect from '@effect/io/Effect'
-// import * as Fiber from '@effect/io/Fiber'
-import * as Scope from '@effect/io/Scope'
-// import * as Chunk from '@fp-ts/data/Chunk'
 import { pipe } from '@fp-ts/data/Function'
-
-// import { asap } from './RefCounter.js'
-
-// // Need to replace with real cancelation of a Fiber somehow
-
-// const EarlyExit = Symbol('EarlyExit')
-// type EarlyExit = typeof EarlyExit
-
-// const earlyExit = Effect.die(EarlyExit)
-
-// const isEarlyExit = (e: unknown): e is EarlyExit => e === EarlyExit
-
-// const catchEarlyExit =
-//   <R2, E2, B>(onEarlyExit: Effect.Effect<R2, E2, B>) =>
-//   <R, E, A>(effect: Effect.Effect<R, E, A>): Effect.Effect<R | R2, E | E2, A | B> =>
-//     pipe(
-//       effect,
-//       Effect.catchAllCause(
-//         (e): Effect.Effect<R2, E | E2, B> =>
-//           Chunk.some(isEarlyExit)(Cause.defects(e)) ? onEarlyExit : Effect.failCause(e),
-//       ),
-//     )
 
 export function withEarlyExit<R, E, A, R2, E2, B>(
   f: (_: Effect.Effect<never, never, never>) => Effect.Effect<R, E, A>,
   onEnd: Effect.Effect<R2, E2, B>,
-): Effect.Effect<R | R2 | Scope.Scope, E | E2, void> {
+): Effect.Effect<R | R2, E | E2, void> {
   return Effect.asyncEffect((cb) =>
     pipe(
       Effect.sync(() => cb(onEnd)),
