2e06e7bf089f4c497adfb80a3713191c97e4b3c8
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-01-05 00:42:11 -0500

    feat: delete progress fetch for now

--- packages/dom/src/Fetch.ts
+++ packages/dom/src/Fetch.ts
@@ -1,13 +1,11 @@
 import * as Effect from '@effect/io/Effect'
 import { identity, pipe } from '@fp-ts/data/Function'
 import { Identity } from '@fp-ts/data/Identity'
-import * as Option from '@fp-ts/data/Option'
 import * as These from '@fp-ts/data/These'
 import { DecodeError } from '@fp-ts/schema/DecodeError'
 import { decoderFor } from '@fp-ts/schema/Decoder'
 import * as S from '@fp-ts/schema/Schema'
 import * as Context from '@typed/context'
-import * as Fx from '@typed/fx'
 
 export interface Fetch extends Identity<typeof globalThis['fetch']> {}
 
@@ -126,73 +124,4 @@ export const fetchWithSchema = <A>(
         )
       }),
     )
-}
-
-/**
- * Fetch a response and return the raw body as a Uint8Array. This is returned as a
- * pair of a Progress indicating if the response is complete and the body currently received
- * as a Uint8Array. This allows checking .length on the Uint8Array to understand progress
- */
-export const fetchRawBody = (
-  input: RequestInfo | URL,
-  init: FetchInit,
-): Fx.Fx<Fetch, FetchError, readonly [Progress, Uint8Array]> =>
-  Fx.gen(function* ($) {
-    const response = yield* $(fetchResponse(input, init))
-    const total = pipe(
-      Option.fromNullable(response.headers.get('content-length')),
-      Option.map(parseFloat),
-    )
-    const body = yield* $(
-      Fx.makeRef((): readonly [Progress, Uint8Array] => [{ loaded: 0, total }, new Uint8Array(0)]),
-    )
-
-    // Read the response until completion
-    if (response.body) {
-      yield* $(
-        Effect.forkScoped(
-          Effect.gen(function* ($) {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            const reader = response.body!.getReader()
-
-            // eslint-disable-next-line no-constant-condition
-            while (true) {
-              const result = yield* $(Effect.promise(() => reader.read()))
-
-              // If we're done, end the stream
-              if (result.done) {
-                return yield* $(body.end)
-              }
-
-              const { value } = result
-
-              // Update the body with the new progress and currently loaded Uint8Array
-              yield* $(
-                body.update(([{ loaded }, current]) => [
-                  { loaded: loaded + value.length, total },
-                  concatUint8Array(current, value),
-                ]),
-              )
-            }
-          }),
-        ),
-      )
-
-      return body
-    }
-
-    // Otherwise return an empty body
-    return Fx.succeed([{ loaded: 0, total: Option.some(0) }, new Uint8Array(0)])
-  })
-
-export interface Progress {
-  readonly loaded: number
-  readonly total: Option.Option<number>
-}
-
-function concatUint8Array(a: Uint8Array, b: Uint8Array): Uint8Array {
-  const c = new Uint8Array(a.length + b.length)
-  c.set(a, 0)
-  c.set(b, a.length)
-  return c
 }
