32f09b95b6d71e4cc98bb5ffb6e074efca488b56
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-01-16 14:56:07 -0500

    refactor: start adding a service

--- example/api/hello.ts
+++ example/api/hello.ts
@@ -1,13 +1,26 @@
 import * as Effect from '@effect/io/Effect'
+import { pipe } from '@fp-ts/data/Function'
+import * as Context from '@typed/context'
 import { FetchHandler } from '@typed/framework/express'
 import { Route } from '@typed/route'
 
-export const handler = FetchHandler(Route('/hello/:name'), (_, { name }) => {
-  return Effect.succeed(
-    new Response(`Hello, ${name}!`, {
-      headers: {
-        'content-type': 'text/plain',
-      },
-    }),
-  )
-})
+export interface I18N {
+  readonly translate: (key: string) => Effect.Effect<never, never, string>
+}
+export const I18N = Context.Tag<I18N>()
+
+export const handler = FetchHandler(Route('/hello/:name'), (_, { name }) =>
+  I18N.withEffect(({ translate }) =>
+    pipe(
+      translate('hello'),
+      Effect.map(
+        (greeting) =>
+          new Response(`${greeting}, ${name}!`, {
+            headers: {
+              'content-type': 'text/plain',
+            },
+          }),
+      ),
+    ),
+  ),
+).provideLayer(I18N.layerOf({ translate: Effect.succeed }))
