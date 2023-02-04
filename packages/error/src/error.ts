cf4b7f321dc659a3949f4cc57f91d266d9e7cd5e
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-02-04 15:57:43 -0500

    refactor: improve inference of Error methods

--- packages/error/src/error.ts
+++ packages/error/src/error.ts
@@ -6,20 +6,49 @@ export const tagged = <Tag extends string>(tag: Tag) =>
     static tag = tag
     readonly _tag = tag
 
-    static catch = <T extends Tagged<Tag>, R, E, A>(f: (error: T) => Effect.Effect<R, E, A>) =>
-      Effect.catchTag(tag, f)
+    static catch<T extends TaggedConstructor<Tag>, R, E, A>(
+      this: T,
+      f: (error: InstanceType<T>) => Effect.Effect<R, E, A>,
+    ): <R2, E2, A2>(
+      effect: Effect.Effect<R2, E2 | InstanceType<T>, A2>,
+    ) => Effect.Effect<R | R2, E | Exclude<E2, InstanceType<T>>, A | A2> {
+      return Effect.catchTag(tag, f as any) as any
+    }
 
-    static catchFx = <T extends Tagged<Tag>, R, E, A>(f: (error: T) => Fx.Fx<R, E, A>) =>
-      Fx.catchTag(tag, f)
+    static catchFx<T extends TaggedConstructor<Tag>, R, E, A>(
+      this: T,
+      f: (error: InstanceType<T>) => Fx.Fx<R, E, A>,
+    ): <R2, E2, A2>(
+      fx: Fx.Fx<R2, E2 | InstanceType<T>, A2>,
+    ) => Fx.Fx<R | R2, E | Exclude<E2, InstanceType<T>>, A | A2> {
+      return Fx.catchTag(tag, f as any) as any
+    }
 
-    static switchMapCatch = <T extends Tagged<Tag>, R, E, A>(f: (error: T) => Fx.Fx<R, E, A>) =>
-      Fx.switchMapCatchTag(tag, f)
+    static switchMapCatch<T extends TaggedConstructor<Tag>, R, E, A>(
+      this: T,
+      f: (error: InstanceType<T>) => Fx.Fx<R, E, A>,
+    ): <R2, E2, A2>(
+      fx: Fx.Fx<R2, E2 | InstanceType<T>, A2>,
+    ) => Fx.Fx<R | R2, E | Exclude<E2, InstanceType<T>>, A | A2> {
+      return Fx.switchMapCatchTag(tag, f as any) as any
+    }
+
+    readonly is = <T extends TaggedConstructor<any>>(constructor: T): this is InstanceType<T> =>
+      this._tag === constructor.tag
   }
 
-export type TaggedConstructor<Tag extends string> = ReturnType<typeof tagged<Tag>>
+export type TaggedConstructor<Tag extends string> = {
+  readonly tag: Tag
+  new (...args: any[]): Tagged<Tag>
+
+  readonly catch: ReturnType<typeof tagged<Tag>>['catch']
+  readonly catchFx: ReturnType<typeof tagged<Tag>>['catchFx']
+  readonly switchMapCatch: ReturnType<typeof tagged<Tag>>['switchMapCatch']
+}
 
 export interface Tagged<Tag extends string> extends Error {
   readonly _tag: Tag
+  readonly is: <T extends TaggedConstructor<any>>(constructor: T) => this is InstanceType<T>
 }
 
 export type TagOf<T> = [T] extends [Tagged<infer Tag>]
