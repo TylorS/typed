ce1be090d0fc5559b68a6ee454d600c2114f1465
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-02-28 15:43:13 -0500

    refactor: use namespace import

--- example/other-pages/react/counter.tsx
+++ example/other-pages/react/counter.tsx
@@ -1,5 +1,5 @@
 import { range } from '@effect/data/ReadonlyArray'
-import { succeed, sync } from '@effect/io/Effect'
+import * as Effect from '@effect/io/Effect'
 import { Route } from '@typed/route'
 
 import { Counter } from '../../components/react-counter.jsx'
@@ -8,8 +8,8 @@ import { renderReact } from './render-react.js'
 
 export const route = Route('/react/:counter')
 
-export const main = renderReact(route, ({ counter }) => succeed(<Counter name={counter} />))
+export const main = renderReact(route, ({ counter }) => Effect.succeed(<Counter name={counter} />))
 
-export const getStaticPaths = sync(() =>
+export const getStaticPaths = Effect.sync(() =>
   range(0, 10).map((i) => route.make({ counter: i.toString() })),
 )
--- example/other-pages/svelte/svelte.ts
+++ example/other-pages/svelte/svelte.ts
@@ -1,6 +1,6 @@
 /// <reference types="svelte" />
 import { range } from '@effect/data/ReadonlyArray'
-import { succeed, sync } from '@effect/io/Effect'
+import * as Effect from '@effect/io/Effect'
 import { Route } from '@typed/route'
 
 import Hello from '../../components/Hello.svelte'
@@ -9,8 +9,8 @@ import { renderSvelte } from './render-svelte.js'
 
 export const route = Route('/svelte/:name')
 
-export const main = renderSvelte(route, Hello, succeed)
+export const main = renderSvelte(route, Hello, Effect.succeed)
 
-export const getStaticPaths = sync(() =>
+export const getStaticPaths = Effect.sync(() =>
   range(0, 10).map((i) => route.make({ name: i.toString() })),
 )
