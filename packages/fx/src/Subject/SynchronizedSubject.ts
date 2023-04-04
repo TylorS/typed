92b1c4f64a76d22adf83535e206078b2f390de05
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-04-04 11:15:28 -0400

    fix: update SynchronizedSubject

--- packages/fx/src/Subject/SynchronizedSubject.ts
+++ packages/fx/src/Subject/SynchronizedSubject.ts
@@ -1,12 +1,12 @@
 import { equals } from '@effect/data/Equal'
 import { identity, pipe } from '@effect/data/Function'
-import * as MutableRef from '@effect/data/MutableRef'
 import * as Option from '@effect/data/Option'
 import * as Effect from '@effect/io/Effect'
 import * as Ref from '@effect/io/Ref'
 import * as Synchronized from '@effect/io/Ref/Synchronized'
 
 import { Fx } from '../Fx.js'
+import { Mutable } from '../_internal/Mutable.js'
 
 import { HoldSubject } from './HoldSubject.js'
 import { Computed, isRefSubject, RefSubject } from './RefSubject.js'
@@ -28,17 +28,17 @@ export namespace SynchronizedSubject {
     initial: () => A,
     eq: (a: A, b: A) => boolean = equals,
   ): SynchronizedSubject<A> {
-    const mutableRef = MutableRef.make(Option.some(initial()))
-    const subject = HoldSubject.unsafeMake<never, A>(mutableRef)
+    const current = Mutable(Option.some(initial()))
+    const subject = HoldSubject.unsafeMake<never, A>(current)
     const locked = Effect.unsafeMakeSemaphore(1).withPermits(1)
 
     const getValue = () =>
       pipe(
-        MutableRef.get(mutableRef),
+        current.get(),
         Option.getOrElse(() => {
           const a = initial()
 
-          MutableRef.set(mutableRef, Option.some(a))
+          current.set(Option.some(a))
 
           return a
         }),
@@ -49,14 +49,14 @@ export namespace SynchronizedSubject {
     ): Effect.Effect<R2, E2, B> =>
       locked(
         Effect.gen(function* ($) {
-          const current = getValue()
-          const [b, a] = yield* $(f(current))
+          const next = getValue()
+          const [b, a] = yield* $(f(next))
 
-          if (eq(current, a)) {
+          if (eq(next, a)) {
             return b
           }
 
-          MutableRef.set(mutableRef, Option.some(a))
+          current.set(Option.some(a))
 
           yield* $(subject.event(a))
 
@@ -82,6 +82,7 @@ export namespace SynchronizedSubject {
       error: subject.error.bind(subject),
       end: subject.end,
       value: subject.value,
+      current,
       eq,
       modify,
       modifyEffect,
@@ -100,7 +101,7 @@ export namespace SynchronizedSubject {
       delete: Effect.sync(() => {
         const value = getValue()
 
-        MutableRef.set(mutableRef, Option.some(value))
+        current.set(Option.some(value))
 
         return value
       }),
