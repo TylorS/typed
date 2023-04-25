0f1a230f7a3b2199ad086a049acd677f1a1a1535
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-04-25 15:59:00 -0400

    refactor: use toArray to implement toReadonlyArray

--- packages/fx/src/toReadonlyArray.ts
+++ packages/fx/src/toReadonlyArray.ts
@@ -2,16 +2,10 @@ import type { Scope } from '@effect/io/Scope'
 
 import type { Fx } from './Fx.js'
 import { Effect } from './externals.js'
-import { observe } from './observe.js'
+import { toArray } from './toArray.js'
 
 export function toReadonlyArray<R, E, A>(
   fx: Fx<R, E, A>,
 ): Effect.Effect<R | Scope, E, ReadonlyArray<A>> {
-  return Effect.gen(function* ($) {
-    const array: Array<A> = []
-
-    yield* $(observe(fx, (a) => Effect.sync(() => array.push(a))))
-
-    return array
-  })
+  return toArray(fx)
 }
