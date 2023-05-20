81fc14546cca0935adc8cda6b149a053173422b8
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-05-20 10:44:02 -0400

    feat: improve the typings of Context.id

--- packages/context/src/identifier.ts
+++ packages/context/src/identifier.ts
@@ -1,6 +1,15 @@
-export function id<const T>(uniqueIdentifier: T) {
-  return class {
-    static readonly _tag = uniqueIdentifier
-    readonly _tag = uniqueIdentifier
+export function id<const T>(uniqueIdentifier: T): TaggedIdentifierConstructor<readonly [], T> {
+  return class Tagged implements TaggedIdentifier<T> {
+    static readonly _tag: T = uniqueIdentifier
+    readonly _tag: T = uniqueIdentifier
   }
+}
+
+export interface TaggedIdentifierConstructor<Args extends readonly any[], T> {
+  readonly _tag: T
+  new (...args: Args): TaggedIdentifier<T>
+}
+
+export interface TaggedIdentifier<T> {
+  readonly _tag: T
 }
