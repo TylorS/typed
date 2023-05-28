045205b08c6ed6df41b5c57268b979b7fc196a0c
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-05-28 10:24:28 -0400

    feat: concatenate routes together

--- packages/route/src/route.ts
+++ packages/route/src/route.ts
@@ -2,23 +2,48 @@ import * as Option from '@effect/data/Option'
 import * as Path from '@typed/path'
 import * as ptr from 'path-to-regexp'
 
-export interface Route<P extends string> {
+export interface Route<in out P extends string> {
+  /**
+   * The underlying path of the route
+   */
   readonly path: P
+
+  /**
+   * Create a path from a given set params
+   */
   readonly make: MakeRoute<P>
+
+  /**
+   * Match a path against this route
+   */
   readonly match: (path: string) => Option.Option<Path.ParamsOf<P>>
+
+  /**
+   * Concatenate this route with another route
+   */
+  readonly concat: <const P2 extends string>(
+    route: Route<P2>,
+    options?: RouteOptions,
+  ) => Route<Path.PathJoin<readonly [P, P2]>>
 }
 
 export type MakeRoute<P extends string> = [keyof Path.ParamsOf<P>] extends [never]
   ? () => P
-  : <Params extends Path.ParamsOf<P>>(params: Params) => Path.Interpolate<P, Params>
+  : <const Params extends Path.ParamsOf<P>>(params: Params) => Path.Interpolate<P, Params>
 
-export function Route<P extends string>(path: P, options?: RouteOptions): Route<P> {
+export function Route<const P extends string>(path: P, options?: RouteOptions): Route<P> {
   const match = Route.makeMatch(path, options?.match)
 
   return {
     path,
     make: ptr.compile(path, options?.make) as Route<P>['make'],
     match,
+    concat<P2 extends string>(
+      route: Route<P2>,
+      options?: RouteOptions,
+    ): Route<Path.PathJoin<readonly [P, P2]>> {
+      return Route(Path.pathJoin(path, route.path), options)
+    },
   }
 }
 
