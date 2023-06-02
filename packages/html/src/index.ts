46607a850148c97076e349caa4f9d81a25da2b77
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-06-02 19:51:53 -0400

    docs: add todo for myself

--- packages/html/src/index.ts
+++ packages/html/src/index.ts
@@ -13,3 +13,9 @@ export * from '@typed/wire'
 
 /// <reference path="./udomdiff.d.ts" />
 /// <reference path="./uparser.d.ts" />
+
+// TODO: RenderTemplate needs to return a sequence of RenderEvents.
+//   - Should return an ordered list of RenderEvents
+//   - RenderEvents should be able to be used to batch updates.
+//   - RenderEvents should be able to be used to construct HTML
+//   - Ideally, RenderEvents can help with hydration
