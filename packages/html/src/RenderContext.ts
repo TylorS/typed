c7b2a54ee5ed4ea2c985cfd4a242760b22b4157c
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-06-03 17:50:45 -0400

    feat: add support for working a queue of updates in the background

--- packages/html/src/RenderContext.ts
+++ packages/html/src/RenderContext.ts
@@ -57,17 +57,23 @@ export const isBot: Effect.Effect<RenderContext, never, boolean> = RenderContext
   (ctx) => ctx.isBot,
 )
 
+export type UpdateWorkerOptions = {
+  readonly environment: Environment
+  readonly isBot?: boolean
+  readonly batchSize?: number
+}
+
+const DEFAULT_BATCH_SIZE = 10
+
 export const withUpdateWorker = (
-  environment: RenderContext['environment'],
-  isBot: RenderContext['isBot'] = false,
+  options: UpdateWorkerOptions,
 ): Layer.Layer<GlobalThis, never, RenderContext> =>
   RenderContext.layerScoped(
     Effect.gen(function* ($) {
+      const { environment, isBot = false, batchSize = DEFAULT_BATCH_SIZE } = options
       const ctx = RenderContext.make(environment, isBot)
 
-      if (environment === 'browser' || environment === 'test') {
-        yield* $(Effect.forkScoped(Effect.forever(runUpdates(ctx.updates))))
-      }
+      yield* $(Effect.forkScoped(Effect.forever(runUpdates(ctx.updates, batchSize))))
 
       return ctx
     }),
@@ -75,32 +81,93 @@ export const withUpdateWorker = (
 
 export function runUpdates(
   updates: RenderContext['updates'],
+  batchSize: number,
 ): Effect.Effect<Scope.Scope | GlobalThis, never, void> {
-  return Effect.gen(function* ($) {
-    const globalThis = yield* $(GlobalThis)
-    const deadline = yield* $(
-      Effect.asyncEffect<never, never, IdleDeadline, Scope.Scope, never, void>((resume) =>
-        Effect.suspend(() => {
+  return Effect.uninterruptibleMask((restore) =>
+    Effect.gen(function* ($) {
+      const deadline = yield* $(restore(whenIdle))
+      const iterator = updates.entries()
+
+      while (!deadline.didTimeout && deadline.timeRemaining() > 0) {
+        const batch = getNextUpdates(updates, iterator, batchSize)
+
+        if (batch.length === 0) {
+          return
+        }
+
+        yield* $(Effect.allPar(batch))
+      }
+    }),
+  )
+}
+
+function getNextUpdates(
+  updates: RenderContext['updates'],
+  iterator: IterableIterator<[Part<any, any>, Effect.Effect<never, never, unknown>]>,
+  batchSize: number,
+) {
+  const output: Effect.Effect<never, never, unknown>[] = []
+
+  for (let i = 0; i < batchSize; ++i) {
+    const result = iterator.next()
+
+    if (result.done) {
+      break
+    }
+
+    const [part, update] = result.value
+
+    output.push(
+      Effect.ensuring(
+        update,
+        Effect.sync(() => updates.delete(part)),
+      ),
+    )
+  }
+
+  return output
+}
+
+export const whenIdle = Effect.asyncEffect<
+  never,
+  never,
+  IdleDeadline,
+  GlobalThis | Scope.Scope,
+  never,
+  void
+>(
+  Effect.serviceFunctionEffect(
+    GlobalThis,
+    (globalThis: GlobalThis) =>
+      (resume: (_: Effect.Effect<never, never, IdleDeadline>) => void) => {
+        // Try to utilize requestIdleCallback if available
+        if (globalThis.requestIdleCallback) {
           const id = globalThis.requestIdleCallback((deadline) => resume(Effect.succeed(deadline)))
 
           return Effect.addFinalizer(() => Effect.sync(() => globalThis.cancelIdleCallback(id)))
-        }),
-      ),
-    )
 
-    const iterator = updates.entries()
+          // Fallback to requestAnimationFrame
+        } else if (globalThis.requestAnimationFrame) {
+          const id = globalThis.requestAnimationFrame(() =>
+            resume(Effect.succeed(mockIdleDeadline(50))),
+          )
 
-    while (!deadline.didTimeout && deadline.timeRemaining() > 0) {
-      const result = iterator.next()
+          return Effect.addFinalizer(() => Effect.sync(() => globalThis.cancelAnimationFrame(id)))
+          // Fallback to setTimeout
+        } else {
+          const id = globalThis.setTimeout(() => resume(Effect.succeed(mockIdleDeadline(50))))
 
-      if (result.done) {
-        return
-      }
+          return Effect.addFinalizer(() => Effect.sync(() => globalThis.clearTimeout(id)))
+        }
+      },
+  ),
+)
 
-      const [part, update] = result.value
+function mockIdleDeadline(duration: number): IdleDeadline {
+  const start = Date.now()
 
-      yield* $(update)
-      updates.delete(part)
-    }
-  })
+  return {
+    didTimeout: false,
+    timeRemaining: () => Math.max(0, duration - (Date.now() - start)),
+  }
 }
