803699d208635ce830cd3690e677fdd1d7fdc037
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-06-29 22:21:55 -0400

    refactor: remove logs

--- packages/html/src/paths.ts
+++ packages/html/src/paths.ts
@@ -1,9 +1,5 @@
-export const findPath = (node: ParentChildNodes, path: ReadonlyArray<number>): Node => {
-  console.log(node.childNodes, path)
-  const r = path.reduce(({ childNodes }, index) => childNodes[index], node) as Node
-  console.log(r)
-  return r
-}
+export const findPath = (node: ParentChildNodes, path: ReadonlyArray<number>): Node =>
+  path.reduce(({ childNodes }, index) => childNodes[index], node) as Node
 
 export interface ParentChildNodes {
   readonly parentNode: Node | null
