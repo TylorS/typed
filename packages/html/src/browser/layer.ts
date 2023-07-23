4f62ce72c01e6dfb7165fe2fbfba469cf7491c23
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-07-23 10:35:24 -0400

    refactor: add return type annotation

--- packages/html/src/browser/layer.ts
+++ packages/html/src/browser/layer.ts
@@ -1,7 +1,10 @@
 import * as Layer from '@effect/io/Layer'
-import { DomServicesElementParams, GlobalThis, Window, domServices } from '@typed/dom'
+import { DomServices, DomServicesElementParams, GlobalThis, Window, domServices } from '@typed/dom'
 
-export const browser = (window: Window & GlobalThis, params?: DomServicesElementParams) =>
+export const browser = (
+  window: Window & GlobalThis,
+  params?: DomServicesElementParams,
+): Layer.Layer<never, never, DomServices> =>
   Layer.provideMerge(
     Layer.succeedContext(GlobalThis.build(window).merge(Window.build(window)).context),
     domServices(params),
