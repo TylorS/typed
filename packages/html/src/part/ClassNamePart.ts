ba9a4cf5ac55b1363699b29a65a1fadf33c9867f
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-07-28 17:56:48 -0400

    fix: use classLIst

--- packages/html/src/part/ClassNamePart.ts
+++ packages/html/src/part/ClassNamePart.ts
@@ -73,12 +73,16 @@ export class ClassNamePart extends BasePart<readonly string[]> {
   static fromElement(element: Element, index: number) {
     const setClassName = (value: string) =>
       Effect.sync(() => {
-        const classNames = value.split(' ')
+        if (value === '') {
+          element.classList.remove(...Array.from(element.classList))
+        } else {
+          const classNames = value.split(' ')
 
-        element.classList.remove(
-          ...Array.from(element.classList).filter((c) => !classNames.includes(c)),
-        )
-        element.classList.add(...classNames)
+          element.classList.remove(
+            ...Array.from(element.classList).filter((c) => !classNames.includes(c)),
+          )
+          element.classList.add(...classNames)
+        }
       })
 
     return new ClassNamePart(setClassName, index, Array.from(element.classList))
