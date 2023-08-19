f5f9020ff819095085cd44f6d12a344b30f064d0
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-08-19 14:31:38 -0400

    feat: simplify the SPA use-case for SSR

--- example/server.ts
+++ example/server.ts
@@ -1,31 +1,11 @@
-import * as Duration from '@effect/data/Duration'
 import * as Effect from '@effect/io/Effect'
-import * as Layer from '@effect/io/Layer'
-import * as Http from '@effect/platform-node/HttpServer'
 import { runMain } from '@effect/platform-node/Runtime'
-import { cacheControl, renderHtml, serve, staticFileMiddleware } from '@typed/framework/server'
+import { spa } from '@typed/framework/server'
 import * as index from 'html:./index'
-import { assetDirectory, clientOutputDirectory } from 'typed:config'
 
 import { ui } from './routing.js'
 
-const app = Http.router.empty.pipe(
-  Http.router.get(
-    '/*',
-    renderHtml(index, () => ui),
-  ),
-  staticFileMiddleware({
-    enable: import.meta.env.PROD,
-    directory: clientOutputDirectory,
-    include: [`${assetDirectory}/**/*`],
-    setHeaders: cacheControl({
-      maxAge: Duration.days(365),
-      immutable: true,
-      etag: true,
-    }),
-  }),
+spa(index, () => ui, { port: 3000, staticFiles: import.meta.env.PROD }).pipe(
+  Effect.tapErrorCause(Effect.logError),
+  runMain,
 )
-
-const server = serve(app, { port: 3000 })
-
-Layer.launch(server).pipe(Effect.tapErrorCause(Effect.logError), runMain)
