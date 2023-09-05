b4fce101138c6602b08f7466bfd216f757fb4a5e
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-09-05 14:07:50 -0400

    chore: add some code comments

--- packages/fx/src/internal/core.ts
+++ packages/fx/src/internal/core.ts
@@ -9,6 +9,9 @@ import * as Sink from "@typed/fx/Sink"
 export const FxTypeId = Symbol.for("@typed/fx/Fx")
 export type FxTypeId = typeof FxTypeId
 
+/**
+ * Annotates an Effect with FxTypeId to signal it is an Fx
+ */
 export function make<R, E = never, A = never>(
   effect: Effect.Effect<R | Sink.Error<E> | Sink.Event<A>, never, unknown>
 ): Fx.WithoutSink<R, E, A> {
@@ -17,6 +20,9 @@ export function make<R, E = never, A = never>(
   }) as Fx.WithoutSink<R, E, A>
 }
 
+/**
+ * Creates an Fx from an Effect that is suspended such that the original Effect is not mutated
+ */
 export function makeSuspend<R, E = never, A = never>(
   effect: Effect.Effect<R | Sink.Error<E> | Sink.Event<A>, never, unknown>
 ): Fx.WithoutSink<R, E, A> {
@@ -29,6 +35,10 @@ export type GenResources<T> = [T] extends [never] ? never
 
 export type GenError<T> = [T] extends [never] ? never : [T] extends [Effect.EffectGen<any, infer E, any>] ? E : never
 
+/**
+ * A small extension to Effect.Adapter that adds a few more methods
+ * for sending events and errors to a Sink.
+ */
 export type Adapter = Effect.Adapter & {
   readonly succeed: <A>(a: A) => Effect.EffectGen<Sink.Event<A>, never, void>
   readonly failCause: <E>(e: Cause.Cause<E>) => Effect.EffectGen<Sink.Error<E>, never, void>
@@ -36,6 +46,12 @@ export type Adapter = Effect.Adapter & {
 }
 
 const adaptersCache = new WeakMap<Effect.Adapter, Adapter>()
+
+/**
+ * Construct an Adapter from an Effect.Adapter. This will cache the Adapter
+ * so that we don't create a new one for each Fx since .bind is not exactly
+ * cheap and there's probably only a single instance of an Effect.Adapter in your application.
+ */
 export function Adapter(effectAdapter: Effect.Adapter): Adapter {
   const cached = adaptersCache.get(effectAdapter)
 
@@ -59,6 +75,9 @@ export function Adapter(effectAdapter: Effect.Adapter): Adapter {
   }
 }
 
+/**
+ * Create an Fx from a Generator function
+ */
 export const makeGen: <E extends Effect.EffectGen<any, any, any>>(
   f: (adapter: Adapter) => Generator<E, unknown, any>
 ) => Fx.WithoutSink<
@@ -70,10 +89,16 @@ export const makeGen: <E extends Effect.EffectGen<any, any, any>>(
     Effect.catchAllCause(Effect.gen((adapter) => f(Adapter(adapter))), Sink.failCause)
   )
 
+/**
+ * Converts an Effect into an Fx
+ */
 export function fromEffect<R, E, A>(effect: Effect.Effect<R, E, A>): Fx.WithoutSink<R, E, A> {
   return make(FromEffect.fromEffect(effect))
 }
 
+/**
+ * Determines if a value is an Fx
+ */
 export function isFx<R, E, A>(value: unknown): value is Fx<R, E, A> {
   return isObject(value) && FxTypeId in value
 }
@@ -84,16 +109,25 @@ function isObject(value: unknown): value is Record<string, unknown> {
 
 export type ScopedFork = <R, E, A>(effect: Effect.Effect<R, E, A>) => Effect.Effect<R, never, Fiber.RuntimeFiber<E, A>>
 
+/**
+ * Helper for creating a locally isolated Scope for forking fibers in.
+ * The fibers will automatically close when the scope is closed.
+ */
 export function withScopedFork<R, E, A>(
   f: (fork: ScopedFork, scope: Scope.Scope) => Effect.Effect<R, E, A>
 ): Effect.Effect<R, E, A> {
   return Effect.acquireUseRelease(Scope.make(), (scope) => f(Effect.forkIn(scope), scope), Scope.close)
 }
 
+/**
+ * Execute an Fx as an Effect, providing a Sink to handle errors and events.
+ */
 export function drain<R, E, A>(fx: Fx<R, E, A>): Effect.Effect<Exclude<R, Sink.Sink<E, A>>, E, void> {
   return withScopedFork((fork) =>
     Sink.drain<E, A>().pipe(
+      // Run our Fx in the scope
       Effect.tap((drain) => fork(drain.provide(fx))),
+      // Wait for the stream to signal completion
       Effect.flatMap((drain) => drain.wait())
     )
   )
