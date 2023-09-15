0499b52f6899b19ff2b0ad9c10c9abb2aa5426e8
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-09-15 19:19:00 -0400

    docs: add some initial docs to Subject

--- packages/fx/src/Subject.ts
+++ packages/fx/src/Subject.ts
@@ -1,4 +1,36 @@
+/**
+ * Subjects are the basis for sharing events between multiple consumers in an effecient manner.
+ * @since 1.18.0
+ */
+
 import type { Fx } from "@typed/fx/Fx"
+import * as internal from "@typed/fx/internal/core-subject"
 import type { Sink } from "@typed/fx/Sink"
 
+/**
+ * A Subject is an Fx which is also a Sink, and can be used to
+ * broadcast events to many consumers.
+ */
 export interface Subject<R, E, A> extends Fx<R, E, A>, Sink<E, A> {}
+
+/**
+ * Constructs a Subject that can be used to broadcast events to many consumers.
+ * @since 1.18.0
+ */
+export const make: <E, A>() => Subject<never, E, A> = internal.makeSubject
+
+/**
+ * Constructs a Subject that can be used to broadcast events to many consumers.
+ * If a previous event has been consumed previously, any "late" subscribers will
+ * receive that previous event.
+ * @since 1.18.0
+ */
+export const makeHold: <E, A>() => Subject<never, E, A> = internal.makeHoldSubject
+
+/**
+ * Constructs a Subject that can be used to broadcast events to many consumers.
+ * If a previous event has been consumed previously, any "late" subscribers will
+ * receive _up to_ `capacity` previous events.
+ * @since 1.18.0
+ */
+export const makeReplay: <E, A>(capacity: number) => Subject<never, E, A> = internal.makeReplaySubject
