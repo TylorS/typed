e5213517bed18d8e38eee37d944dbc1124cc9625
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-09-18 17:45:53 -0400

    WIP: scaffold Navigation package

---
+++ packages/navigation/src/Navigation.ts
@@ -0,0 +1,7 @@
+import * as Context from "@typed/context"
+
+export interface Navigation {}
+
+export const Navigation: Context.Tagged<Navigation> = Context.Tagged<Navigation>(
+  "@typed/navigation/Navigation"
+)
