74c591ecfc161955577c511c77615411e476f532
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-10-05 18:55:59 -0400

    feat: convert TemplateInstance back to Fx

--- packages/template/src/TemplateInstance.ts
+++ packages/template/src/TemplateInstance.ts
@@ -1,14 +1,14 @@
+import type * as Fx from "@typed/fx/Fx"
 import type { ElementRef } from "@typed/template/ElementRef"
 import type { RenderEvent } from "@typed/template/RenderEvent"
 import type { Rendered } from "@typed/wire"
 import type { NoSuchElementException } from "effect/Cause"
 import type { Effect } from "effect/Effect"
-import type * as Stream from "effect/Stream"
 
 export const TemplateInstanceTypeId = Symbol.for("@typed/template/TemplateInstance")
 export type TemplateInstanceTypeId = typeof TemplateInstanceTypeId
 
-export interface TemplateInstance<E, T extends Rendered = Rendered> extends Stream.Stream<never, E, RenderEvent> {
+export interface TemplateInstance<E, T extends Rendered = Rendered> extends Fx.Fx<never, E, RenderEvent> {
   readonly [TemplateInstanceTypeId]: ElementRef<T>
 
   readonly get: Effect<never, NoSuchElementException, T>
