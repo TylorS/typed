f68c4e4607a561ce3960d434ac4cbc38ca8c4263
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-10-04 23:59:33 -0400

    feat: add getter

--- packages/template/src/TemplateInstance.ts
+++ packages/template/src/TemplateInstance.ts
@@ -1,6 +1,8 @@
 import type { ElementRef } from "@typed/template/ElementRef"
 import type { RenderEvent } from "@typed/template/RenderEvent"
 import type { Rendered } from "@typed/wire"
+import type { NoSuchElementException } from "effect/Cause"
+import type { Effect } from "effect/Effect"
 import type * as Stream from "effect/Stream"
 
 export const TemplateInstanceTypeId = Symbol.for("@typed/template/TemplateInstance")
@@ -9,6 +11,7 @@ export type TemplateInstanceTypeId = typeof TemplateInstanceTypeId
 export interface TemplateInstance<E, T extends Rendered = Rendered> extends Stream.Stream<never, E, RenderEvent> {
   readonly [TemplateInstanceTypeId]: ElementRef<T>
 
+  readonly get: Effect<never, NoSuchElementException, T>
   readonly query: ElementRef<T>["query"]
   readonly events: ElementRef<T>["events"]
   readonly elements: ElementRef<T>["elements"]
