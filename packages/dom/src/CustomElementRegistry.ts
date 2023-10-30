756b184f4ee3bb5e0ace8db380a4c756ee51a017
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-10-29 23:30:52 -0400

    feat: add CustomElementRegistry

---
+++ packages/dom/src/CustomElementRegistry.ts
@@ -0,0 +1,35 @@
+import * as context from "@typed/context"
+import type { NoSuchElementException } from "effect/dist/declarations/src/Cause"
+import * as Effect from "effect/Effect"
+import * as Option from "effect/Option"
+
+export interface CustomElementRegistry extends globalThis.CustomElementRegistry {}
+
+export type CustomElementConstructor = globalThis.CustomElementConstructor
+
+export type ElementDefinitionOptions = globalThis.ElementDefinitionOptions
+
+export const CustomElementRegistry: context.Tagged<CustomElementRegistry> = context.Tagged<CustomElementRegistry>(
+  "@typed/dom/CustomElementRegister"
+)
+
+export const get = <K extends keyof HTMLElementTagNameMap>(
+  name: K
+): Effect.Effect<CustomElementRegistry, NoSuchElementException, CustomElementConstructor> =>
+  CustomElementRegistry.withEffect((r) => Option.fromNullable(r.get(name)))
+
+export const define = <K extends keyof HTMLElementTagNameMap>(
+  name: K,
+  constructor: CustomElementConstructor,
+  options?: ElementDefinitionOptions
+): Effect.Effect<CustomElementRegistry, never, void> =>
+  CustomElementRegistry.with((r) => r.define(name, constructor, options))
+
+export const whenDefined = <K extends keyof HTMLElementTagNameMap>(
+  name: K
+): Effect.Effect<CustomElementRegistry, never, CustomElementConstructor> =>
+  CustomElementRegistry.withEffect((r) => Effect.promise(() => r.whenDefined(name)))
+
+export const upgrade = (
+  node: Node
+): Effect.Effect<CustomElementRegistry, never, void> => CustomElementRegistry.with((r) => r.upgrade(node))
