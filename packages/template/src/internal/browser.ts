7fc6b79ab074263642b09e281d603961a771e6e3
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-10-29 23:32:50 -0400

    perf: use explicit comparision

--- packages/template/src/internal/browser.ts
+++ packages/template/src/internal/browser.ts
@@ -19,7 +19,7 @@ export function makeRenderNodePart(
   return new NodePartImpl(index, ({ part, value }) => {
     return ctx.queue.add(part, () => {
       matchValue(value, (content) => {
-        if (!text) {
+        if (text === undefined) {
           text = document.createTextNode("")
         }
         text.textContent = content
