1e0c151c7dea4281005c14a3f4e29ca7f6a3cf7d
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-01-17 17:15:14 -0500

    test: add some basic Decoder tests

---
+++ packages/decoder/test/index.ts
@@ -0,0 +1,75 @@
+import * as Schema from "@effect/schema/Schema"
+import * as Decoder from "@typed/decoder"
+import * as Effect from "effect/Effect"
+
+describe("Decoder", () => {
+  const schema = Schema.struct({
+    id: Schema.string,
+    name: Schema.string,
+    age: Schema.number
+  })
+
+  const decoder = Decoder.fromSchema(schema)
+
+  it("decodes a schema", async () => {
+    const test = Effect.gen(function*(_) {
+      const result = yield* _(decoder.decode({ id: "1", name: "Bob", age: 42 }))
+
+      expect(result).toEqual({
+        id: "1",
+        name: "Bob",
+        age: 42
+      })
+    })
+
+    await Effect.runPromise(test)
+  })
+
+  describe("mapEffect", () => {
+    it("allows transforming the result of a decoder with an Effect", async () => {
+      const text = Decoder.mapEffect(decoder, (r) => Effect.succeed(`${r.name} is ${r.age}`))
+      const test = Effect.gen(function*(_) {
+        const result = yield* _(text.decode({ id: "1", name: "Bob", age: 42 }))
+
+        expect(result).toEqual("Bob is 42")
+      })
+
+      await Effect.runPromise(test)
+    })
+
+    it("allows transforming the result of a Schema with an Effect", async () => {
+      const text = Decoder.mapEffect(schema, (r) => Effect.succeed(`${r.name} is ${r.age}`))
+      const test = Effect.gen(function*(_) {
+        const result = yield* _(text.decode({ id: "1", name: "Bob", age: 42 }))
+
+        expect(result).toEqual("Bob is 42")
+      })
+
+      await Effect.runPromise(test)
+    })
+  })
+
+  describe("map", () => {
+    it("allows transforming the result of a decoder", async () => {
+      const text = Decoder.map(decoder, (r) => `${r.name} is ${r.age}`)
+      const test = Effect.gen(function*(_) {
+        const result = yield* _(text.decode({ id: "1", name: "Bob", age: 42 }))
+
+        expect(result).toEqual("Bob is 42")
+      })
+
+      await Effect.runPromise(test)
+    })
+
+    it("allows transforming the result of a schema", async () => {
+      const text = Decoder.map(schema, (r) => `${r.name} is ${r.age}`)
+      const test = Effect.gen(function*(_) {
+        const result = yield* _(text.decode({ id: "1", name: "Bob", age: 42 }))
+
+        expect(result).toEqual("Bob is 42")
+      })
+
+      await Effect.runPromise(test)
+    })
+  })
+})
