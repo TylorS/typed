883de2b053fc91192242778e423e6ae3b4044489
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-02-28 03:14:21 -0500

    fix: add timezones to timestamps

--- examples/realworld/src/api/infrastructure/db/migrations/001_initialize.ts
+++ examples/realworld/src/api/infrastructure/db/migrations/001_initialize.ts
@@ -11,8 +11,8 @@ export default Effect.flatMap(
   password TEXT NOT NULL,
   bio text,
   image text,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   deleted boolean NOT NULL DEFAULT FALSE
 );`)
 
@@ -21,8 +21,8 @@ export default Effect.flatMap(
   title text NOT NULL,
   description text NOT NULL,
   body text NOT NULL,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   author text NOT NULL REFERENCES users(email),
   deleted boolean NOT NULL DEFAULT FALSE
 );`)
@@ -30,8 +30,8 @@ export default Effect.flatMap(
       yield* _(sql`CREATE TABLE IF NOT EXISTS comments(
   id serial PRIMARY KEY,
   body text NOT NULL,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   author text NOT NULL REFERENCES users(email),
   article text NOT NULL REFERENCES articles(slug),
   deleted boolean NOT NULL DEFAULT FALSE
@@ -40,7 +40,7 @@ export default Effect.flatMap(
       yield* _(sql`CREATE TABLE IF NOT EXISTS favorites(
   user_email text NOT NULL REFERENCES users(email),
   article_slug text NOT NULL REFERENCES articles(slug),
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   deleted boolean NOT NULL DEFAULT FALSE,
   PRIMARY KEY (user_email, article_slug)
 );
@@ -49,7 +49,7 @@ export default Effect.flatMap(
       yield* _(sql`CREATE TABLE IF NOT EXISTS follows(
   follower text NOT NULL REFERENCES users(email),
   followed text NOT NULL REFERENCES users(email),
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   deleted boolean NOT NULL DEFAULT FALSE,
   PRIMARY KEY (follower, followed)
 );
@@ -58,14 +58,14 @@ export default Effect.flatMap(
       yield* _(sql`
 CREATE TABLE IF NOT EXISTS tags(
   tag text PRIMARY KEY NOT NULL UNIQUE,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   deleted boolean NOT NULL DEFAULT FALSE
 );`)
 
       yield* _(sql`CREATE TABLE IF NOT EXISTS article_tags(
   article_slug text NOT NULL REFERENCES articles(slug),
   tag text NOT NULL REFERENCES tags(tag),
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
   deleted boolean NOT NULL DEFAULT FALSE,
   PRIMARY KEY (article_slug, tag)
 );`)
@@ -73,8 +73,8 @@ CREATE TABLE IF NOT EXISTS tags(
       yield* _(sql`CREATE TABLE IF NOT EXISTS jwts(
   user_email text NOT NULL REFERENCES users(email),
   jwt text NOT NULL,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  expires_at timestamp NOT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  expires_at timestamp with time zone NOT NULL,
   revoked boolean NOT NULL DEFAULT FALSE,
   PRIMARY KEY (user_email, jwt)
 );`)
