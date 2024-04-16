d873e4981f16a58859b9a7f3ef470bce622ca465
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-04-15 23:01:25 -0400

    fix: better mime types

--- packages/core/src/Platform.ts
+++ packages/core/src/Platform.ts
@@ -227,6 +227,7 @@ function isFile(fs: FileSystem, path: string) {
 function gzipHeaders(filePath: string, cacheControl?: (filePath: string) => { maxAge: number; immutable?: boolean }) {
   return {
     "Content-Encoding": "gzip",
+    "Content-Type": getContentType(filePath.slice(0, -3)),
     ...cacheControlHeaders(filePath, cacheControl)
   }
 }
@@ -241,6 +242,109 @@ function cacheControlHeaders(
   const { immutable, maxAge } = cacheControl(filePath)
 
   return {
-    "Cache-Control": `${immutable ? "public, " : ""}max-age=${maxAge}`
+    "Cache-Control": `${immutable ? "public, " : ""}max-age=${maxAge}`,
   }
+}
+
+const mimeTypesToExtensions = {
+  "application/atom+xml": ["atom"],
+  "application/java-archive": ["jar","war","ear"],
+  "application/javascript": ["js"],
+  "application/json": ["json"],
+  "application/mac-binhex40": ["hqx"],
+  "application/msword": ["doc"],
+  "application/octet-stream": ["bin","exe","dll","deb","dmg","iso","img","msi","msp","msm"],
+  "application/pdf": ["pdf"],
+  "application/postscript": ["ps","eps","ai"],
+  "application/rss+xml": ["rss"],
+  "application/rtf": ["rtf"],
+  "application/vnd.apple.mpegurl": ["m3u8"],
+  "application/vnd.google-earth.kml+xml": ["kml"],
+  "application/vnd.google-earth.kmz": ["kmz"],
+  "application/vnd.ms-excel": ["xls"],
+  "application/vnd.ms-fontobject": ["eot"],
+  "application/vnd.ms-powerpoint": ["ppt"],
+  "application/vnd.oasis.opendocument.graphics": ["odg"],
+  "application/vnd.oasis.opendocument.presentation": ["odp"],
+  "application/vnd.oasis.opendocument.spreadsheet": ["ods"],
+  "application/vnd.oasis.opendocument.text": ["odt"],
+  "application/vnd.openxmlformats-officedocument.presentationml.presentation": ["pptx"],
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": ["xlsx"],
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document": ["docx"],
+  "application/vnd.wap.wmlc": ["wmlc"],
+  "application/wasm": ["wasm"],
+  "application/x-7z-compressed": ["7z"],
+  "application/x-cocoa": ["cco"],
+  "application/x-java-archive-diff": ["jardiff"],
+  "application/x-java-jnlp-file": ["jnlp"],
+  "application/x-makeself": ["run"],
+  "application/x-perl": ["pl","pm"],
+  "application/x-pilot": ["prc","pdb"],
+  "application/x-rar-compressed": ["rar"],
+  "application/x-redhat-package-manager": ["rpm"],
+  "application/x-sea": ["sea"],
+  "application/x-shockwave-flash": ["swf"],
+  "application/x-stuffit": ["sit"],
+  "application/x-tcl": ["tcl","tk"],
+  "application/x-x509-ca-cert": ["der","pem","crt"],
+  "application/x-xpinstall": ["xpi"],
+  "application/xhtml+xml": ["xhtml"],
+  "application/xspf+xml": ["xspf"],
+  "application/zip": ["zip"],
+  "audio/midi": ["mid","midi","kar"],
+  "audio/mpeg": ["mp3"],
+  "audio/ogg": ["ogg"],
+  "audio/x-m4a": ["m4a"],
+  "audio/x-realaudio": ["ra"],
+  "font/woff": ["woff"],
+  "font/woff2": ["woff2"],
+  "image/avif": ["avif"],
+  "image/gif": ["gif"],
+  "image/jpeg": ["jpeg","jpg"],
+  "image/png": ["png"],
+  "image/svg+xml": ["svg","svgz"],
+  "image/tiff": ["tif","tiff"],
+  "image/vnd.wap.wbmp": ["wbmp"],
+  "image/webp": ["webp"],
+  "image/x-icon": ["ico"],
+  "image/x-jng": ["jng"],
+  "image/x-ms-bmp": ["bmp"],
+  "text/css": ["css"],
+  "text/html": ["html","htm","shtml"],
+  "text/mathml": ["mml"],
+  "text/plain": ["txt"],
+  "text/vnd.sun.j2me.app-descriptor": ["jad"],
+  "text/vnd.wap.wml": ["wml"],
+  "text/x-component": ["htc"],
+  "text/xml": ["xml"],
+  "video/3gpp": ["3gpp","3gp"],
+  "video/mp2t": ["ts"],
+  "video/mp4": ["mp4"],
+  "video/mpeg": ["mpeg","mpg"],
+  "video/quicktime": ["mov"],
+  "video/webm": ["webm"],
+  "video/x-flv": ["flv"],
+  "video/x-m4v": ["m4v"],
+  "video/x-mng": ["mng"],
+  "video/x-ms-asf": ["asx","asf"],
+  "video/x-ms-wmv": ["wmv"],
+  "video/x-msvideo": ["avi"],
+}
+
+const extensionsToMimeTypes = Object.entries(mimeTypesToExtensions).reduce(
+  (acc, [mimeType, extensions]) => {
+    for (const extension of extensions) {
+      acc[extension] = mimeType
+    }
+    return acc
+  },
+  {} as Record<string, string>
+)
+
+/**
+ * @since 1.0.0
+ */
+export const getContentType = (filePath: string) => { 
+  const extension = filePath.split(".").pop() ?? ""
+  return extensionsToMimeTypes[extension] ?? "application/octet-stream"
 }
