a00fed56727119518454961a1a06d8dbaddceb8f
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-04-20 14:30:10 -0400

    WIP: switch back to rendering for now

--- examples/realworld/src/client.ts
+++ examples/realworld/src/client.ts
@@ -1,7 +1,7 @@
 import "./styles.css"
 
 import { isAuthenticated } from "@/services"
-import { hydrateFromWindow, Fx, Navigation, Router, hydrateToLayer } from "@typed/core"
+import { fromWindow, Fx, Navigation, renderToLayer, Router } from "@typed/core"
 import { Storage } from "@typed/dom/Storage"
 import { Effect, Layer, Logger, LogLevel, pipe } from "effect"
 import * as Ui from "./ui"
@@ -14,13 +14,13 @@ const onNotFound = Effect.gen(function*(_) {
   }
 })
 
-const main = pipe(Ui.router, Router.notFoundWith(onNotFound), Ui.layout, hydrateToLayer, Layer.launch)
+const main = pipe(Ui.router, Router.notFoundWith(onNotFound), Ui.layout, renderToLayer, Layer.launch)
 
 pipe(
   main,
   Effect.provide(Ui.Live),
   Effect.provide(Storage.layer(localStorage)),
-  Effect.provide(hydrateFromWindow(window, { rootElement: document.getElementById("app")! })),
+  Effect.provide(fromWindow(window, { rootElement: document.getElementById("app")! })),
   Logger.withMinimumLogLevel(LogLevel.Debug),
   Effect.scoped,
   Effect.runPromise
