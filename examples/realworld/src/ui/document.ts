c6a20bb83a844650fa28bc5259d332fa490a95f6
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-04-21 01:58:23 -0400

    feat: avoid 3rd party hosting for css + fonts

--- examples/realworld/src/ui/document.ts
+++ examples/realworld/src/ui/document.ts
@@ -15,17 +15,6 @@ export function document<Content extends Fx.Fx<RenderEvent | null, any, any>>(
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://code/iconicframework.com" >
         <link rel="preconnect" href="https://fonts.googleapis.com" >
-        <link
-          href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link rel="stylesheet" type="text/css" href="https://demo.productionready.io/main.css" />
         <base href="/" />
         ${head}
       </head>
