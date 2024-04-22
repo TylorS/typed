37982e393dd5d7dde79569494b11a98b2ae8422a
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-04-22 17:50:46 -0400

    fix: ensure template builds

--- packages/template/src/internal/v2/hydrate.ts
+++ packages/template/src/internal/v2/hydrate.ts
@@ -9,9 +9,17 @@ import { DomRenderEvent, type RenderEvent } from "../../RenderEvent.js"
 import type { RenderQueue } from "../../RenderQueue.js"
 import type { RenderTemplate } from "../../RenderTemplate.js"
 import type * as Template from "../../Template.js"
-import { getHydrateEntry } from "../hydrate.js"
+import { CouldNotFindCommentError, CouldNotFindManyCommentError } from "../errors.js"
+import type { CouldNotFindRootElement } from "../errors.js"
 import { HydrateContext } from "../HydrateContext.js"
-import { findHydratePath, type ParentChildNodes } from "../utils.js"
+import {
+  findHydratePath,
+  getPreviousNodes,
+  isComment,
+  isCommentStartingWithValue,
+  isCommentWithValue,
+  type ParentChildNodes
+} from "../utils.js"
 import { getBrowserEntry } from "./render-entry.js"
 import type { TemplateContext } from "./render.js"
 import {
@@ -170,4 +178,217 @@ function setupPart(
     console.error(error)
     throw error
   }
+}
+
+export function findRootParentChildNodes(where: HTMLElement): ParentChildNodes {
+  const childNodes = findRootChildNodes(where)
+
+  return {
+    parentNode: where,
+    childNodes
+  }
+}
+
+const START = "typed-start"
+const END = "typed-end"
+
+// Finds all of the childNodes between the "typed-start" and "typed-end" comments
+export function findRootChildNodes(where: HTMLElement): Array<Node> {
+  let start = -1
+  let end = -1
+
+  const { childNodes } = where
+  const length = childNodes.length
+
+  for (let i = 0; i < length; i++) {
+    const node = childNodes[i]
+
+    if (node.nodeType === node.COMMENT_NODE && node.nodeValue === START) {
+      start = i
+      break
+    }
+  }
+
+  for (let i = length - 1; i >= Math.max(start, 0); i--) {
+    const node = childNodes[i]
+
+    if (node.nodeType === node.COMMENT_NODE && node.nodeValue === END) {
+      end = i
+      break
+    }
+  }
+
+  // If we can't find the start and end comments, just return all childNodes
+  if (start === -1 && end === -1) {
+    return Array.from(childNodes)
+  }
+
+  start = start === -1 ? 0 : start
+  end = end === -1 ? length - 1 : end
+
+  const rootChildNodes: Array<Node> = Array(end - start)
+
+  for (let i = start + 1, j = 0; i <= end; i++) {
+    rootChildNodes[j++] = childNodes[i]
+  }
+
+  return rootChildNodes
+}
+
+export function findTemplateResultPartChildNodes(
+  where: ParentChildNodes,
+  parentTemplate: Template.Template,
+  partIndex: number,
+  manyIndex?: string
+): Either.Either<ParentChildNodes, CouldNotFindRootElement | CouldNotFindManyCommentError | CouldNotFindCommentError> {
+  const [, path] = parentTemplate.parts[partIndex]
+  const parentNode = findHydratePath(where, path) as HTMLElement
+
+  const childNodesEither = findPartChildNodes(parentNode, partIndex)
+  if (Either.isLeft(childNodesEither)) return Either.left(childNodesEither.left)
+
+  const childNodes = childNodesEither.right
+
+  if (manyIndex) {
+    const manyChildNodes = findManyChildNodes(childNodes, manyIndex)
+    if (Either.isLeft(manyChildNodes)) return Either.left(manyChildNodes.left)
+    return Either.right<ParentChildNodes>({ parentNode, childNodes: manyChildNodes.right })
+  }
+
+  return Either.right<ParentChildNodes>({
+    parentNode,
+    childNodes
+  })
+}
+
+export function findManyChildNodes(
+  childNodes: Array<Node>,
+  manyIndex: string
+): Either.Either<Array<Node>, CouldNotFindManyCommentError> {
+  const either = findManyComment(childNodes, manyIndex)
+  if (Either.isLeft(either)) return Either.left(either.left)
+
+  const [, index] = either.right
+  const toCheck = childNodes.slice(0, index)
+  const nodes = findPreviousManyComment(toCheck)
+
+  return Either.right(nodes)
+}
+
+export function findPartChildNodes(
+  { childNodes }: ParentChildNodes,
+  partIndex: number
+): Either.Either<Array<Node>, CouldNotFindRootElement | CouldNotFindCommentError> {
+  const either = findPartComment(childNodes, partIndex)
+  if (Either.isLeft(either)) return Either.left(either.left)
+  const [comment] = either.right
+  return Either.right([...getPreviousNodes(comment, partIndex), comment])
+}
+
+export function findPartComment(
+  childNodes: ArrayLike<Node>,
+  partIndex: number
+): Either.Either<readonly [Comment, number], CouldNotFindCommentError> {
+  const search = partIndex === -1 ? `typed-end` : `hole${partIndex}`
+
+  for (let i = 0; i < childNodes.length; ++i) {
+    const node = childNodes[i]
+
+    if (isCommentWithValue(node, search)) {
+      return Either.right([node, i] as const)
+    }
+  }
+
+  return Either.left(new CouldNotFindCommentError(partIndex))
+}
+
+export function findManyComment(
+  childNodes: ArrayLike<Node>,
+  manyIndex: string
+): Either.Either<readonly [Comment, number], CouldNotFindManyCommentError> {
+  const search = `many${manyIndex}`
+
+  for (let i = 0; i < childNodes.length; ++i) {
+    const node = childNodes[i]
+
+    if (isCommentWithValue(node, search)) {
+      return Either.right([node, i] as const)
+    }
+  }
+
+  return Either.left(new CouldNotFindManyCommentError(manyIndex))
+}
+
+export function findPreviousManyComment(
+  childNodes: Array<Node>
+) {
+  for (let i = childNodes.length - 1; i > -1; --i) {
+    const node = childNodes[i]
+
+    if (isCommentStartingWithValue(node, "many")) {
+      return childNodes.slice(i + 1)
+    }
+  }
+  return childNodes
+}
+
+export function getHydrateEntry({
+  document,
+  manyIndex,
+  parentTemplate,
+  renderContext,
+  rootIndex,
+  strings,
+  where
+}: {
+  document: Document
+  renderContext: RenderContext
+  where: ParentChildNodes
+  rootIndex: number
+  parentTemplate: Template.Template | null
+  strings: TemplateStringsArray
+  manyIndex?: string
+}): Either.Either<
+  { readonly template: Template.Template; readonly wire: Node | Array<Node>; readonly where: ParentChildNodes },
+  CouldNotFindRootElement | CouldNotFindCommentError | CouldNotFindManyCommentError
+> {
+  const { template } = getBrowserEntry(document, renderContext, strings)
+
+  if (parentTemplate) {
+    const either = findTemplateResultPartChildNodes(where, parentTemplate, rootIndex, manyIndex)
+    if (Either.isLeft(either)) {
+      return Either.left(either.left)
+    }
+    where = either.right
+  }
+
+  const wire = (() => {
+    if (!parentTemplate) {
+      const childNodes = Array.from(where.childNodes).filter((node) =>
+        isComment(node) ? !isCommentWithValue(node, END) : true
+      )
+
+      if (childNodes.length === 1) {
+        return childNodes[0]
+      }
+
+      return childNodes
+    }
+
+    if (where.childNodes.length === 1) {
+      return where.childNodes[0]
+    }
+
+    return Array.from(where.childNodes).filter(
+      (node) => !isCommentWithValue(node, `hole${rootIndex}`)
+    )
+  })()
+
+  return Either.right(
+    {
+      template,
+      wire,
+      where
+    } as const
+  )
 }
