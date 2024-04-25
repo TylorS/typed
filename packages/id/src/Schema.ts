5ecb7b8fbf806495d9a1082cbdd4cced44bc84ee
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-04-25 19:44:12 -0400

    fix: ensure nanoId arbs are valid

--- packages/id/src/Schema.ts
+++ packages/id/src/Schema.ts
@@ -2,6 +2,7 @@
  * @since 1.0.0
  */
 
+import { Arbitrary } from "@effect/schema"
 import * as Schema from "@effect/schema/Schema"
 import * as NanoId from "./NanoId.js"
 import * as Uuid from "./Uuid.js"
@@ -9,7 +10,7 @@ import * as Uuid from "./Uuid.js"
 /**
  * @since 1.0.0
  */
-export const uuid: Schema.Schema<Uuid.Uuid, string> = Schema.String.pipe(
+export const uuid: Schema.Schema<Uuid.Uuid, string> = Schema.UUID.pipe(
   Schema.fromBrand(Uuid.Uuid)
 )
 
@@ -17,5 +18,12 @@ export const uuid: Schema.Schema<Uuid.Uuid, string> = Schema.String.pipe(
  * @since 1.0.0
  */
 export const nanoId: Schema.Schema<NanoId.NanoId, string> = Schema.String.pipe(
-  Schema.fromBrand(NanoId.NanoId)
+  Schema.length(21),
+  Schema.pattern(/^[A-Za-z0-9_-]+$/),
+  Schema.fromBrand(NanoId.NanoId),
+  Arbitrary.arbitrary(() => (fc) =>
+    fc.array(fc.integer({ min: 0, max: 63 }), { minLength: 21, maxLength: 21 }).map((_) =>
+      NanoId.nanoId(_ as any as NanoId.NanoIdSeed)
+    )
+  )
 )
