8afac1eb4c8db39a54caaad7d587ebf1a35d162e
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-05-06 13:31:16 -0400

    fix: add support for Fx-based params

--- examples/realworld/src/ui/components/NavLink.ts
+++ examples/realworld/src/ui/components/NavLink.ts
@@ -1,28 +1,37 @@
-import type { Placeholder, RenderEvent, Route } from "@typed/core"
-import { EventHandler, Fx, html, Navigation, Router } from "@typed/core"
+import type { RenderEvent, Route } from "@typed/core"
+import { Fx, html, Link, Placeholder, Router } from "@typed/core"
 import type { MatchInput } from "@typed/router"
 import { asRouteGuard } from "@typed/router"
 
-export function NavLink<E, R, I extends MatchInput.Any>(
+type NavLinkRouteParams<I extends MatchInput.Any, E2, R2> =
+  | Route.Route.ParamsList<MatchInput.Route<I>>
+  | [Fx.Fx<Route.Route.Params<MatchInput.Route<I>>, E2, R2>]
+
+export function NavLink<E, R, I extends MatchInput.Any, E2 = never, R2 = never>(
   content: Placeholder<string | RenderEvent, E, R>,
   input: I,
-  ...params: Route.Route.ParamsList<MatchInput.Route<I>>
+  ...params: NavLinkRouteParams<I, E2, R2>
 ) {
-  const { route } = asRouteGuard(input)
-  const to = route.interpolate(params[0] ?? {})
-  const isActive = Router.isActive(route, ...params)
-  const className = Fx.when(isActive, {
-    onFalse: "nav-link",
-    onTrue: "nav-link active"
-  })
+  return Fx.gen(function*(_) {
+    const { route } = asRouteGuard(input)
+    const paramsRef = yield* Placeholder.asRef((params[0] ?? {}) as Route.Route.Params<MatchInput.Route<I>>)
+    const to = Fx.map(paramsRef, (params) => route.interpolate(params))
+    const isActive = Fx.switchMap(paramsRef, (params) => Router.isActive<MatchInput.Route<I>>(route, params))
+    const className = Fx.when(isActive, {
+      onFalse: "nav-link",
+      onTrue: "nav-link active"
+    })
 
-  return html`<li class="nav-item">
-    <a
-      onclick=${EventHandler.preventDefault(() => Navigation.navigate(to))} 
-      class=${className}
-      href=${to}
-    >
-      ${content}
-    </a>
+    return html`<li class="nav-item">
+    ${
+      Link(
+        {
+          to,
+          className
+        },
+        content
+      )
+    }
   </li>`
+  })
 }
