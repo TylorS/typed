ef70bbc1be9ad72cd4858a58a85c567efb4707f2
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-05-15 12:33:08 -0400

    fix: use Link component

--- examples/realworld/src/ui/components/NavLink.ts
+++ examples/realworld/src/ui/components/NavLink.ts
@@ -1,8 +1,7 @@
 import type { Placeholder, RenderEvent, Route } from "@typed/core"
-import { EventHandler, Fx, html, Navigation, RefSubject, Router } from "@typed/core"
+import { html, Link, RefSubject, Router } from "@typed/core"
 import type { MatchInput } from "@typed/router"
 import { asRouteGuard } from "@typed/router"
-import { Effect } from "effect"
 
 export function NavLink<E, R, I extends MatchInput.Any>(
   props: {
@@ -12,14 +11,21 @@ export function NavLink<E, R, I extends MatchInput.Any>(
   },
   ...params: Route.Route.ParamsList<MatchInput.Route<I>>
 ) {
-  return Fx.gen(function*(_) {
-    const { route } = asRouteGuard(props.route)
-    const to = route.interpolate(params[0] ?? {})
-    const isActive = Router.isActive<MatchInput.Route<I>>(route, ...params)
-    const href = props.relative ? Router.makeHref(route, ...params) : yield* RefSubject.of(to)
-    const className = RefSubject.when(isActive, { onFalse: "nav-link", onTrue: "nav-link active" })
-    const onClick = EventHandler.preventDefault(() => Effect.flatMap(href, Navigation.navigate))
+  const { route } = asRouteGuard(props.route)
+  const to = route.interpolate(params[0] ?? {})
+  const isActive = Router.isActive<MatchInput.Route<I>>(route, ...params)
+  const className = RefSubject.when(isActive, { onFalse: "nav-link", onTrue: "nav-link active" })
 
-    return html`<li class="nav-item"><a href=${to} class=${className} onclick=${onClick}>${props.content}</a></li>`
-  })
+  return html`<li class="nav-item">
+    ${
+    Link(
+      {
+        to,
+        className,
+        relative: props.relative ?? false
+      },
+      props.content
+    )
+  }
+  </li>`
 }
